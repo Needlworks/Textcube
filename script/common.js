fb579393c31eaead73b01fde0886431004ba6d63
peris <textcube@needlworks.org>
2006-04-24 10:15:12 +0000

    replaceAll 문제 수정 (crizin 님이 지적하신 부분 수정)
    http://www.tattertools.com/ko/forum/viewtopic.php?id=181

--- script/common.js
+++ script/common.js
@@ -722,13 +722,7 @@ if(!String.prototype.trim) {
 
 if(!String.prototype.replaceAll) {
 	String.prototype.replaceAll = function(source, target) {
+		source = source.replace(/(\W)/g, "\\$1");
 		return this.replace(new RegExp(source, "gi"), target);
-		/*
-		var buffer = this;
-		if(source != target)
-			while(buffer.indexOf(source) != -1)
-				buffer = buffer.replace(source, target);
-		return buffer;
-		*/
 	}
 }
