f2a762e07cc5aea09b0ecb1d77c0f5377ea0da49
inureyes <inureyes@gmail.com>
2006-04-26 15:17:10 +0000

    * Synchronize with latest trunk. (rev.54)

--- script/common.js
+++ script/common.js
@@ -669,24 +669,73 @@ function searchChildNodes(obj, tagName) {
 	return nodes;
 }
 
-function getEmbedCode(url,w,h,id,bg,vars,menu){
-
-	if( menu == undefined) {
-		menu = true;	
+function getEmbedCode(movie,width,height,id,bg,FlashVars,menu, transparent, quality, bgcolor, allowScriptAccess, version){
+	try {
+		if(movie == undefined || width == undefined || height == undefined)
+			return false;
+		
+		if ( FlashVars == undefined) {
+			var _FlashVars_object = '';
+			var _FlashVars_embed = '';
+		} else {
+			var _FlashVars_object = '<param name="FlashVars" value="'+FlashVars+'" />';
+			var _FlashVars_embed = ' FlashVars="'+FlashVars+'" ';
+		}
+		
+		if ( menu == undefined) {
+			var _menu_object = '';
+			var _menu_embed = '';
+		} else {
+			var _menu_object = '<param name="menu" value="'+menu+'" />';
+			var _menu_embed = ' menu="'+menu+'" ';
+		}
+		
+		if ( transparent == undefined) {
+			var _transparent_object = '';
+			var _transparent_embed = '';
+		} else {
+			var _transparent_object = '<param name="wmode" value="'+transparent+'" />';
+			var _transparent_embed = ' wmode="'+transparent+'" ';
+		}
+		
+		if ( quality == undefined) {
+			var _quality_object = '';
+			var _quality_embed = '';
+		} else {
+			var _quality_object = '<param name="quality" value="'+quality+'" />';
+			var _quality_embed = ' quality="'+quality+'" ';
+		}
+		
+		if ( bgcolor == undefined) {
+			var _bgcolor_object = '';
+			var _bgcolor_embed = '';
+		} else {
+			var _bgcolor_object = '<param name="bgcolor" value="'+bgcolor+'" />';
+			var _bgcolor_embed = ' bgcolor="'+bgcolor+'" ';
+		}
+		
+		if ( allowScriptAccess == undefined) {
+			var _allowScriptAccess_object = '';
+			var _allowScriptAccess_embed = '';
+		} else {
+			var _allowScriptAccess_object = '<param name="allowScriptAccess" value="'+allowScriptAccess+'" />';
+			var _allowScriptAccess_embed = ' allowScriptAccess="'+allowScriptAccess+'" ';
+		}
+	
+		if  (version == undefined) {
+			version = '7,0,0,0';
+		}
+	
+		var flashStr=
+		'<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version='+version+'" width="'+width+'" height="'+height+'" id="'+id+'" align="middle"><param name="movie" value="'+movie+'" />'+_allowScriptAccess_object+_FlashVars_object+_menu_object+_quality_object+_bgcolor_object+_transparent_object;
+		flashStr += '<embed type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" src="'+movie+'"'+' width="'+width+'"'+' height="'+height+'"'+_allowScriptAccess_embed+_FlashVars_embed+_menu_embed+_quality_embed+_bgcolor_embed+_transparent_embed+'/>'+
+		'</object>';
+		
+		return flashStr;
+	} catch(e) {
+		return false;
 	}
 	
-	var flashStr=
-	'<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0" width="'+w+'" height="'+h+'" id="'+id+'" align="middle">'+
-	'<param name="allowScriptAccess" value="always" />'+
-	'<param name="movie" value="'+url+'" />'+
-	'<param name="FlashVars" value="'+vars+'" />'+
-	'<param name="menu" value="'+menu+'" />'+
-	'<param name="quality" value="high" />'+
-	'<param name="bgcolor" value="'+bg+'" />'+
-	'<embed src="'+url+'" FlashVars="'+vars+'" menu="false" quality="high" bgcolor="'+bg+'" width="'+w+'" height="'+h+'" name="'+id+'" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />'+
-	'</object>';
-
-	return flashStr;
 } 
 
 function writeCode(str, id) {
