3f31be834a5d75823e697216a0976869c5c12e45
inureyes <inureyes@gmail.com>
2006-04-29 04:52:24 +0000

    * i18n관련 지역화 수정 및 추가   * English  : Remengen님  ( http://tattertools.com/ko/forum/viewtopic.php?id=235 )   * Japanese : Louice P.님 ( http://tattertools.com/ko/forum/viewtopic.php?id=225 )
     * body id 관련 개선사항
      * 카테고리나 검색, 태그등 리스트 관련된 경우 그 리스트의 검색글을 id로 돌려준다.
      * 글읽기때는 entry를, 공지글 읽기 때는 notice를 id로 돌려준다.
      * 리스트 모드에서 검색어를 id로 돌려줄 경우 한글, white-space 처리에 문제가 있으므로
        다른 방법을 의논하여 수정해야 한다.

--- lib/piece/blog/begin.php
+++ lib/piece/blog/begin.php
@@ -7,9 +7,11 @@ else
 	$pageTitle = '';
 $skin = new Skin($skinSetting['skin']);
 $view = str_replace('[##_t3_##]', getUpperView(isset($paging) ? $paging : null) . $skin->skin . getLowerView() . getScriptsOnFoot(), $skin->outter);
-if (isset($list))
-	$view = str_replace('[##_body_id_##]',htmlspecialchars($list['title']), $view);
-else
+
+if ($suri['directive'] == '/')
 	$view = str_replace('[##_body_id_##]','page', $view);
-	
+else if (isset($list))
+	$view = str_replace('[##_body_id_##]',$suri['value'], $view);
+else
+	$view = str_replace('[##_body_id_##]',ltrim($suri['directive'],'/'), $view);
 ?>
