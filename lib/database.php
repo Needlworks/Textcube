0883b1d5343f207c1ea198dec3824f34117cbd42
papacha <textcube@needlworks.org>
2006-05-01 05:49:54 +0000

    #67  * Eolin.PHP.Core component    * Add UTF8::length()    * Add UTF8::lengthAsEm()    * Add UTF8::lessen()    * Add UTF8::lessenAsByte()    * Add UTF8::lessenAsEm()  * 사용하는 DB의 UTF8 지원 여부에 따라 길이 제한을 반영하는 함수 추가    * Add mysql_lessen()

--- lib/database.php
+++ lib/database.php
@@ -1,6 +1,16 @@
 <?
 mysql_connect($database['server'], $database['username'], $database['password']);
 mysql_select_db($database['database']);
-@mysql_query('SET CHARACTER SET utf8');
+if (mysql_query('SET CHARACTER SET utf8')) {
+	$database['utf8'] = true;
+	function mysql_lessen($str, $length = 255, $tail = '..') {
+		return UTF8::lessen($str, $length, $tail);
+	}
+} else {
+	$database['utf8'] = false;
+	function mysql_lessen($str, $length = 255, $tail = '..') {
+		return UTF8::lessenAsByte($str, $length, $tail);
+	}
+}
 @mysql_query('SET SESSION collation_connection = \'utf8_general_ci\'');
 ?>
