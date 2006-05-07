1f20f0f9dccfc628ff5fee6fdb71dcd1e29a8d1f
crizin <crizin@gmail.com>
2006-05-07 13:48:42 +0000

    #90 피드 목록의 왼쪽 + 기호를 누르면 해당 피드만 업데이트 되도록 수정

---
+++ blog/owner/reader/update/item.php
@@ -0,0 +1,8 @@
+<?
+define('ROOT', '../../../..');
+require ROOT . '/lib/include.php';
+if ($feed = fetchQueryRow("SELECT * FROM {$database['prefix']}Feeds WHERE id = {$suri['value']}"))
+	respondResultPage(updateFeed($feed));
+else
+	respondResultPage(99);
+?>
