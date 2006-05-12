25ebb0f729971f283ba9895f9222a4c47fbd1c00
inureyes <inureyes@gmail.com>
2006-05-12 04:59:51 +0000

    * Synchronize with latest trunk. (rev.161)  * 댓글 퍼머링크 관련 수정.   * 글 퍼머링크를 문자로 표시했을 경우 댓글의 퍼머링크 헤드도 문자로 표시되도록 함. (http://tattertools.com/ko/forum/viewtopic.php?id=362)

--- lib/view/view.php
+++ lib/view/view.php
@@ -348,7 +348,7 @@ function getTrackbacksView($entryId, & $skin) {
 }
 
 function getCommentView($entryId, & $skin) {
-	global $blogURL, $owner, $suri, $paging, $blog;
+	global $blogURL, $owner, $suri, $paging, $blog,$entry;
 	$authorized = doesHaveOwnership();
 	$skinValue = getSkinSetting($owner);
 	$blogSetting = getBlogSetting($owner);
@@ -388,7 +388,7 @@ function getCommentView($entryId, & $skin) {
 				dress($prefix1 . '_rep_name', fireEvent(($isComment ? 'ViewCommenter' : 'ViewGuestCommenter'), '<a href="' . htmlspecialchars(addProtocolSense($commentSubItem['homepage'])) . '" onclick="return openLinkInNewWindow(this)">' . htmlspecialchars($commentSubItem['name']) . '</a>', $commentSubItem), $commentSubItemView);
 			dress($prefix1 . '_rep_desc', fireEvent(($isComment ? 'ViewCommentContent' : 'ViewGuestCommentContent'), nl2br(addLinkSense(htmlspecialchars($commentSubItem['comment']), ' onclick="return openLinkInNewWindow(this)"')), $commentSubItem), $commentSubItemView);
 			dress($prefix1 . '_rep_date', Timestamp::format5($commentSubItem['written']), $commentSubItemView);
-			dress($prefix1 . '_rep_link',"$blogURL/{$entryId}#comment{$commentSubItem['id']}", $commentSubItemView);
+			dress($prefix1 . '_rep_link',"$blogURL/".($blog['useSlogan'] ?  "entry/".encodeURL($entry['slogan']) : $entryId)."#comment{$commentSubItem['id']}", $commentSubItemView);
 			dress($prefix1 . '_rep_onclick_delete', "deleteComment({$commentSubItem['id']});return false", $commentSubItemView);
 			$commentSubItemsView .= $commentSubItemView;
 		}
@@ -406,7 +406,7 @@ function getCommentView($entryId, & $skin) {
 		}
 		dress($prefix1 . '_rep_onclick_reply', $doubleCommentPermissionScript . "commentComment({$commentItem['id']});return false", $commentItemView);
 		dress($prefix1 . '_rep_onclick_delete', "deleteComment({$commentItem['id']});return false", $commentItemView);
-		dress($prefix1 . '_rep_link', "$blogURL/{$entryId}#comment{$commentItem['id']}", $commentItemView);
+		dress($prefix1 . '_rep_link',"$blogURL/".($blog['useSlogan'] ?  "entry/".encodeURL($entry['slogan']) : $entryId)."#comment{$commentItem['id']}", $commentItemView);
 		$commentItemsView .= $commentItemView;
 	}
 	dress($prefix1 . '_rep', $commentItemsView, $commentView);
--- script/gallery/iMazing/embed.php
+++ script/gallery/iMazing/embed.php
@@ -37,14 +37,14 @@ body {
 			iMazing<?=$_GET["d"]?>='';
 			iMazing<?=$_GET["d"]?>+='<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0" width="100%" height="100%">';
 			iMazing<?=$_GET["d"]?>+='<param name="movie" value="<?=$root?>/script/gallery/iMazing/main.swf"/>';
-			iMazing<?=$_GET["d"]?>+='<param name="FlashVars" value="image=<?=$imageStr?>&amp;frame=<?=$_GET["f"]?>&amp;transition=<?=$_GET["t"]?>&amp;navigation=<?=$_GET["n"]?>&amp;slideshowInterval=<?=$_GET["si"]?>&amp;page=<?=$_GET["p"]?>&amp;align=<?=$_GET["a"]?>&amp;skinPath=/script/gallery/iMazing/&amp;"/>';
+			iMazing<?=$_GET["d"]?>+='<param name="FlashVars" value="image=<?=$imageStr?>&amp;frame=<?=$_GET["f"]?>&amp;transition=<?=$_GET["t"]?>&amp;navigation=<?=$_GET["n"]?>&amp;slideshowInterval=<?=$_GET["si"]?>&amp;page=<?=$_GET["p"]?>&amp;align=<?=$_GET["a"]?>&amp;skinPath=<?=$root?>/script/gallery/iMazing/&amp;"/>';
 			iMazing<?=$_GET["d"]?>+='<param name="allowScriptAccess" value="sameDomain" />';
 			iMazing<?=$_GET["d"]?>+='<param name="menu" value="false" />';
 			iMazing<?=$_GET["d"]?>+='<param name="quality" value="high" />';
 			iMazing<?=$_GET["d"]?>+='<param name="bgcolor" value="#FFFFFF"/>';
 			iMazing<?=$_GET["d"]?>+='<!--[if !IE]> <-->';
 			iMazing<?=$_GET["d"]?>+='<object type="application/x-shockwave-flash" data="<?=$root?>/script/gallery/iMazing/main.swf" width="100%" height="100%">';
-			iMazing<?=$_GET["d"]?>+='<param name="FlashVars" value="image=<?=$imageStr?>&amp;frame=<?=$_GET["f"]?>&amp;transition=<?=$_GET["t"]?>&amp;navigation=<?=$_GET["n"]?>&amp;slideshowInterval=<?=$_GET["si"]?>&amp;page=<?=$_GET["p"]?>&amp;align=<?=$_GET["a"]?>&amp;skinPath=/script/gallery/iMazing/&amp;"/>';
+			iMazing<?=$_GET["d"]?>+='<param name="FlashVars" value="image=<?=$imageStr?>&amp;frame=<?=$_GET["f"]?>&amp;transition=<?=$_GET["t"]?>&amp;navigation=<?=$_GET["n"]?>&amp;slideshowInterval=<?=$_GET["si"]?>&amp;page=<?=$_GET["p"]?>&amp;align=<?=$_GET["a"]?>&amp;skinPath=<?=$root?>/script/gallery/iMazing/&amp;"/>';
 			iMazing<?=$_GET["d"]?>+='<param name="allowScriptAccess" value="sameDomain" />';
 			iMazing<?=$_GET["d"]?>+='<param name="menu" value="false" />';
 			iMazing<?=$_GET["d"]?>+='<param name="quality" value="high" />';
