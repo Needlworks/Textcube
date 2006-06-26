4b7f07745bc53ef6456ba3ccfaf7608dc5c78379
inureyes <inureyes@gmail.com>
2006-06-26 17:49:45 +0000

    * 트랙백 휴지통 삭제 후 댓글 휴지통으로 가는 오류 수정.

--- lib/piece/owner/contentMenu90.php
+++ lib/piece/owner/contentMenu90.php
@@ -13,7 +13,7 @@
 				<hr class="hidden" />
 				
 				<div id="psuedo-box">
-					<form method="post" action="<?=$blogURL?>/owner/trash/comment">
+					<form method="post" action="<?=$blogURL?>/owner/trash/trackback">
 						<input type="hidden" name="page" value="<?=$suri['page']?>" />
 						
 						<div id="data-outbox">
