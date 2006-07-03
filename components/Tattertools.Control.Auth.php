b9447d92dacd037c29fdfab84f4b767613e799c1
inureyes <inureyes@gmail.com>
2006-07-03 02:44:42 +0000

    * Synchronize with trunk. (r565)

--- components/Tattertools.Control.Auth.php
+++ components/Tattertools.Control.Auth.php
@@ -5,7 +5,7 @@ class Auth {
 		$loginid = mysql_escape_string($loginid);
 		$result = mysql_query("SELECT userid FROM {$database['prefix']}Users WHERE userid = $owner AND loginid = '$loginid' AND `password` = '" . md5($password) . "'");
 		if ($result && (mysql_num_rows($result) == 1)) {
-			mysql_query("UPDATE Users SET lastLogin = UNIX_TIMESTAMP() WHERE loginid = '$loginid'");
+			mysql_query("UPDATE {$database['prefix']}Users SET lastLogin = UNIX_TIMESTAMP() WHERE loginid = '$loginid'");
 			return true;
 		}
 		return false;
