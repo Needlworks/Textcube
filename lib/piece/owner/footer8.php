f3afd12b97675f27155105e892a85c9fd87830e7
inureyes <inureyes@gmail.com>
2006-07-21 19:09:36 +0000

    * 팝업 수정이 위지윅 에디터 모드에서 작동하지 않는 문제 수정.

--- lib/piece/owner/footer8.php
+++ lib/piece/owner/footer8.php
@@ -31,63 +31,6 @@
 					case "TEXTAREA":
 						return;
 				}
-				switch (oEvent.keyCode) {
-					case 81: //Q
-						window.location = "<?php echo $blogURL?>/";
-						break;
-					case 82: //R
-						window.location = "<?php echo $blogURL?>/owner/reader";
-						break;
-<?php 
-if (defined('__TATTERTOOLS_READER__')) {
-?>
-					case 65: //A
-					case 72: //H
-						Reader.prevEntry();
-						break;
-					case 83: //S
-					case 76: //L
-						Reader.nextEntry();
-						break;
-					case 68: //D
-						Reader.openEntryInNewWindow();
-						break;
-					case 70: //F
-						Reader.showUnreadOnly();
-						break;
-					case 71: //G
-						Reader.showStarredOnly();
-						break;
-					case 84: //T
-						Reader.updateAllFeeds();
-						break;
-					case 87: //W
-						Reader.toggleStarred();
-						break;
-					case 74: //J
-						window.scrollBy(0, 100);
-						break;
-					case 75: //K
-						window.scrollBy(0, -100);
-						break;
-<?php 
-}
-if (isset($paging['prev'])) {
-?>
-					case 65: //A
-						window.location = "<?php echo "{$paging['url']}{$paging['prefix']}{$paging['prev']}{$paging['postfix']}"?>";
-						break;
-<?php 
-}
-if (isset($paging['next'])) {
-?>
-					case 83: //S
-						window.location = "<?php echo "{$paging['url']}{$paging['prefix']}{$paging['next']}{$paging['postfix']}"?>";
-						break;
-<?php 
-}
-?>
-				}
 			}
 		//]]>
 	</script>
