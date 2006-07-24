e399a1e6aa3cc3d9297c8a48390d5d8a2a82a68d
nani <textcube@needlworks.org>
2006-07-24 06:43:04 +0000

    * lib/view/html.php 인코딩에 슬래시(/)가 빠진 것 추가

--- lib/view/html.php
+++ lib/view/html.php
@@ -5,7 +5,7 @@ function printSimpleHtmlHeader($title) {
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ko">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <title><?php echo $title?></title>
 </head>
 <body>
