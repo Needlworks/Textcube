698356eb2e27878052c44bc045a985fb491deda4
peris <textcube@needlworks.org>
2006-07-24 14:59:53 +0000

    * 센터 플러그인(scope=admin) 최소한의 동작을 위한 커밋
    AD_RecentPS_Default 플러그인을 사용으로 변경하면 센터에 최근 글이 나타남
    
    (기존의 tag element와 어떤 구분이 필요한지 아직 파악 못하여 그냥 tag element사용)

--- blog/owner/center/dashboard/index.php
+++ blog/owner/center/dashboard/index.php
@@ -3,12 +3,21 @@ define('ROOT', '../../../..');
 require ROOT . '/lib/includeForOwner.php';
 require ROOT . '/lib/piece/owner/headerA.php';
 require ROOT . '/lib/piece/owner/contentMenuA0.php';
+
+ob_start();
 ?>
 						<form method="post" action="<?=$blogURL?>/owner/center/dashboard">
 							<div id="part-center-dashboard" class="part">
 								<h2 class="caption"><span class="main-text"><?php echo _t('태터툴즈에 대한 여러가지 정보를 봅니다')?></span></h2>
 							</div>
 						</form>
+						<div>[##_AD_RecentPS_Default_##]</div>
 <?
+$contents = ob_get_contents();
+ob_end_clean();
+
+handleTags($contents);
+echo removeAllTags($contents);
+
 require ROOT . '/lib/piece/owner/footer1.php';
 ?>
