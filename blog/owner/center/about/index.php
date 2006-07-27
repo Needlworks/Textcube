5e7365d8ed1a46b9e2e952ced4c8aa86bd9ead42
inureyes <inureyes@gmail.com>
2006-07-27 08:14:29 +0000

    * center / about란 업데이트

--- blog/owner/center/about/index.php
+++ blog/owner/center/about/index.php
@@ -8,8 +8,135 @@ require ROOT . '/lib/piece/owner/contentMenuA2.php';
 							<div id="part-center-about" class="part">
 								<h2 class="caption"><span class="main-text"><?php echo  _t('태터툴즈')?></span></h2>
 							</div>
-					        <?php echo  TATTERTOOLS_NAME?> : 	
-					        <?php echo  TATTERTOOLS_VERSION?>
+							<h3>Brand yourself! : <?php echo  TATTERTOOLS_NAME?><?php echo  TATTERTOOLS_VERSION?></h3>
+							<p><?php echo _t('ⓒ2004 - 2006. 모든 저작권은 개발자 및 공헌자에게 있습니다. 태터툴즈는 태터앤컴퍼니와 태터앤프렌즈에서 개발합니다. 태터툴즈와 태터툴즈 로고는 태터앤컴퍼니의 상표입니다.')?> </p>
+							<div id="part-description">
+								<h3><?php echo _t('개발자')?></h3>
+
+								<h4><?php echo _t('Maintainer')?></h4>
+								<table>
+								<thead>
+									<tr>
+									<td></td>
+									</tr>
+								</thead>
+								<tbody>
+									<tr><td>gendoh</td></tr>
+									<tr><td>inureyes</td></tr>
+									<tr><td>papacha</td></tr>
+								</tbody>
+								</table>
+
+								<h4><?php echo _t('Developer')?></h4>
+								<table>
+								<thead>
+									<tr>
+										<td></td>
+										<td></td>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>coolengineer</td>
+										<td>XML-RPC API interface<td>
+									</tr>
+									<tr>
+										<td>crizin</td>
+										<td>reader / editor</td>
+									</tr>
+									<tr>
+										<td>egoing</td>
+										<td>imazing / owner function</td>
+									</tr>
+									<tr>
+										<td>gendoh</td>
+										<td>security / EAS / quality assurance</td>
+									</tr>
+									<tr>
+										<td>ghost_ghost</td>
+										<td>plugin setting / xml schema</td>
+									</tr>
+									<tr>
+										<td>graphittie</td>
+										<td>UI / xhtml specification / documentation</td>
+									</tr>
+									<tr>
+										<td>inureyes</td>
+										<td>function addition / bug tracking / DB management</td>
+									</tr>
+									<tr>
+										<td>leezche</td>
+										<td>skin</td>
+									</tr>
+									<tr>
+										<td>nani</td>
+										<td>skin</td>
+									</tr>
+									<tr>
+										<td>papacha</td>
+										<td>library / component / quality assurance</td>
+									</tr>
+									<tr>
+										<td>peris</td>
+										<td>plugin architecture</td>
+									</tr>
+								</tbody>
+								</table>
+
+								<h4><?php echo _t('Internationalization')?></h4>
+								<table>
+								<thead>
+									<tr>
+										<td></td>
+										<td></td>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>Louice P.</td>
+										<td>Japanese</td>
+									</tr>
+									<tr>
+										<td>Hanul</td>
+										<td>Japanese</td>
+									</tr>
+									<tr>
+										<td>KIM</td>
+										<td>Chinese</td>
+									</tr>
+								</tbody>
+								</table>
+								
+								<h4><?php echo _t('Support manager')?></h4>
+								<table>
+								<thead>
+									<tr>
+										<td></td>
+										<td></td>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>J.Parker</td>
+										<td>plugin manager</td>
+									</tr>
+									<tr>
+										<td>nani</td>
+										<td>skin manager</td>
+									</tr>
+									<tr>
+										<td>LonnieNa</td>
+										<td>manual manager</td>
+									</tr>
+								</tbody>
+								</table>
+								<h3><?php echo _t('공헌자')?></h3>
+								<h4><?php echo _t('Code Contributor')?></h4>
+								<p>랜덤여신, McFuture</p>
+
+								<h4><?php echo _t('Reporter')?></h4>
+								<p>마모루, 건더기, 유마, 섭이, JCrew, cirrus, 작은인장, 김종찬, 김정훈, BLue, 소필, webthink, 일모리, lunamoth, 빌리디안, iamtiz, rooine, baragi74, soonJin, Juno, 딘제, iarchitect, Rukxer, gofeel, Ever_K, BlueOcean, thessando, advck1123, danew, 엉뚱이, 마잇, 하노아, Naive, mintstate, 바둥이, expansor, 싸이친구, rhapsody, 제주시티, funny4u</p>
+							</div>
 						</form>
 <?php
 require ROOT . '/lib/piece/owner/footer1.php';
