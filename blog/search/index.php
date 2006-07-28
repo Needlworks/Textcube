b9bba4425b9400d3c8d61465034565612ec9951a
inureyes <inureyes@gmail.com>
2006-07-28 12:35:52 +0000

    * Synchronize with trunk. (r767)

--- blog/search/index.php
+++ blog/search/index.php
@@ -6,6 +6,9 @@ $search = $suri['value'];
 if (!empty($search) && !empty($suri['page'])) {
 	$list = array('title' => $search, 'items' => getEntryListBySearch($owner, $search));
 	$commentList = getCommentList($owner, $search);
+} else {  
+	$list = array('title' => '', 'items' => array());  
+	$commentList = array('title' => '', 'items' => array());  
 }
 list($entries, $paging) = getEntriesWithPagingBySearch($owner, $search, $suri['page'], $blog['entriesOnPage']);
 require ROOT . '/lib/piece/blog/begin.php';
