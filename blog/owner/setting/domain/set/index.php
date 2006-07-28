992c8513d3ad8e5be036061e5bf0b8b2d7ea6d0f
crizin <crizin@gmail.com>
2006-07-28 07:44:29 +0000

    * 누락된 파일 추가 (tistory @2307)

---
+++ blog/owner/setting/domain/set/index.php
@@ -0,0 +1,16 @@
+<?
+define('ROOT', '../../../../..');
+require ROOT . '/lib/includeForOwner.php';
+requireStrictRoute();
+
+if(empty($_POST['secondaryDomain']) && $_POST['defaultDomain'] == 1)
+	respondResultPage(4);
+else if(!setPrimaryDomain($owner, $_POST['primaryDomain']))
+	respondResultPage(2);
+else if(!setSecondaryDomain($owner, $_POST['secondaryDomain']))
+	respondResultPage(3);
+else if(!setDefaultDomain($owner, $_POST['defaultDomain']))
+	respondResultPage(1);
+else
+	respondResultPage(0);
+?>
