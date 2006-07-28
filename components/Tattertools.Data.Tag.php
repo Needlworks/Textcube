64c580ffc7438acba16efd7a8d23f5861c44e043
gendoh <gendoh@gmail.com>
2006-07-28 09:01:03 +0000

    (tistory @2360) Tag 관련 수정

--- components/Tattertools.Data.Tag.php
+++ components/Tattertools.Data.Tag.php
@@ -1,37 +1,119 @@
 <?
 class Tag {
 	/*@static@*/
-	function getId($tag, $add = false) {
-		global $database, $owner;
-
-		$tag = Tag::make($tag);
-		if (empty($tag))
-			return null;
-
-		if ($result = mysql_query("SELECT id FROM {$database['prefix']}Tags WHERE name = '" . mysql_escape_string($tag) . "'")) {
-			if ($row = mysql_fetch_row($result)) {
-				mysql_free_result($result);
-				return $row[0];
-			}
-			mysql_free_result($result);
+	function addTagsWithEntryId($owner, $entry, /*string array*/$taglist)
+	{
+		global $database;
+		if ($taglist == null)
+			return;
+			
+		$taglist = array_filter($taglist, 'removeEmptyTagHelper');
+			
+		foreach($taglist as &$tag) {
+			$tag = mysql_real_escape_string(trim($tag));
 		}
-		if ($add) {
-			if ($result = mysql_query("INSERT INTO {$database['prefix']}Tags VALUES(NULL,'" . mysql_escape_string($tag) . "')"))
-				return mysql_insert_id();
-		}
-		return null;
+
+		// step 1. Insert Tags
+		$tagliststr = '(\'' . implode('\') , (\'', $taglist) . '\')';
+		DBQuery::execute("INSERT IGNORE INTO {$database['prefix']}Tags (name) VALUES $tagliststr ");
+
+		// the point of Race condition
+		// if other entry is deleted, some missing tags can be exist so they are not related with this entry.
+		
+		// step 2. Insert Relations
+		$tagliststr =  '\'' . implode('\' , \'', $taglist) . '\'';
+		DBQuery::execute("INSERT INTO {$database['prefix']}TagRelations
+								(SELECT $owner, t.id, $entry FROM tt_Tags as t 
+										WHERE 
+											name in ( $tagliststr ) AND  
+											t.id NOT IN 
+												( SELECT tag FROM tt_TagRelations WHERE 
+													(tag = t.id) AND (entry = $entry) AND (owner = $owner)
+												)
+								)");
 	}
-	
+
 	/*@static@*/
-	function make($tag) {
-		$tag = trim($tag);
-		$tag = str_replace('&quot;', '"', $tag);
-		$tag = str_replace('&#39;', '\'', $tag);
-		$tag = preg_replace('/ +/', ' ', $tag);
-		$tag = preg_replace('/[\x00-\x1f]|[\x7f]/', '', $tag);
-		$tag = preg_replace('/^(-|\s)+/', '', $tag);
-		$tag = preg_replace('/(-|\s)+$/', '', $tag);
-		return $tag;
+	function modifyTagsWithEntryId($owner, $entry, /*string array*/$taglist)
+	{
+		global $database;
+		if ($taglist == null)
+			$taglist = array();
+			
+		$taglist = array_filter($taglist, 'removeEmptyTagHelper');
+		
+		foreach($taglist as &$tag) {
+			$tag = mysql_real_escape_string(trim($tag));
+		}
+		
+		// step 1. Get deleted Tag
+		$oldtaglist = DBQuery::queryColumn("SELECT name FROM {$database['prefix']}Tags WHERE EXISTS
+								(SELECT * FROM {$database['prefix']}TagRelations WHERE owner = $owner AND entry = $entry AND tag = id)");
+		if ($oldtaglist == null)
+			$oldtaglist = array();
+		foreach($oldtaglist as &$tag) {
+			$tag = mysql_real_escape_string(trim($tag));
+		}
+		
+		$deletedTagList = array_diff($oldtaglist, $taglist);
+		$insertedTagList = array_diff($taglist, $oldtaglist);
+		
+		// step 2. Insert Tag
+		if (count($insertedTagList) > 0) 
+		{
+			$tagliststr = '(\'' . implode('\') , (\'', $insertedTagList) . '\')';
+			DBQuery::execute("INSERT IGNORE INTO {$database['prefix']}Tags (name) VALUES $tagliststr ");
+		
+		// step 3. Insert Relation
+			$tagliststr =  '\'' . implode('\' , \'', $insertedTagList) . '\'';
+			DBQuery::execute("INSERT INTO {$database['prefix']}TagRelations
+									(SELECT $owner, t.id, $entry FROM tt_Tags as t 
+											WHERE 
+												name in ( $tagliststr ) AND  
+												t.id NOT IN 
+													( SELECT tag FROM tt_TagRelations WHERE 
+														(tag = t.id) AND (entry = $entry) AND (owner = $owner)
+													)
+									)");
+		}
+		
+		// step 4. Delete Tag
+		if (count($deletedTagList) > 0)
+		{
+			// small step, get tag id list
+			$tagliststr =  '\'' . implode('\' , \'', $deletedTagList) . '\'';
+			$taglist = DBQuery::queryColumn(
+					"SELECT id FROM {$database['prefix']}Tags WHERE EXISTS (SELECT * FROM {$database['prefix']}TagRelations 
+							WHERE owner = $owner AND entry = $entry AND tag = id)
+								AND name in ( $tagliststr )");
+			if ($taglist == null) 
+				return; // What?
+			
+			// now delete tag
+			$tagliststr = implode(',', $taglist);
+			DBQuery::execute("DELETE FROM {$database['prefix']}Tags WHERE id in ( $tagliststr ) AND NOT EXISTS (SELECT * FROM {$database['prefix']}TagRelations WHERE (tag = id) AND ((entry <> $entry) OR (owner <> $owner)))");
+		
+		// step 5. Delete Relation
+			DBQuery::execute("DELETE FROM {$database['prefix']}TagRelations WHERE owner = $owner AND entry = $entry");
+		
+		// step 6. Delete Tag one more time
+			DBQuery::execute("DELETE FROM {$database['prefix']}Tags WHERE id in ( $tagliststr ) AND NOT EXISTS (SELECT * FROM {$database['prefix']}TagRelations WHERE (tag = id))");
+
+		}
 	}
+
+	/*@static@*/
+	function deleteTagsWithEntryId($owner, $entry)
+	{
+		global $database;
+		$taglist = DBQuery::queryColumn("SELECT tag FROM {$database['prefix']}TagRelations WHERE owner = $owner AND entry = $entry");
+		if ($taglist != null) {
+			$tagliststr = implode(',', $taglist);
+			
+			DBQuery::execute("DELETE FROM {$database['prefix']}Tags WHERE id in ( $tagliststr ) AND NOT EXISTS (SELECT * FROM {$database['prefix']}TagRelations WHERE (tag = id) AND ((entry <> $entry) OR (owner <> $owner)))");
+			DBQuery::execute("DELETE FROM {$database['prefix']}TagRelations WHERE owner = $owner AND entry = $entry");
+			DBQuery::execute("DELETE FROM {$database['prefix']}Tags WHERE id in ( $tagliststr ) AND NOT EXISTS (SELECT * FROM {$database['prefix']}TagRelations WHERE (tag = id))");
+		}
+	}	
 }
 ?>
