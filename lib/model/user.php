bc34c391ba14cfe6983d75cdad8921ea4103ba59
gendoh <gendoh@gmail.com>
2006-07-28 08:21:04 +0000

    (tistory @2335) $user['loginid'] 제거

--- lib/model/user.php
+++ lib/model/user.php
@@ -1,9 +1,10 @@
 <?
 if (doesHaveMembership()) {
 	$user = array('id' => getUserId());
-	list($user['loginid'], $user['name']) = fetchQueryRow("select loginid, name from {$database['prefix']}Users where userid = {$user['id']}");
+	list($user['name']) = fetchQueryRow("select name from {$database['prefix']}Users where userid = {$user['id']}");
 	list($user['blog'], $user['timezone']) = fetchQueryRow("select name, timezone from {$database['prefix']}BlogSettings where owner = {$user['id']}");
 	$user['homepage'] = getBlogURL($user['blog']);
-} else
+} else {
 	$user = null;
+}
 ?>
