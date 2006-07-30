fa42741d9163eb2bcb75630d2ad1812f194e2f16
graphittie <graphittie@gmail.com>
2006-07-30 10:20:07 +0000

    * 관리자 화면의 base.js 파일을 지정하는 자바스크립트 제거. 삭제된 파일임. http://forum.tattertools.com/ko/viewtopic.php?pid=6751#p6751

--- lib/piece/owner/header4.php
+++ lib/piece/owner/header4.php
@@ -21,7 +21,6 @@
 	<script type="text/javascript" src="<?php echo $service['path']?>/script/common.js"></script>
 	<script type="text/javascript" src="<?php echo $service['path']?>/script/gallery.js"></script>
 	<script type="text/javascript" src="<?php echo $service['path']?>/script/owner.js"></script>
-	<script type="text/javascript" src="<?php echo $service['path']?>/style/base.js"></script>
 	<script type="text/javascript" src="<?php echo $service['path'].$adminSkinSetting['skin']?>/custom.js"></script>
 </head>
 <body id="body-statistics">
