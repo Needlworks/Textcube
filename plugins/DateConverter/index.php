57b6948c61d1bc7806fb81656d6edf9f4f84c69d
graphittie <graphittie@gmail.com>
2006-07-30 16:56:42 +0000

    * 날짜 형식 변경 플러그인. setLocale()의 문제로 인해 strftime() 함수사용을 배제. 이에 따라 language.php 파일이 추가됨. 환경설정 부분은 아직 미구현.

--- plugins/DateConverter/index.php
+++ plugins/DateConverter/index.php
@@ -1,212 +1,114 @@
 <?php
+// 언어 템플릿 *******************************************************************************************************
+	// %Y = 4자리수 연도 [0000]
+	// %y = 2자리수 연도 [00]
+	
+	// %M = 앞의 0을 표시하는 달. [01~12] or January/Febuary/March/April...
+	// %m = 앞의 0을 표시하지 않는 달. [1~12] or Jan/Feb/Mar/Apr...
+	
+	// %D = 앞의 0을 표시하는 일(日). [01~31]
+	// %d = 앞의 0을 표시하지 않는 일(日). [1~31]
+	// %o = 서수 어미가 붙은 일(日). 주로 라틴 문자권에서 사용. [1~31] + [day ordinal postfix]
+	
+	// %H = 앞의 0을 표시하는 24시간. [00~24]
+	// %h = 앞의 0을 표시하지 않는 24시간. [0~24]
+	// %F = 앞의 0을 표시하는 12시간. [00~12]
+	// %f = 앞의 0을 표시하지 않는 12시간. [0~12]
+	
+	// %I = 앞의 0을 표시하는 분. [00~59]
+	// %i = 앞의 0을 표시하지 않는 분. [0~59]
+	
+	// %S = 앞의 0을 표시하는 초. [00~59]
+	// %s = 앞의 0을 표시하지 않는 초. [0~59]
+
 function convertDateFormat($argTarget, $argType) {
-	/*
-		$rgDateFormat['archive date'] = array()
-			① string	- 언어 : default/english/korean.
-			② integer	- 달(month)의 문자열 길이. '0'이면 문자열을 자르지 않고 전부 보여준다.
-			③ integer	- 요일(day)의 문자열 길이. '0'이면 문자열을 자르지 않고 전부 보여준다.
-			④ boolean	- 숫자에 자리수를 맞출 것인지 여부. 맞추면 true, 안 맞추면 false. 예) 06월, 09일.
-	*/
+	global $pluginURL;
+	
+	$temp = explode('/', $pluginURL);
+	array_shift($temp);
+	array_shift($temp);
+	
+	include ROOT.'/'.implode('/', $temp)."/language.php";
+	
 	$rgDateFormat = array();
-	$rgDateFormat['archive date'] = array("language" => "english", "format" => "%M %Y");											// 저장소 날짜.
+	$rgDateFormat['archive date'] = array("language" => "english", "format" => "%Y|%M");											// 저장소 날짜.
 	$rgDateFormat['calendar head'] = array("language" => "english", "format" => "%m %Y");											// 달력 머릿날짜.
 	$rgDateFormat['comment date'] = array("language" => "japanese", "format" => "%Y年 %M月{ %D日}{ %H時}{ %I分}");					// 댓글 날짜.
 	$rgDateFormat['comment list date'] = array("language" => "korean", "format" => "%Y년 %M월{ %D일}");								// 댓글 목록 날짜.
 	$rgDateFormat['guestbook comment date'] = array("language" => "korean", "format" => "%Y년 %M월{ %D일}{ %H시}{ %I분}");			// 방명록 날짜.
 	$rgDateFormat['list date'] = array("language" => "korean", "format" => "%Y년 %M월{ %D일}");										// 목록 날짜.
 	$rgDateFormat['notice date'] = array("language" => "korean", "format" => "%Y년 %M월{ %D일}{ %H시}{ %I분}{ %S초}");				// 공지 날짜.
-	$rgDateFormat['post date'] = array("language" => "english", "format" => "{%o }%M %Y{ %H}{:%I}{:%S}");							// 포스트 날짜.
+	$rgDateFormat['post date'] = array("language" => "english", "format" => "{%M}{ %o,} %Y{ %H}{:%I}{:%S}");						// 포스트 날짜.
+	$rgDateFormat['recent comment date'] = array("language" => "english", "format" => "{%M}{ %d,}{ %Y}{ %H}{:%I}{:%S}");			// 최근 댓글 날짜.
+	$rgDateFormat['recent trackback date'] = array("language" => "english", "format" => "{%o }%M");									// 최근 트랙백 날짜.
 	$rgDateFormat['trackback date'] = array("language" => "english", "format" => "%M{ %d,} %Y{ %H}{:%I}{:%S}");						// 트랙백 날짜.
-	$rgDateFormat['recent trackback date'] = array("language" => "english", "format" => "{%M}{ %d,} %Y{ %H}{:%I}{:%S}");			// 최근 트랙백 날짜.
-	$rgDateFormat['recent comment date'] = array("language" => "english", "format" => "{%M}{ %d,} %Y{ %H}{:%I}{:%S}");				// 최근 댓글 날짜.
-	
-	// 언어 템플릿 *******************************************************************************************************
-	// %Y = [0000]
-	// %M = [01~12] or January/Febuary/March/April...
-	// %D = [01~31]
-	// %y = [00]
-	// %m = [1~12] or Jan/Feb/Mar/Apr...
-	// %d = [1~31]
-	// %o = [1~31] + [day ordinal postfix]
-	// %H = [00~24]
-	// %I = [00~59]
-	// %S = [00~59]
-	// %F = [00~12]
-	// %h = [0~24]
-	// %i = [0~59]
-	// %s = [0~59]
-	// %f = [0~12]
-	$rgDateInformation = array();
-	
-	// - 영어.
-	$rgDateInformation['english']['day ordinal postfix'] = array(); // 날짜 뒤에 붙는 문자. 
-	$rgDateInformation['english']['day ordinal postfix']['00'] = NULL;
-	$rgDateInformation['english']['day ordinal postfix']['01'] = 'st';
-	$rgDateInformation['english']['day ordinal postfix']['02'] = 'nd';
-	$rgDateInformation['english']['day ordinal postfix']['03'] = 'rd';
-	$rgDateInformation['english']['day ordinal postfix']['04'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['05'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['06'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['07'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['08'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['09'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['10'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['11'] = 'st';
-	$rgDateInformation['english']['day ordinal postfix']['12'] = 'nd';
-	$rgDateInformation['english']['day ordinal postfix']['13'] = 'rd';
-	$rgDateInformation['english']['day ordinal postfix']['14'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['15'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['16'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['17'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['18'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['19'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['20'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['21'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['22'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['23'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['24'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['25'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['26'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['27'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['28'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['29'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['30'] = 'th';
-	$rgDateInformation['english']['day ordinal postfix']['31'] = 'st';
-
-	//$rgDateInformation['english']['month']['01'] = 'January';
-	//$rgDateInformation['english']['month']['02'] = 'Febuary';
-	//$rgDateInformation['english']['month']['03'] = 'March';
-	//$rgDateInformation['english']['month']['04'] = 'April';
-	//$rgDateInformation['english']['month']['05'] = 'May';
-	//$rgDateInformation['english']['month']['06'] = 'June';
-	//$rgDateInformation['english']['month']['07'] = 'July';
-	//$rgDateInformation['english']['month']['08'] = 'August';
-	//$rgDateInformation['english']['month']['09'] = 'September';
-	//$rgDateInformation['english']['month']['10'] = 'October';
-	//$rgDateInformation['english']['month']['11'] = 'November';
-	//$rgDateInformation['english']['month']['12'] = 'December';
-
-	//$rgDateInformation['english']['weekday'][0] = 'Sunday';
-	//$rgDateInformation['english']['weekday'][1] = 'Monday';
-	//$rgDateInformation['english']['weekday'][2] = 'Tuesday';
-	//$rgDateInformation['english']['weekday'][3] = 'Wednesday';
-	//$rgDateInformation['english']['weekday'][4] = 'Thursday';
-	//$rgDateInformation['english']['weekday'][5] = 'Friday';
-	//$rgDateInformation['english']['weekday'][6] = 'Saturday';
-	
 	
 	$strLanguage = $rgDateFormat[$argType]['language'];
 	
-	if (!empty($strLanguage) && $strLanguage != "default") {
-		list($strDateHead, $strDateTail) = split("( )+", $argTarget);
-		list($strYear, $strMonth, $strDay) = explode("/", $strDateHead);
-		if (!empty($strDateTail)) {
-			list($strHour, $strMinute, $strSecond) = explode("/", $strDateTail);
-		}
-		
-		$rgTimeFormat = split("(/|:| )", $argTarget);
-		
-		setlocale(LC_TIME, $strLanguage);
-		$strFlag = strftime("%p", strtotime($argTarget));
-		
-		// PHP 자체 내에 설정이 들어있는 언어인 경우.
-		if ($strLanguage == "english" || ($strFlag != "PM" && $strFlag != "AM")) {
-			$rgCustomIdentifier = array();
-			$rgCustomIdentifier['%Y'] = strftime("%Y", strtotime($argTarget));
-			if (eregi("^[0-9]{1,2}$", strftime("%b", strtotime($argTarget)), $rgTemp)) {
-				$rgCustomIdentifier['%M'] = strftime("%m", strtotime($argTarget));
-			} else {
-				$rgCustomIdentifier['%M'] = strftime("%B", strtotime($argTarget));
-			}
-			if (isset($rgTimeFormat[2]))
-				$rgCustomIdentifier['%D'] = strftime("%d", strtotime($argTarget));
-			
-			$rgCustomIdentifier['%y'] = strftime("%y", strtotime($argTarget));
-			$rgCustomIdentifier['%m'] = strftime("%b", strtotime($argTarget));
-			if (isset($rgTimeFormat[2]))
-				$rgCustomIdentifier['%d'] = intval(strftime("%d", strtotime($argTarget)));
-			
-			if (isset($rgTimeFormat[2])) {
-				if (isset($rgDateInformation[$strLanguage]['day ordinal postfix'][$rgCustomIdentifier['%D']])) {
-					$rgCustomIdentifier['%o'] = $rgCustomIdentifier['%d'].$rgDateInformation[$strLanguage]['day ordinal postfix'][$rgCustomIdentifier['%D']];
-				} else {
-					$rgCustomIdentifier['%o'] = $rgCustomIdentifier['%d'];
-				}
-			}
-			
-			if (isset($rgTimeFormat[3]))
-				$rgCustomIdentifier['%H'] = strftime("%H", strtotime($argTarget));
-			if (isset($rgTimeFormat[4]))
-				$rgCustomIdentifier['%I'] = strftime("%M", strtotime($argTarget));
-			if (isset($rgTimeFormat[5]))
-				$rgCustomIdentifier['%S'] = strftime("%S", strtotime($argTarget));
-			
-			if (isset($rgTimeFormat[3]))
-				$rgCustomIdentifier['%h'] = strftime("%I", strtotime($argTarget));
-			if (isset($rgTimeFormat[4]))
-				$rgCustomIdentifier['%i'] = intval(strftime("%M", strtotime($argTarget)));
-			if (isset($rgTimeFormat[5]))
-				$rgCustomIdentifier['%s'] = intval(strftime("%S", strtotime($argTarget)));
-			
-			if (isset($rgTimeFormat[3]))
-				$rgCustomIdentifier['%F'] = intval(strftime("%H", strtotime($argTarget)));
-			if (isset($rgTimeFormat[3]))
-				$rgCustomIdentifier['%f'] = intval(strftime("%I", strtotime($argTarget)));
-			
-			$newTarget = strtr($rgDateFormat[$argType]['format'], $rgCustomIdentifier);
-			$newTarget = eregi_replace("\{[^\{]*%[a-z][^\}]*\}", "", $newTarget);
-			$newTarget = eregi_replace("\{|\}", "", $newTarget);
-		// PHP에 설정이 들어있지 않은 언어의 경우, 자체 설정을 사용.
-		} else {
-			$strMonth = strftime("%m", strtotime($argTarget));
-			$strday = strftime("%d", strtotime($argTarget));
-			
-			$rgCustomIdentifier = array();
-			$rgCustomIdentifier['%Y'] = strftime("%Y", strtotime($argTarget));
-			$rgCustomIdentifier['%M'] = $rgDateInformation[$strLanguage]['month'][$strMonth];
-			if (isset($rgTimeFormat[2]))
-				$rgCustomIdentifier['%D'] = strftime("%d", strtotime($argTarget));
-			$rgCustomIdentifier['%y'] = strftime("%y", strtotime($argTarget));
-			$rgCustomIdentifier['%m'] = UTF8::lessen($rgDateInformation[$strLanguage]['month'][$strMonth], 3);
-			if (isset($rgTimeFormat[2]))
-				$rgCustomIdentifier['%d'] = intval(strftime("%d", strtotime($argTarget)));
-			
-			if (isset($rgTimeFormat[2])) {
-				if (isset($rgDateInformation[$strLanguage]['day ordinal postfix'][$rgCustomIdentifier['%D']])) {
-					$rgCustomIdentifier['%o'] = $rgCustomIdentifier['%d'].$rgDateInformation[$strLanguage]['day ordinal postfix'][$rgCustomIdentifier['%D']];
-				} else {
-					$rgCustomIdentifier['%o'] = $rgCustomIdentifier['%d'];
-				}
-			}
-						
-			if (isset($rgTimeFormat[3]))
-				$rgCustomIdentifier['%H'] = strftime("%H", strtotime($argTarget));
-			if (isset($rgTimeFormat[4]))
-				$rgCustomIdentifier['%I'] = strftime("%M", strtotime($argTarget));
-			if (isset($rgTimeFormat[5]))
-				$rgCustomIdentifier['%S'] = strftime("%S", strtotime($argTarget));
-			
-			if (isset($rgTimeFormat[3]))
-				$rgCustomIdentifier['%h'] = strftime("%I", strtotime($argTarget));
-			if (isset($rgTimeFormat[4]))
-				$rgCustomIdentifier['%i'] = intval(strftime("%M", strtotime($argTarget)));
-			if (isset($rgTimeFormat[5]))
-				$rgCustomIdentifier['%s'] = intval(strftime("%S", strtotime($argTarget)));
-			
-			if (isset($rgTimeFormat[3]))
-				$rgCustomIdentifier['%F'] = intval(strftime("%H", strtotime($argTarget)));
-			if (isset($rgTimeFormat[3]))
-				$rgCustomIdentifier['%f'] = intval(strftime("%I", strtotime($argTarget)));
-			
-			$newTarget = strtr($rgDateFormat[$argType]['format'], $rgCustomIdentifier);
-			$newTarget = eregi_replace("\{[^\{]*%[a-z][^\}]*\}", "", $newTarget);
-			$newTarget = eregi_replace("\{|\}", "", $newTarget);
-		}
-	} else {
-		$newTarget = $argTarget;
+	if (empty($strLanguage) || $strLanguage == "default") {
+		$strLanguage = "korean";
 	}
 	
-	return $newTarget;
+	eregi("^(([0-9]{4})/([0-9]{2})/?([0-9]{2})?)? ?(([0-9]{2}):([0-9]{2}):?([0-9]{2})?)?$", $argTarget, $rgTemp);
+	$strYear = $rgTemp[2];
+	$strMonth = $rgTemp[3];
+	$strDay = $rgTemp[4];
+	$strHour = $rgTemp[6];
+	$strMinute = $rgTemp[7];
+	$strSecond = $rgTemp[9];
+	
+	$rgCustomIdentifier = array();
+	if ($strYear != false) {
+		$rgCustomIdentifier['%Y'] = $strYear;
+		$rgCustomIdentifier['%y'] = eregi_replace("^[0-9]{2}", "", $strYear);
+	}
+	if ($strMonth != false) {
+		if (isset($rgDateInformation[$strLanguage]['month'][$strMonth])) {
+			$rgCustomIdentifier['%M'] = $rgDateInformation[$strLanguage]['month'][$strMonth];
+			if (isset($rgDateInformation[$strLanguage]['month']['length'])) {
+				$rgCustomIdentifier['%m'] = eregi_replace("^(.{".$rgDateInformation[$strLanguage]['month']['length']."})(.*)$", "\\1", $rgDateInformation[$strLanguage]['month'][$strMonth]);
+			} else {
+				$rgCustomIdentifier['%m'] = $rgCustomIdentifier['%M'];
+			}
+		} else {
+			$rgCustomIdentifier['%M'] = $strMonth;
+			$rgCustomIdentifier['%m'] = eregi_replace("^[0-9]{2}", "", $strYear);
+		}
+	}
+	if ($strDay != false) {
+		$rgCustomIdentifier['%D'] = $strDay;
+		$rgCustomIdentifier['%d'] = eregi_replace("^[0-9]{2}", "", $strYear);
+		if (isset($rgDateInformation[$strLanguage]['day ordinal postfix']['00'])) {
+			$rgCustomIdentifier['%o'] = $rgCustomIdentifier['%d'].$rgDateInformation[$strLanguage]['day ordinal postfix']['00'];
+		} else if (isset($rgDateInformation[$strLanguage]['day ordinal postfix'][$strDay])) {
+			$rgCustomIdentifier['%o'] = $rgCustomIdentifier['%d'].$rgDateInformation[$strLanguage]['day ordinal postfix'][$strDay];
+		} else {
+			$rgCustomIdentifier['%o'] = $rgCustomIdentifier['%d'];
+		}
+	}
+	if ($strHour != false) {
+		$rgCustomIdentifier['%H'] = $strHour;
+		$rgCustomIdentifier['%h'] = eregi_replace("^0", "", $strHour);
+		$rgCustomIdentifier['%f'] = ($rgCustomIdentifier['%h'] > 12) ? $rgCustomIdentifier['%h'] - 12 : $rgCustomIdentifier['%h'];
+		$rgCustomIdentifier['%F'] = ($rgCustomIdentifier['%f'] < 10) ? '0'.$rgCustomIdentifier['%f'] : $rgCustomIdentifier['%f'];
+	}
+	if ($strMinute != false) {
+		$rgCustomIdentifier['%I'] = $strMinute;
+		$rgCustomIdentifier['%i'] = eregi_replace("^0", "", $strMinute);
+	}
+	if ($strSecond != false) {
+		$rgCustomIdentifier['%S'] = $strSecond;
+		$rgCustomIdentifier['%s'] = eregi_replace("^0", "", $strSecond);
+	}
+	
+	$newTarget = strtr($rgDateFormat[$argType]['format'], $rgCustomIdentifier);
+	$newTarget = eregi_replace("\{[^\{]*%[a-z][^\}]*\}", "", $newTarget);
+	
+	return trim(eregi_replace("\{|\}", "", $newTarget));
+}
+
+function removeHeadZero($number) {
+	return eregi_replace("^0", "", $number);
 }
 
 function convertDateLangLD($argTarget, $argMother) {
