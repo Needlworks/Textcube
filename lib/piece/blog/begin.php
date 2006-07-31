8909b42e960f9056f72782854642c6ce24ad4382
graphittie <graphittie@gmail.com>
2006-07-31 16:09:12 +0000

    #122 : Body id support.  * 태터툴즈 기본 body id를 좀 더 복잡한 형식으로 수정. 중복 방지를 위함.

--- lib/piece/blog/begin.php
+++ lib/piece/blog/begin.php
@@ -9,16 +9,16 @@ $skin = new Skin($skinSetting['skin']);
 $view = str_replace('[##_t3_##]', getUpperView(isset($paging) ? $paging : null) . $skin->skin . getLowerView() . getScriptsOnFoot(), $skin->outter);
 
 if ($suri['directive'] == '/') {
-	dress('body_id',"page",$view);
+	dress('body_id',"tt-body-page",$view);
 } else if (!empty($category)) {
-	dress('body_id',getCategoryBodyIdById($owner,$category) ? getCategoryBodyIdById($owner,$category) : 'category',$view);
+	dress('body_id',getCategoryBodyIdById($owner,$category) ? getCategoryBodyIdById($owner,$category) : 'tt-body-category',$view);
 } else if (!empty($search)) {
-	dress('body_id',"search",$view);
+	dress('body_id',"tt-body-search",$view);
 } else if (!empty($period)) {
-	dress('body_id',"archive",$view);
+	dress('body_id',"tt-body-archive",$view);
 //} else if (isset($list)) {
 //	dress('body_id',$suri['value'],$view);
 } else {
-	dress('body_id',ltrim($suri['directive'],'/'),$view);
+	dress('body_id',"tt-body-".ltrim($suri['directive'],'/'),$view);
 }
 ?>
