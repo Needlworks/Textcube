99c96b957cc6496290778182e900e633208db185
gendoh <gendoh@gmail.com>
2006-08-02 04:56:03 +0000

    Tag 가져오기 관련 수정 SiteTag에서 너무 많은 태그 존재시 문제가 생겨 2000개로 한정

--- lib/model/tag.php
+++ lib/model/tag.php
@@ -9,7 +9,7 @@ function getTagId($owner, $name) {
 function getTags($entry) {
 	global $database, $owner;
 	$tags = array();
-	$result = mysql_query("select * from {$database['prefix']}Tags, {$database['prefix']}TagRelations where id = tag and entry = $entry and owner = $owner group by name order by name");
+	$result = mysql_query("select * from {$database['prefix']}Tags, {$database['prefix']}TagRelations where id = tag and entry = $entry and owner = $owner group by tag order by name");
 	if ($result) {
 		while ($tag = mysql_fetch_array($result))
 			array_push($tags, $tag);
@@ -21,12 +21,12 @@ function getRandomTags($owner) {
 	global $database, $skinSetting;
 	$tags = array();
 	$aux = ($skinSetting['tagsOnTagbox'] == - 1) ? '' : "limit {$skinSetting['tagsOnTagbox']}";
-	if ($skinSetting['tagboxAlign'] == 1)
-		$result = mysql_query("select name, count(*) cnt from {$database['prefix']}Tags, {$database['prefix']}TagRelations where id = tag and owner = $owner GROUP BY name ORDER BY cnt DESC $aux");
-	else if ($skinSetting['tagboxAlign'] == 2)
-		$result = mysql_query("select distinct name from {$database['prefix']}Tags, {$database['prefix']}TagRelations where id = tag and owner = $owner ORDER BY name $aux");
-	else
-		$result = mysql_query("select name from {$database['prefix']}Tags, {$database['prefix']}TagRelations where id = tag and owner = $owner GROUP BY name ORDER BY RAND() $aux");
+	if ($skinSetting['tagboxAlign'] == 1) // order by count
+		$result = mysql_query("select name, count(*) cnt from {$database['prefix']}Tags, {$database['prefix']}TagRelations where id = tag and owner = $owner GROUP BY tag ORDER BY cnt DESC $aux");
+	else if ($skinSetting['tagboxAlign'] == 2) // order by name
+		$result = mysql_query("select distinct name from {$database['prefix']}Tags, {$database['prefix']}TagRelations where id = tag and owner = $owner GROUP BY tag ORDER BY name $aux");
+	else // random
+		$result = mysql_query("select name from {$database['prefix']}Tags, {$database['prefix']}TagRelations where id = tag and owner = $owner GROUP BY tag ORDER BY RAND() $aux");
 	if ($result) {
 		while (list($tag) = mysql_fetch_row($result))
 			array_push($tags, $tag);
@@ -37,7 +37,7 @@ function getRandomTags($owner) {
 function getSiteTags($owner) {
 	global $database;
 	$names = array();
-	$result = mysql_query("select name from {$database['prefix']}Tags t, {$database['prefix']}TagRelations r where t.id = r.tag and r.owner = $owner group by name order by name");
+	$result = mysql_query("select name from {$database['prefix']}Tags t, {$database['prefix']}TagRelations r where t.id = r.tag and r.owner = $owner GROUP BY tag order by name LIMIT 2000"); // TODO: Need paging SiteTags
 	if ($result) {
 		while (list($name) = mysql_fetch_array($result))
 			array_push($names, $name);
@@ -59,6 +59,7 @@ function getTagFrequencyRange() {
 			$min = $count;
 	}
 	return array($max, $min);
+	
 }
 
 function getTagFrequency($tag, $max, $min) {
@@ -80,7 +81,7 @@ function getTagFrequency($tag, $max, $min) {
 function suggestLocalTags($owner, $filter) {
 	global $database;
 	$tags = array();
-	$result = mysql_query("select distinct name, count(*) cnt from {$database['prefix']}Tags, {$database['prefix']}TagRelations where id = tag and owner = $owner and $filter group by name order by cnt desc limit 10");
+	$result = mysql_query("select distinct name, count(*) cnt from {$database['prefix']}Tags, {$database['prefix']}TagRelations where id = tag and owner = $owner and $filter group by tag order by cnt desc limit 10");
 	if ($result) {
 		while ($tag = mysql_fetch_array($result))
 			array_push($tags, $tag[0]);
