4106c41becddedc8cceffcf38b32b760fc37c90b
inureyes <inureyes@gmail.com>
2006-08-08 10:43:41 +0000

    * 자잘한 네이밍 실수 수정

--- blog/owner/setting/plugins/currentSetting/index.php
+++ blog/owner/setting/plugins/currentSetting/index.php
@@ -24,7 +24,7 @@ function saveConfig(plugin){
 	var xmlcon= new Converter(document, fiednamelist) ; 
     	var xmlData = encodeURIComponent(xmlcon.getXMLData());
 //      alert( xmlcon.getXMLData());	
-	var request = new HTTPRequest("POST" , "<?php echo $blogURL;?>/owner/setting/plugins/recieveConfig");
+	var request = new HTTPRequest("POST" , "<?php echo $blogURL;?>/owner/setting/plugins/receiveConfig");
 	PM.addRequest(request, "<?php echo _t('설정을 저장중 입니다.');?>");
 	request.onSuccess = function () {
 		PM.removeRequest(this);
