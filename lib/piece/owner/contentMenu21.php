0977efe5a7ef8915f255bb7ded495841da23a1f8
inureyes <inureyes@gmail.com>
2006-08-13 17:54:41 +0000

    * 관리자 화면 재배열을 위한 작업

--- lib/piece/owner/contentMenu21.php
+++ lib/piece/owner/contentMenu21.php
@@ -3,8 +3,8 @@
 				
 				<div id="sub-menu-box">
 					<ul id="sub-menu">
-						<li id="sub-menu-add" class="selected"><a href="<?php echo $blogURL;?>/owner/link/add"><span class="text"><?php echo _t('링크추가');?></span></a></li>
-						<li id="sub-menu-list"><a href="<?php echo $blogURL;?>/owner/link"><span class="text"><?php echo _t('링크목록');?></span></a></li>
+						<li id="sub-menu-add" class="selected"><a href="<?php echo $blogURL;?>/owner/link/add"><span class="text"><?php echo _t('링크 추가');?></span></a></li>
+						<li id="sub-menu-list"><a href="<?php echo $blogURL;?>/owner/link"><span class="text"><?php echo _t('링크 목록');?></span></a></li>
 						<li id="sub-menu-helper"><a href="http://www.tattertools.com/doc/12" onclick="window.open(this.href); return false;"><span class="text"><?php echo _t('도우미');?></span></a></li>
 					</ul>
 				</div>
