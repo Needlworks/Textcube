75d02602971952f8687351e6140516f4770c1e05
inureyes <inureyes@gmail.com>
2006-08-13 18:15:16 +0000

    * 관리자 화면 명칭 수정.

--- lib/piece/owner/contentMenu53.php
+++ lib/piece/owner/contentMenu53.php
@@ -3,11 +3,11 @@
 				
 				<div id="sub-menu-box">
 					<ul id="sub-menu">
-						<li id="sub-menu-blog"><a href="<?php echo $blogURL;?>/owner/setting/blog"><span class="text"><?php echo _t('블로그 환경');?></span></a></li>
-						<li id="sub-menu-account"><a href="<?php echo $blogURL;?>/owner/setting/account"><span class="text"><?php echo _t('계정정보');?></span></a></li>
+						<li id="sub-menu-blog"><a href="<?php echo $blogURL;?>/owner/setting/blog"><span class="text"><?php echo _t('기본 설정');?></span></a></li>
+						<li id="sub-menu-account"><a href="<?php echo $blogURL;?>/owner/setting/account"><span class="text"><?php echo _t('계정 정보');?></span></a></li>
 						<li id="sub-menu-plugin" class="selected"><a href="<?php echo $blogURL;?>/owner/setting/plugins"><span class="text"><?php echo _t('플러그인');?></span></a></li>
 						<li id="sub-menu-data"><a href="<?php echo $blogURL;?>/owner/data"><span class="text"><?php echo _t('데이터');?></span></a></li>
-						<li id="sub-menu-etc"><a href="<?php echo $blogURL;?>/owner/setting/etc"><span class="text"><?php echo _t('기타 환경');?></span></a></li>
+						<li id="sub-menu-etc"><a href="<?php echo $blogURL;?>/owner/setting/etc"><span class="text"><?php echo _t('기타 설정');?></span></a></li>
 						<li id="sub-menu-helper"><a href="http://www.tattertools.com/doc/22" onclick="window.open(this.href); return false;"><span class="text"><?php echo _t('도우미');?></span></a></li>
 					</ul>
 				</div>
