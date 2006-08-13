c67fe9732329c82622de646dbfee7856dbe776b9
inureyes <inureyes@gmail.com>
2006-08-13 18:10:31 +0000

    * 관리자 메뉴의 명칭 변경 작업

--- lib/piece/owner/contentMenuB0.php
+++ lib/piece/owner/contentMenuB0.php
@@ -1,9 +1,9 @@
 			<div id="layout-body">
-				<h2><?php echo _t('서브메뉴 : 확장기능');?></h2>
+				<h2><?php echo _t('서브메뉴 : 플러그인);?></h2>
 				
 				<div id="sub-menu-box">
 					<ul id="sub-menu">
-						<li id="sub-menu-plugin" class="selected"><a href="<?php echo $blogURL;?>/owner/plugin"><span class="text"><?php echo _t('확장기능 설정');?></span></a></li>
+						<li id="sub-menu-plugin" class="selected"><a href="<?php echo $blogURL;?>/owner/plugin"><span class="text"><?php echo _t('플러그인 목록');?></span></a></li>
 						<li id="sub-menu-helper"><a href="http://www.tattertools.com/doc/19" onclick="window.open(this.href); return false;"><span class="text"><?php echo _t('도우미');?></span></a></li>
 					</ul>
 				</div>
