0c681112777ac9a34a54378bcf841e3b6e70fd0f
inureyes <inureyes@gmail.com>
2006-08-13 17:57:49 +0000

    * 관리자 화면 재배치를 위한 작업.

--- lib/piece/owner/contentMenu02.php
+++ lib/piece/owner/contentMenu02.php
@@ -4,11 +4,11 @@
 				<div id="sub-menu-box">
 					<ul id="sub-menu">
 						<li id="sub-menu-add"><a href="<?php echo $blogURL;?>/owner/entry/post" onclick="window.location.href = '<?php echo $blogURL;?>/owner/entry/post'<?php echo (getDraftEntryId() ? "+(confirm('" . _t('임시 저장본을 보시겠습니까?') . "') ? '?draft' : '')" : '');?>"><span class="text"><?php echo _t('글쓰기');?></span></a></li>
-						<li id="sub-menu-list"><a href="<?php echo $blogURL;?>/owner/entry"><span class="text"><?php echo _t('글목록');?></span></a></li>
+						<li id="sub-menu-list"><a href="<?php echo $blogURL;?>/owner/entry"><span class="text"><?php echo _t('글 목록');?></span></a></li>
 						<li id="sub-menu-thread"><a href="<?php echo $blogURL;?>/owner/entry/comment"><span class="text"><?php echo _t('댓글');?></span></a></li>
-						<li id="sub-menu-notify"><a href="<?php echo $blogURL;?>/owner/entry/notify"><span class="text"><?php echo _t('댓글알리미');?></span></a></li>
+						<li id="sub-menu-notify"><a href="<?php echo $blogURL;?>/owner/entry/notify"><span class="text"><?php echo _t('댓글 알리미');?></span></a></li>
 						<li id="sub-menu-trackback" class="selected"><a href="<?php echo $blogURL;?>/owner/entry/trackback"><span class="text"><?php echo _t('트랙백');?></span></a></li>
-						<li id="sub-menu-category"><a href="<?php echo $blogURL;?>/owner/entry/category"><span class="text"><?php echo _t('분류관리');?></span></a></li>
+						<li id="sub-menu-category"><a href="<?php echo $blogURL;?>/owner/entry/category"><span class="text"><?php echo _t('분류 관리');?></span></a></li>
 						<li id="sub-menu-helper"><a href="http://www.tattertools.com/doc/7" onclick="window.open(this.href); return false;"><span class="text"><?php echo _t('도우미');?></span></a></li>
 					</ul>
 				</div>
