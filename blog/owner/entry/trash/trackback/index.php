7154d8876e0cb760a5b58bcf593c3006773f25b2
inureyes <inureyes@gmail.com>
2006-08-21 14:29:33 +0000

    * 트랙백 필터 누락된 부분 추가.

--- blog/owner/entry/trash/trackback/index.php
+++ blog/owner/entry/trash/trackback/index.php
@@ -45,7 +45,7 @@ require ROOT . '/lib/piece/owner/contentMenu06.php';
 										param  	=  '?value='	+ encodeURIComponent(value);
 										param 	+= '&mode=' 	+ mode;
 										param 	+= '&command=' 	+ command;
-										var request = new HTTPRequest("GET", "<?php echo $blogURL;?>/owner/setting/etc/filter/change/" + param);
+										var request = new HTTPRequest("GET", "<?php echo $blogURL;?>/owner/setting/filter/change/" + param);
 										var iconList = document.getElementsByTagName("a");	
 										for (var i = 0; i < iconList.length; i++) {
 											icon = iconList[i];
