7f4871bbb896013379241f3a4c8a82f92bbda8a3
sihwp <sihwpg@gmail.com>
2006-08-25 02:04:02 +0000

    * fix  * 사소한 버그 픽스

--- blog/owner/setting/account/invite/index.php
+++ blog/owner/setting/account/invite/index.php
@@ -12,7 +12,7 @@ $IV = array(
 );
 require ROOT . '/lib/includeForOwner.php';
 requireStrictRoute();
-if ($service['type'] == 'single') || (getUserId() > 1))
+if (($service['type'] == 'single') || (getUserId() > 1))
 	respondResultPage(false);
 
 respondResultPage($result);
