420a0c738e87c78b9036c95ab7b5e1a07fdc1bcd
egoing <egoing@gmail.com>
2006-09-06 06:59:06 +0000

    * autoplay = false로 변경

--- lib/view/ownerView.php
+++ lib/view/ownerView.php
@@ -555,22 +555,22 @@ function printEntryFileList($attachments, $param) {
 					}
 					
 					if((new RegExp("\\.(mov)$", "gi").exec(fileName))) {			
-						code = '<object classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="'+width+'" height="'+height+'"><param name="src" value="<?=$service['path']?>/attach/<?=$owner?>/'+fileName+'"/><param name="controller" value="true"><param name="scale" value="Aspect">';
-						code += '<!--[if !IE]> <--><object type="video/quicktime" data="<?=$service['path']?>/attach/<?=$owner?>/'+fileName+'" width="'+width+'" height="'+height+'" showcontrols="true" TYPE="video/quicktime" scale="Aspect" nomenu="true"><param name="showcontrols" value="true"><param name="scale" value="ToFit"></object><!--> <![endif]--></object>';
-						
+						code = '<object classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="'+width+'" height="'+height+'"><param name="src" value="<?=$service['path']?>/attach/<?=$owner?>/'+fileName+'"/><param name="controller" value="true"><param name="autoplay" value="false"><param name="scale" value="Aspect">';
+						code += '<!--[if !IE]> <--><object type="video/quicktime" data="<?=$service['path']?>/attach/<?=$owner?>/'+fileName+'" width="'+width+'" height="'+height+'" showcontrols="true" TYPE="video/quicktime" scale="Aspect" nomenu="true"><param name="showcontrols" value="true"><param name="autoplay" value="false"><param name="scale" value="ToFit"></object><!--> <![endif]--></object>';
 						writeCode(code,'previewSelected');
 						
 						return false;
 					}
 					
 				
-					if((new RegExp("\\.(mp2|wma|mid|midi|mpg|wav)$", "gi").exec(fileName))) {
+					if((new RegExp("\\.(mp2|wma|mid|midi|mpg|wav|avi|mp4)$", "gi").exec(fileName))) {
 						code ='<object width="'+width+'" height="'+height+'" classid="CLSID:22D6F312-B0F6-11D0-94AB-0080C74C7E95" codebase="http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=5,1,52,701" standby="Loading for you" type="application/x-oleobject" align="middle">';		
 						code +='<param name="FileName" value="<?=$service['path']?>/attach/<?=$owner?>/'+fileName+'">';
 						code +='<param name="ShowStatusBar" value="False">';
 						code +='<param name="DefaultFrame" value="mainFrame">';
-						code +='<param name="showControls" value="false">';
-						code +='<embed type="application/x-mplayer2" pluginspage = "http://www.microsoft.com/Windows/MediaPlayer/" src="<?=$service['path']?>/attach/<?=$owner?>/'+fileName+'" align="middle" width="'+width+'" height="'+height+'" showControls="false" defaultframe="mainFrame" showstatusbar="false"></embed>';
+						code +='<param name="autoplay" value="false">';
+						code +='<param name="showControls" value="true">';
+						code +='<embed type="application/x-mplayer2" pluginspage = "http://www.microsoft.com/Windows/MediaPlayer/" src="<?=$service['path']?>/attach/<?=$owner?>/'+fileName+'" align="middle" width="'+width+'" height="'+height+'" showControls="true" defaultframe="mainFrame" showstatusbar="false" autoplay="false"></embed>';
 						code +='</object>';
 						
 						writeCode(code,'previewSelected');
