39908fc6d049a151df063ecf665c07dddeb419e5
graphittie <graphittie@gmail.com>
2006-09-10 15:26:59 +0000

    #206 : blogIcon 기본 내장.   * 출력되는 블로그 아이콘 사이즈를 32*32로 변경.

--- plugins/BlogIcon/index.php
+++ plugins/BlogIcon/index.php
@@ -3,6 +3,6 @@ function BlogIcon_main($target, $mother) {
 	if (empty($mother['homepage']))
 		return $target;
 	$slash = ($mother['homepage']{strlen($mother['homepage']) - 1} == '/' ? '' : '/');
-	return "<img src=\"{$mother['homepage']}{$slash}index.gif\" width=\"16\" height=\"16\" onerror=\"this.parentNode.removeChild(this)\" /> $target";
+	return "<img src=\"{$mother['homepage']}{$slash}index.gif\" width=\"32\" height=\"32\" onerror=\"this.parentNode.removeChild(this)\" /> $target";
 }
 ?>
