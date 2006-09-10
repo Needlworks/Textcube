9619649782acba96df65516c550d63165cfcbf86
graphittie <graphittie@gmail.com>
2006-09-10 14:13:04 +0000

    * 파비콘의 용도에 부합하지 않는 플러그인으로 판단되어 삭제.

--- plugins/FaviconOfSite/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-function FaviconOfSite_main($target, $mother) {
-	if (empty($mother['homepage']))
-		return $target;
-	$slash = ($mother['homepage']{strlen($mother['homepage']) - 1} == '/' ? '' : '/');
-	return "<img src=\"{$mother['homepage']}{$slash}favicon.ico\" width=\"16\" height=\"16\" onerror=\"this.parentNode.removeChild(this)\" /> $target";
-}
-?>
