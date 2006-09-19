57579a904f1c8ef0a52bbdabd527fceaccd3052c
gendoh <gendoh@gmail.com>
2006-09-19 06:00:27 +0000

    리퍼러 플러그인 수정완료

--- plugins/AD_Referer_Default/index.php
+++ plugins/AD_Referer_Default/index.php
@@ -1,26 +1,23 @@
 <?php
-requireComponent( "Tattertools.Model.Statistics");
-requireComponent( "Tattertools.Model.Paging");
-requireComponent( "Tattertools.Function.misc");
-if (isset($_POST['page']))
-	$_GET['page'] = $_POST['page'];
-$IV = array(
-	'GET' => array(
-		'page' => array('int', 1, 'default' => 1)
-	),
-	'POST' => array(
-		'perPage' => array('int', 1, 'default' => 20)
-	)
-);
-$page = getUserSetting('rowsPerPage', 20);
-if (empty($_POST['perPage'])) {  
-	$perPage = $page;  
-} else if ($page != $_POST['perPage']) {  
-	setUserSetting('rowsPerPage', $_POST['perPage']);  
-	$perPage = $_POST['perPage'];  
-} else {  
-	$perPage = $_POST['perPage'];  
-}  
+function AD_Referer_Default()
+{
+	global $owner, $pluginMenuURL, $pluginAccessURL, $pluginHandlerURL;
+	requireComponent( "Tattertools.Model.Statistics");
+	requireComponent( "Tattertools.Model.Paging");
+	requireComponent( "Tattertools.Function.misc");
+	if (isset($_POST['page']))
+		$_GET['page'] = $_POST['page'];
+		
+	$page = getUserSetting('rowsPerPage', 20);
+	
+	if (empty($_POST['perPage'])) {  
+		$perPage = $page;  
+	} else if ($page != $_POST['perPage']) {  
+		setUserSetting('rowsPerPage', $_POST['perPage']);  
+		$perPage = $_POST['perPage'];  
+	} else {  
+		$perPage = $_POST['perPage'];  
+	}  
 ?>
 						<script type="text/javascript">
 							//<![CDATA[
@@ -45,19 +42,19 @@ if (empty($_POST['perPage'])) {
 								</thead>
 								<tbody>
 <?php
-$temp = Statistics::getRefererStatistics($owner);
-for ($i=0; $i<sizeof($temp); $i++) {
-	$record = $temp[$i];
-	
-	$className = ($i % 2) == 1 ? 'even-line' : 'odd-line';
-	$className .= ($i == sizeof($temp) - 1) ? ' last-line' : '';
+	$temp = Statistics::getRefererStatistics($owner);
+	for ($i=0; $i<sizeof($temp); $i++) {
+		$record = $temp[$i];
+		
+		$className = ($i % 2) == 1 ? 'even-line' : 'odd-line';
+		$className .= ($i == sizeof($temp) - 1) ? ' last-line' : '';
 ?>
 									<tr class="<?php echo $className;?> inactive-class" onmouseover="rolloverClass(this, 'over')" onmouseout="rolloverClass(this, 'out')">
 										<td class="rank"><?php echo $i + 1;?>.</td>
 										<td class="site"><a href="http://<?php echo escapeJSInAttribute($record['host']);?>" onclick="window.open(this.href); return false;"><?php echo htmlspecialchars($record['host']);?></a> <span class="count">(<?php echo $record['count'];?>)</span></td>
 									</tr>
 <?php
-}
+	}
 ?>
 								</tbody>
 							</table>
@@ -65,7 +62,7 @@ for ($i=0; $i<sizeof($temp); $i++) {
 						
 						<hr class="hidden" />
 						
-						<form id="part-statistics-log" class="part" method="post" action="<?php echo $blogURL;?>/owner/plugin/adminMenu?name=AD_Referer_Default">
+						<form id="part-statistics-log" class="part" method="post" action="<?php echo $pluginMenuURL;?>">
 							<h2 class="caption"><span class="main-text">리퍼러 로그</span></h2>
 							
 							<table class="data-inbox" cellspacing="0" cellpadding="0">
@@ -77,20 +74,20 @@ for ($i=0; $i<sizeof($temp); $i++) {
 								</thead>
 								<tbody>
 <?php
-$more = false;
-list($referers, $paging) = Statistics::getRefererLogsWithPage($suri['page'], $perPage);
-for ($i=0; $i<sizeof($referers); $i++) {
-	$record = $referers[$i];
-	
-	$className = ($i % 2) == 1 ? 'even-line' : 'odd-line';
-	$className .= ($i == sizeof($referers) - 1) ? ' last-line' : '';
+	$more = false;
+	list($referers, $paging) = Statistics::getRefererLogsWithPage($_GET['page'], $perPage);
+	for ($i=0; $i<sizeof($referers); $i++) {
+		$record = $referers[$i];
+		
+		$className = ($i % 2) == 1 ? 'even-line' : 'odd-line';
+		$className .= ($i == sizeof($referers) - 1) ? ' last-line' : '';
 ?>
 									<tr class="<?php echo $className;?> inactive-class" onmouseover="rolloverClass(this, 'over')" onmouseout="rolloverClass(this, 'out')">
 										<td class="date"><?php echo Timestamp::formatDate($record['referred']);?></td>
 										<td class="address"><a href="<?php echo escapeJSInAttribute($record['url']);?>" onclick="window.open(this.href); return false;" title="<?php echo htmlspecialchars($record['url']);?>"><?php echo fireEvent('ViewRefererURL', htmlspecialchars(UTF8::lessenAsEm($record['url'], 70)), $record);?></a></td>
 									</tr>
 <?php
-}
+	}
 ?>
 								</tbody>
 							</table>
@@ -102,10 +99,10 @@ for ($i=0; $i<sizeof($referers); $i++) {
 									<div id="page-navigation">
 										<span id="page-list">
 <?php
-$paging['prefix'] = '?name=AD_Referer_Default&page=';
-$pagingTemplate = '[##_paging_rep_##]';
-$pagingItemTemplate = '<a [##_paging_rep_link_##]>[[##_paging_rep_link_num_##]]</a>';
-echo str_repeat("\t", 8).Paging::getPagingView($paging, $pagingTemplate, $pagingItemTemplate).CRLF;
+	$paging['prefix'] = $pluginMenuURL . '&page=';
+	$pagingTemplate = '[##_paging_rep_##]';
+	$pagingItemTemplate = '<a [##_paging_rep_link_##]>[[##_paging_rep_link_num_##]]</a>';
+	echo str_repeat("\t", 8).Paging::getPagingView($paging, $pagingTemplate, $pagingItemTemplate).CRLF;
 ?>
 										</span>
 									</div>
@@ -113,17 +110,17 @@ echo str_repeat("\t", 8).Paging::getPagingView($paging, $pagingTemplate, $paging
 										<?php echo misc::getArrayValue(explode('%1', '한 페이지에 목록 %1건 표시'), 0);?>
 										<select name="perPage" onchange="document.getElementById('part-statistics-log').submit()">					
 <?php
-for ($i = 10; $i <= 100; $i += 5) {
-	if ($i == $perPage) {
+	for ($i = 10; $i <= 100; $i += 5) {
+		if ($i == $perPage) {
 ?>
 											<option value="<?php echo $i;?>" selected="selected"><?php echo $i;?></option>
 <?php
-	} else {
+		} else {
 ?>
 											<option value="<?php echo $i;?>"><?php echo $i;?></option>
 <?php
+		}
 	}
-}
 ?>
 										</select>
 										<?php echo Misc::getArrayValue(explode('%1', '한 페이지에 목록 %1건 표시'), 1);?>
@@ -133,3 +130,6 @@ for ($i = 10; $i <= 100; $i += 5) {
 								</div>
 							</div>
 						</form>
+<?php 
+}
+?>
