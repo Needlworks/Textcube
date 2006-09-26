6bd3a1d42e620149c137cc833073f8d0edbd86b6
graphittie <graphittie@gmail.com>
2006-09-26 02:43:57 +0000

    * 누락된 DateConverter/language.php 파일 추가.

---
+++ plugins/DateConverter/language.php
@@ -0,0 +1,78 @@
+<?php
+$rgDateInformation = array();
+
+// - 한국어.
+$rgDateInformation['korean']['weekday'][0] = '일요일';
+$rgDateInformation['korean']['weekday'][1] = '월요일';
+$rgDateInformation['korean']['weekday'][2] = '화요일';
+$rgDateInformation['korean']['weekday'][3] = '수요일';
+$rgDateInformation['korean']['weekday'][4] = '목요일';
+$rgDateInformation['korean']['weekday'][5] = '금요일';
+$rgDateInformation['korean']['weekday'][6] = '토요일';
+
+// - 영어.
+$rgDateInformation['english']['day ordinal postfix']['00'] = NULL;
+$rgDateInformation['english']['day ordinal postfix']['01'] = 'st';
+$rgDateInformation['english']['day ordinal postfix']['02'] = 'nd';
+$rgDateInformation['english']['day ordinal postfix']['03'] = 'rd';
+$rgDateInformation['english']['day ordinal postfix']['04'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['05'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['06'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['07'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['08'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['09'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['10'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['11'] = 'st';
+$rgDateInformation['english']['day ordinal postfix']['12'] = 'nd';
+$rgDateInformation['english']['day ordinal postfix']['13'] = 'rd';
+$rgDateInformation['english']['day ordinal postfix']['14'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['15'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['16'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['17'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['18'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['19'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['20'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['21'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['22'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['23'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['24'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['25'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['26'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['27'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['28'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['29'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['30'] = 'th';
+$rgDateInformation['english']['day ordinal postfix']['31'] = 'st';
+
+$rgDateInformation['english']['month']['length'] = 3;
+$rgDateInformation['english']['month']['01'] = 'January';
+$rgDateInformation['english']['month']['02'] = 'Febuary';
+$rgDateInformation['english']['month']['03'] = 'March';
+$rgDateInformation['english']['month']['04'] = 'April';
+$rgDateInformation['english']['month']['05'] = 'May';
+$rgDateInformation['english']['month']['06'] = 'June';
+$rgDateInformation['english']['month']['07'] = 'July';
+$rgDateInformation['english']['month']['08'] = 'August';
+$rgDateInformation['english']['month']['09'] = 'September';
+$rgDateInformation['english']['month']['10'] = 'October';
+$rgDateInformation['english']['month']['11'] = 'November';
+$rgDateInformation['english']['month']['12'] = 'December';
+
+$rgDateInformation['english']['weekday']['length'] = 3;
+$rgDateInformation['english']['weekday'][0] = 'Sunday';
+$rgDateInformation['english']['weekday'][1] = 'Monday';
+$rgDateInformation['english']['weekday'][2] = 'Tuesday';
+$rgDateInformation['english']['weekday'][3] = 'Wednesday';
+$rgDateInformation['english']['weekday'][4] = 'Thursday';
+$rgDateInformation['english']['weekday'][5] = 'Friday';
+$rgDateInformation['english']['weekday'][6] = 'Saturday';
+
+// - 일본어.
+$rgDateInformation['japanese']['weekday'][0] = '日曜日';
+$rgDateInformation['japanese']['weekday'][1] = '月曜日';
+$rgDateInformation['japanese']['weekday'][2] = '火曜日';
+$rgDateInformation['japanese']['weekday'][3] = '水曜日';
+$rgDateInformation['japanese']['weekday'][4] = '木曜日';
+$rgDateInformation['japanese']['weekday'][5] = '金曜日';
+$rgDateInformation['japanese']['weekday'][6] = '土曜日';
+?>
