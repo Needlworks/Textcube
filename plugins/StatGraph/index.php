5ba072608ad7609610f0a450defbcdb87cc58efe
gendoh <gendoh@gmail.com>
2006-10-10 13:04:22 +0000

    버럭!

--- plugins/StatGraph/index.php
+++ plugins/StatGraph/index.php
@@ -1,19 +1,7 @@
-<?
-/* Statistics Graph for Tattertools 1.1
-   ----------------------------------
-   Version 1.0
-
-   Creator          : gendoh (http://process.kaist.ac.kr/~gendoh)
-   Maintainer       : gendoh
-
-   Created at       : 2006.10.2
-   Last modified at : 2006.10.10
-
- This plugin adds Statistics Graph on sidebar panel.
-
- if installed at other directory, edit a location of count
- global $pluginURL;
-*/
+<?php
+// Statistics Graph by Gendoh http://gendoh.tistory.com
+// if installed at other directory, edit a location of count
+// global $pluginURL;
 
 function DisplayStatisticsGraph($target) 
 { 
