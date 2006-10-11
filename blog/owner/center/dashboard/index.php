4d6b39def4271a9e9e135e40afc2ce1172bcb432
gendoh <gendoh@gmail.com>
2006-10-11 02:50:15 +0000

    1. 태터 공지사항 테스트 2. 자투리들 사이즈 456->356으로 변경 테스트.

--- blog/owner/center/dashboard/index.php
+++ blog/owner/center/dashboard/index.php
@@ -50,6 +50,81 @@ if (false) {
 	
 <form method="post" action="<?php echo $blogURL;?>/owner/center/dashboard">
 	<div id="part-center-dashboard" class="part">
+<?php
+if (!isset($_REQUEST['edit'])) {
+	echo '<h2 class="caption"><span class="main-text">' . _t('태터툴즈 공지사항') . '</span></h2>';
+	echo '<div class="notice-section">';
+
+	$noticeURL = 'http://blog.tattertools.com/rss';
+	list($result, $feed, $xml) = getRemoteFeed($noticeURL);
+	if ($result == 0) {
+		$xmls = new XMLStruct();
+		$noticeEntries = array();
+		if ($xmls->open($xml, $service['encoding'])) {
+			if ($xmls->getAttribute('/rss', 'version')) {
+				for ($i = 0; $link = $xmls->getValue("/rss/channel/item[$i]/link"); $i++) {
+					$item = array('permalink' => rawurldecode($link));
+					$item['title'] = $xmls->getValue("/rss/channel/item[$i]/title");
+					if ($xmls->getValue("/rss/channel/item[$i]/pubDate"))
+						$item['written'] = parseDate($xmls->getValue("/rss/channel/item[$i]/pubDate"));
+					else if ($xmls->getValue("/rss/channel/item[$i]/dc:date"))
+						$item['written'] = parseDate($xmls->getValue("/rss/channel/item[$i]/dc:date"));
+					else
+						$item['written'] = 0;
+					array_push($noticeEntries, $item);
+				}
+			} else if ($xmls->getAttribute('/feed', 'version')) {
+				for ($i = 0; $link = $xmls->getValue("/feed/entry[$i]/id"); $i++) {
+					for ($j = 0; $rel = $xmls->getAttribute("/feed/entry[$i]/link[$j]", 'rel'); $j++) {
+						if($rel == 'alternate') {
+							$link = $xmls->getAttribute("/feed/entry[$i]/link[$j]", 'href');
+							break;
+						}
+					}
+					$item = array('permalink' => rawurldecode($link));
+					$item['author'] = $xmls->getValue("/feed/entry[$i]/author/name");
+					$item['title'] = $xmls->getValue("/feed/entry[$i]/title");
+					$item['written'] = parseDate($xmls->getValue("/feed/entry[$i]/issued"));
+					array_push($noticeEntries, $item);
+				}
+			} else if ($xmls->getAttribute('/rdf:RDF', 'xmlns')) {
+				for ($i = 0; $link = $xmls->getValue("/rdf:RDF/item[$i]/link"); $i++) {
+					$item = array('permalink' => rawurldecode($link));
+					$item['author'] = $xmls->getValue("/rdf:RDF/item[$i]/dc:creator");
+					$item['title'] = $xmls->getValue("/rdf:RDF/item[$i]/title");
+					$item['written'] = parseDate($xmls->getValue("/rdf:RDF/item[$i]/dc:date"));
+					array_push($noticeEntries, $item);
+				}
+			}
+		}	
+		
+		if (count($noticeEntries) > 0) {
+			echo '<ol>';
+			foreach($noticeEntries as $item) {
+				echo '<li>';
+				echo '<a href="' , $item['permalink'] , '">';
+				echo '<span class="titlespan">' , htmlspecialchars($item['title']) , '</span>';
+				echo '<span class="timespan">' , Timestamp::format5($item['written']) , '</span>';
+				echo '</a>';
+				echo '</li>';
+			}
+			echo '</ol>';
+		} else {
+			echo _t('공지사항이 없습니다.');
+		}
+		
+	} else {
+		echo _t('공지사항을 가져올 수 없습니다. 잠시 후 다시 시도해 보심시오');
+	}
+
+	unset($feed);
+	unset($xmls);
+	unset($noticeEntries);
+	echo '</div>';
+}
+
+
+?>
 		<h2 class="caption"><span class="main-text"><?php echo _t('조각보를 봅니다');?></span></h2>
 <?php
 
