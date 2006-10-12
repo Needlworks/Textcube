015f1eee0cc1e5df772e884ecf5fa91d1edf14cb
gendoh <gendoh@gmail.com>
2006-10-12 05:26:50 +0000

    저장버튼 클릭시 폼전송 되는 오류 수정

--- blog/owner/link/edit/item.php
+++ blog/owner/link/edit/item.php
@@ -83,7 +83,7 @@ require ROOT . '/lib/piece/owner/contentMenu20.php';
 								</div>
 								
 								<div class="button-box">
-									<input type="submit" class="edit-button input-button" value="<?php echo _t('저장하기');?>" onclick="updateLink()" />
+									<input type="submit" class="edit-button input-button" value="<?php echo _t('저장하기');?>" onclick="updateLink(); return false;" />
 									<span class="hidden">|</span>
 									<a class="cancel-button button" href="<?php echo $blogURL;?>/owner/link"><span class="text"><?php echo _t('취소하기');?></span></a>
 								</div>
