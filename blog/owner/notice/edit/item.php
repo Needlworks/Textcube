81c08d3f0219abb75f6259fbc3308d78d1fa37ad
inureyes <inureyes@gmail.com>
2006-10-18 13:22:24 +0000

    * 안쓰는 파일과 디렉토리 삭제

--- blog/owner/notice/edit/item.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('__TATTERTOOLS_NOTICE__', true);
-require '../../entry/edit/item.php';
-?>
--- blog/owner/notice/post/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('__TATTERTOOLS_NOTICE__', true);
-define('__TATTERTOOLS_POST__', true);
-require '../../entry/edit/item.php';
-?>
