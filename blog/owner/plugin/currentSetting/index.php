4ae2897f3736c3d40dbc5b4242e5c8b561d00a6f
graphittie <graphittie@gmail.com>
2006-10-29 14:14:56 +0000

    * 플러그인 환경설정 폼 구조 변경 및 "취소" 버튼 삭제.

--- blog/owner/plugin/currentSetting/index.php
+++ blog/owner/plugin/currentSetting/index.php
@@ -25,49 +25,56 @@ if( is_null($result) )	respondNotFoundPage();
 	<script type="text/javascript" src="<?php echo $service['path'];?>/script/pluginconfig.js"> </script>
 	<script type="text/javascript">
 		//<![CDATA[
-		var fiednamelist = <?php echo $result['script'] ;?>;
-
-		var errorMessage ={
-			"1": "<?php echo _t('데이터처리 오류 발생.');?>",
-			"2": "<?php echo _t('잘못된 입력 입니다.');?>"
-		};
-		function saveConfig(plugin){
-			var xmlcon= new Converter(document, fiednamelist) ; 
-		   	var xmlData = encodeURIComponent(xmlcon.getXMLData());
-			var request = new HTTPRequest("POST" , "<?php echo $targetURL;?>");
-			PM.addRequest(request, "<?php echo _t('설정을 저장하고 있습니다.');?>");
-			request.onSuccess = function () {
-				PM.removeRequest(this);
-				PM.showMessage("<?php echo _t('저장 완료');?>", "center", "bottom");
-				window.close();
-			};		
-			request.onError = function () {
-				PM.removeRequest(this);
-				if( this.getText("/response/error") == "9" )
-					alert(this.getText("/response/customError"));
-				else if( undefined != errorMessage[ this.getText("/response/error") ] )
-					alert( errorMessage[ this.getText("/response/error") ] );
-				else if( undefined != this.getText("/response/error") )			
-					alert("<?php echo _t('알 수 없는 에러입니다.');?>" );
-				else 
-					alert("<?php echo _t('데이터 처리 페이지를 찾을 수 없습니다.');?>");
+			var fiednamelist = <?php echo $result['script'] ;?>;
+			
+			var errorMessage ={
+				"1": "<?php echo _t('데이터처리 오류 발생.');?>",
+				"2": "<?php echo _t('잘못된 입력 입니다.');?>"
 			};
-			request.onVerify = function() {
-				return (this.getText("/response/error") == "0" );
-			};			
-			request.send("Name=" + encodeURIComponent(plugin) + "&DATA=" + xmlData);
-			xmlcon = null;
-			request = null;
-		}	
-	//]]>
-</script>
+			function saveConfig(plugin){
+				var xmlcon= new Converter(document, fiednamelist) ; 
+			   	var xmlData = encodeURIComponent(xmlcon.getXMLData());
+				var request = new HTTPRequest("POST", "<?php echo $targetURL;?>");
+				PM.addRequest(request, "<?php echo _t('설정을 저장하고 있습니다.');?>");
+				request.onSuccess = function () {
+					PM.removeRequest(this);
+					PM.showMessage("<?php echo _t('저장 완료');?>", "center", "bottom");
+					window.close();
+				};		
+				request.onError = function () {
+					PM.removeRequest(this);
+					if( this.getText("/response/error") == "9" )
+						alert(this.getText("/response/customError"));
+					else if( undefined != errorMessage[ this.getText("/response/error") ] )
+						alert( errorMessage[ this.getText("/response/error") ] );
+					else if( undefined != this.getText("/response/error") )			
+						alert("<?php echo _t('알 수 없는 에러입니다.');?>" );
+					else 
+						alert("<?php echo _t('데이터 처리 페이지를 찾을 수 없습니다.');?>");
+				};
+				request.onVerify = function() {
+					return (this.getText("/response/error") == "0" );
+				};			
+				request.send("Name=" + encodeURIComponent(plugin) + "&DATA=" + xmlData);
+				xmlcon = null;
+				request = null;
+			}	
+		//]]>
+	</script>
 </head>
 <body>
-	<h3 class="caption"><?php echo _f("%1 설정", $pluginName);?></h3>
-	<div id='config_data'><?php echo $result['code'];?></div>
-	<div class="submit">
-		<input type="button" value="<?php echo _t('설정');?>" onclick="saveConfig('<?php echo $pluginName;?>');" />
-		<input type="button" value="<?php echo _t('취소');?>" onclick="self.close();" />
-	</div>
+	<form method="post" action="<?php echo $targetURL;?>">
+		<div id="layout-head">
+			<h1 class="caption"><?php echo _f("%1 설정", $pluginName);?></h1>
+		</div>
+		<div id="layout-body">
+			<div id="config_data">
+<?php echo $result['code'];?>
+			</div>
+		</div>
+		<div id="layout-foot" class="button-box">
+			<input type="submit" class="input-button" value="<?php echo _t('저장하기');?>" onclick="saveConfig('<?php echo $pluginName;?>'); return false;" />
+		</div>
+	</form>
 </body>
 </html>
