0154e74ec04f104d034140917b9c4c651f90546d
inureyes <inureyes@gmail.com>
2006-10-30 10:21:28 +0000

    * 키워드 사용시 절대주소 변경 불가 부분 수정.

--- lib/model/entry.php
+++ lib/model/entry.php
@@ -294,14 +294,15 @@ function addEntry($owner, $entry) {
 	global $database, $blog;
 	$entry['title'] = mysql_lessen(trim($entry['title']));
 	$entry['location'] = mysql_lessen(trim($entry['location']));
-	if(empty($entry['slogan'])||($entry['category'] == -1)) {
+	if((empty($entry['slogan']))||($entry['category'] == -1)) {
 		$slogan = $slogan0 = getSlogan($entry['title']);
 	} else {
 		$slogan = $slogan0 = getSlogan($entry['slogan']);
 	}
 	$slogan = mysql_real_escape_string($slogan);
 	$result = mysql_query("SELECT slogan FROM {$database['prefix']}Entries WHERE owner = $owner AND slogan = '$slogan' LIMIT 1");
-	if(($result!=false)&&($entry['category'] == -1)) return false;
+
+	if((mysql_num_rows($result)!=0)&&($entry['category'] == -1)) return false;
 
 	for ($i = 1; mysql_num_rows($result) > 0; $i++) {
 		if ($i > 100)
