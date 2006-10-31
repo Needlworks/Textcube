7c5228daf9fc6ceeebd21ba1ced0ebfc4a0a62de
gendoh <gendoh@gmail.com>
2006-10-31 14:01:52 +0000

    윈도우 환경등 테이블명이 대소문자를 안가리는 환경에서의 문제 수정

--- blog/owner/plugin/tableSetting/index.php
+++ blog/owner/plugin/tableSetting/index.php
@@ -65,10 +65,28 @@ $escapename = preg_replace($likeEscape, $likeReplace, $database['prefix']);
 $query = "show tables like '{$escapename}%'";
 $dbtables = DBQuery::queryColumn($query);
 
+$result = DBQuery::queryRow("show variables like 'lower_case_table_names'");
+$dbCaseSensitive = ($result['Value'] == 1) ? true : false;
+
 $prefix = $database['prefix'];
 $definedTables = array("{$prefix}Attachments", "{$prefix}BlogSettings", "{$prefix}BlogStatistics", "{$prefix}Categories", "{$prefix}Comments", "{$prefix}CommentsNotified", "{$prefix}CommentsNotifiedQueue", "{$prefix}CommentsNotifiedSiteInfo", "{$prefix}DailyStatistics", "{$prefix}Entries", "{$prefix}FeedGroupRelations", "{$prefix}FeedGroups", "{$prefix}FeedItems", "{$prefix}FeedReads", "{$prefix}Feeds", "{$prefix}FeedSettings", "{$prefix}FeedStarred", "{$prefix}Filters", "{$prefix}Links", "{$prefix}Plugins", "{$prefix}RefererLogs", "{$prefix}RefererStatistics", "{$prefix}ReservedWords", "{$prefix}ServiceSettings", "{$prefix}Sessions", "{$prefix}SessionVisits", "{$prefix}SkinSettings", "{$prefix}TagRelations", "{$prefix}Tags", "{$prefix}TrackbackLogs", "{$prefix}Trackbacks", "{$prefix}Users", "{$prefix}UserSettings");
 
 $dbtables = array_values(array_diff($dbtables, $definedTables));
+if ($dbCaseInsensitive == true) {
+	$tempTables = $definedTables;
+	$definedTables = array();
+	foreach($tempTables as $table) {
+		$table = strtolower($table);
+		array_push($definedTables, $table);
+	}
+	$tempTables = $dbtables;
+	$dbtables = array();
+	foreach($tempTables as $table) {
+		$table = strtolower($table);
+		array_push($dbtables, $table);
+	}
+	$dbtables = array_values(array_diff($dbtables, $definedTables));
+}
 
 $query = "select name, value from {$database['prefix']}ServiceSettings WHERE name like 'Database\_%'";
 $plugintablesraw = DBQuery::queryAll($query);
@@ -82,6 +100,8 @@ foreach($plugintablesraw as $table) {
 		$plugintables[$plugin .'/'. $version] = array('plugin' => $plugin, 'version' => $version, 'tables' => array());
 	}
 	array_push($plugintables[$plugin .'/'. $version]['tables'], $dbname);
+	
+	if ($dbCaseInsensitive == true) $dbname = strtolower($dbname);
 	
 	if (($pos = array_search($dbname, $dbtables)) !== false) {
 		array_splice($dbtables, $pos, 1);
