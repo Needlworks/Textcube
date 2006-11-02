9392731f341b228d2fcf43c1e1cb6fe7fd4d007c
inureyes <inureyes@gmail.com>
2006-11-02 08:09:47 +0000

    * 사용성 테스트 반영   * 기본적으로 필요하다고 건의된 플러그인 제작 후 추가.   * SB_Spacer : 환경설정에서 정한 픽셀 높이만큼 사이드바에 빈 칸을 만들어줌.

---
+++ plugins/SB_Spacer/index.php
@@ -0,0 +1,46 @@
+<?php
+/* Spacer plugin for Tattertools 1.1
+   ----------------------------------
+   Version 1.0
+   Tatter and Friends development team.
+
+   Creator          : inureyes
+   Maintainer       : inureyes
+
+   Created at       : 2006.11.1
+   Last modified at : 2006.11.2
+ 
+ This plugin adds space on the sidebar.
+ For the detail, visit http://forum.tattertools.com/ko
+
+
+ General Public License
+ http://www.gnu.org/licenses/gpl.html
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+*/
+function SB_Spacer($target)
+{
+	global $configVal;
+	requireComponent('Tattertools.Function.misc');
+	$data = misc::fetchConfigVal($configVal);
+	if(!is_null($data)){
+		$height = $data['height'];
+	} else {
+		$height = '20';
+	}
+	$text = '<div class="SB_Spacer" style="height:'.$height.'px;"></div>';
+	return $text;
+}
+
+function SB_Spacer_DataSet($data){
+	requireComponent('Tattertools.Function.misc');
+	if(!is_integer(intval($data['height']))) return false;
+	$cfg = misc::fetchConfigVal($data);
+	return true;
+}
+?>
