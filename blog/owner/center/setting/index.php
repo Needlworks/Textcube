77beb7463fae000950a6c430241eb8fa469108db
inureyes <inureyes@gmail.com>
2006-11-04 08:43:08 +0000

    * GPL 명기 (2)

--- blog/owner/center/setting/index.php
+++ blog/owner/center/setting/index.php
@@ -1,5 +1,7 @@
 <?php
-// define('__TATTERTOOLS_CENTER__', true);
+/// Copyright (c) 2004-2006, Tatter & Company / Tatter & Friends.
+/// All rights reserved. Licensed under the GPL.
+/// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
 define('ROOT', '../../../..');
 
 require ROOT . '/lib/includeForOwner.php';
