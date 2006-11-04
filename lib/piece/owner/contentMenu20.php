b4b25e18342883695b2f30c9706a907d833e94f4
graphittie <graphittie@gmail.com>
2006-11-04 15:58:56 +0000

    * 잘못된 도우미 링크 수정.

--- lib/piece/owner/contentMenu20.php
+++ lib/piece/owner/contentMenu20.php
@@ -5,7 +5,7 @@
 					<ul id="sub-menu">
 						<li id="sub-menu-add"><a href="<?php echo $blogURL;?>/owner/link/add"><span class="text"><?php echo _t('링크 추가');?></span></a></li>
 						<li id="sub-menu-list" class="selected"><a href="<?php echo $blogURL;?>/owner/link"><span class="text"><?php echo _t('링크 목록');?></span></a></li>
-						<li id="sub-menu-helper"><a href="<?php echo getHelpURL('link/add');?>" onclick="window.open(this.href); return false;"><span class="text"><?php echo _t('도우미');?></span></a></li>
+						<li id="sub-menu-helper"><a href="<?php echo getHelpURL('link');?>" onclick="window.open(this.href); return false;"><span class="text"><?php echo _t('도우미');?></span></a></li>
 					</ul>
 				</div>
 				
