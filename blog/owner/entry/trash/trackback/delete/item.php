2193796d3b019c516290b9fbb127f1842d917785
inureyes <inureyes@gmail.com>
2006-11-04 09:08:22 +0000

    * GPL 명기 (3)

--- blog/owner/entry/trash/trackback/delete/item.php
+++ blog/owner/entry/trash/trackback/delete/item.php
@@ -1,10 +1,11 @@
 <?php
+/// Copyright (c) 2004-2006, Tatter & Company / Tatter & Friends.
+/// All rights reserved. Licensed under the GPL.
+/// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
 define('ROOT', '../../../../../..');
 
 require ROOT . '/lib/includeForOwner.php';
-
-$isAjaxRequest = checkAjaxRequest();
-
+$isAjaxRequest = checkAjaxRequest();\
 if (deleteTrackback($owner, $suri['id']) !== true)
 	$isAjaxRequest ? respondResultPage(0) : header("Location: ".$_SERVER['HTTP_REFERER']);
 else
