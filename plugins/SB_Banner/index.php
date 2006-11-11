3142b39e46f245c2a457d0a019fe6b9799549033
inureyes <inureyes@gmail.com>
2006-11-11 11:44:12 +0000

    * GPL 관련 주석 추가. (배포본으로부터 추가됨)

--- plugins/SB_Banner/index.php
+++ plugins/SB_Banner/index.php
@@ -1,5 +1,27 @@
 <?php
+/* Sidebar Banner example plugin for Tattertools 1.1
+   ----------------------------------
+   Version 1.0
+   Tatter and Friends development team.
 
+   Creator          : gendoh
+   Maintainer       : gendoh
+
+   Created at       : 2006.9.3
+   Last modified at : 2006.11.11
+
+ This plugin is an example for adding banners into sidebar.
+ For the detail, visit http://forum.tattertools.com/ko
+
+ General Public License
+ http://www.gnu.org/licenses/gpl.html
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+*/
 function SB_Banner_withImage($parameters)
 {
 	if (isset($parameters['preview'])) {
