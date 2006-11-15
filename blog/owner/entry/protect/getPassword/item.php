552fc33ce32a6347efe27387bd5d115a43f767e7
gendoh <gendoh@gmail.com>
2006-11-15 00:28:03 +0000

    패스워드 없을 때 'null'이라고 나오는 버그 수정

--- blog/owner/entry/protect/getPassword/item.php
+++ blog/owner/entry/protect/getPassword/item.php
@@ -5,5 +5,6 @@
 define('ROOT', '../../../../..');
 require ROOT . '/lib/includeForOwner.php';
 $password = fetchQueryCell("SELECT `password` FROM `{$database['prefix']}Entries` WHERE `owner` = $owner AND `id` = {$suri['id']}");
+if (is_null($password)) $password = '';
 printRespond(array('error' => 0, 'password' => $password));
 ?>
