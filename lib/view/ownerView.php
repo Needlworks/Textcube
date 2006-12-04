fd5a2fa2a0537fcae08a361fc73dfa8cea50302f
graphittie <graphittie@gmail.com>
2006-12-04 08:55:00 +0000

    * 에디터 속성창에서 리샘플링 체크박스를 선택해도 class가 지정되지 않는 문제 및 워터마크 체크 박스가 표시되지 않는 버그 수정(워터마크 파일 존재 체크 루틴 삭제).

--- lib/view/ownerView.php
+++ lib/view/ownerView.php
@@ -1051,13 +1051,7 @@ function printEntryEditorProperty($alt=NULL) {
 													<dl class="resample-property-box line">
 														<dd>
 															<input type="checkbox" id="propertyImage1_resample1" onclick="checkResampling('resample', 1, 1); editor.setProperty()" /> <label for="propertyImage1_resample1"><?php echo _t('이미지에 리샘플링을 적용합니다.');?></label><br />
-<?php
-		if (file_exists(ROOT."/attach/$owner/watermark.gif")) {
-?>
 															<input type="checkbox" id="propertyImage1_watermark1" onclick="checkResampling('watermark', 1, 1); editor.setProperty()" /> <label for="propertyImage1_watermark1"><?php echo _t('이미지에 워터마크를 찍습니다.');?></label>
-<?php
-		}
-?>
 														</dd>
 													</dl>
 <?php
@@ -1093,13 +1087,7 @@ function printEntryEditorProperty($alt=NULL) {
 													<dl class="resample-property-box line">
 														<dd>
 															<input type="checkbox" id="propertyImage2_resample1" onclick="checkResampling('resample', 2, 1); editor.setProperty()" /> <label for="propertyImage2_resample1"><?php echo _t('이미지에 리샘플링을 적용합니다.');?></label><br />
-<?php
-		if (file_exists(ROOT."/attach/$owner/watermark.gif")) {
-?>
 															<input type="checkbox" id="propertyImage2_watermark1" onclick="checkResampling('watermark', 2, 1); editor.setProperty()" /> <label for="propertyImage2_watermark1"><?php echo _t('이미지에 워터마크를 찍습니다.');?></label>
-<?php
-		}
-?>
 														</dd>
 													</dl>
 <?php
@@ -1127,13 +1115,7 @@ function printEntryEditorProperty($alt=NULL) {
 													<dl class="resample-property-box line">
 														<dd>
 															<input type="checkbox" id="propertyImage2_resample2" onclick="checkResampling('resample', 2, 2); editor.setProperty()" /> <label for="propertyImage2_resample2"><?php echo _t('이미지에 리샘플링을 적용합니다.');?></label><br />
-<?php
-		if (file_exists(ROOT."/attach/$owner/watermark.gif")) {
-?>
 															<input type="checkbox" id="propertyImage2_watermark2" onclick="checkResampling('watermark', 2, 2); editor.setProperty()" /> <label for="propertyImage2_watermark2"><?php echo _t('이미지에 워터마크를 찍습니다.');?></label>
-<?php
-		}
-?>
 														</dd>
 													</dl>
 <?php
@@ -1169,13 +1151,7 @@ function printEntryEditorProperty($alt=NULL) {
 													<dl class="resample-property-box line">
 														<dd>
 															<input type="checkbox" id="propertyImage3_resample1" onclick="checkResampling('resample', 3, 1); editor.setProperty()" /> <label for="propertyImage3_resample1"><?php echo _t('이미지에 리샘플링을 적용합니다.');?></label><br />
-<?php
-		if (file_exists(ROOT."/attach/$owner/watermark.gif")) {
-?>
 															<input type="checkbox" id="propertyImage3_watermark1" onclick="checkResampling('watermark', 3, 1); editor.setProperty()" /> <label for="propertyImage3_watermark1"><?php echo _t('이미지에 워터마크를 찍습니다.');?></label>
-<?php
-		}
-?>	
 														</dd>
 													</dl>
 <?php
@@ -1203,13 +1179,7 @@ function printEntryEditorProperty($alt=NULL) {
 													<dl class="resample-property-box line">
 														<dd>
 															<input type="checkbox" id="propertyImage3_resample2" onclick="checkResampling('resample', 3, 2); editor.setProperty()" /> <label for="propertyImage3_resample2"><?php echo _t('이미지에 리샘플링을 적용합니다.');?></label><br />
-<?php
-		if (file_exists(ROOT."/attach/$owner/watermark.gif")) {
-?>
 															<input type="checkbox" id="propertyImage3_watermark2" onclick="checkResampling('watermark', 3, 2); editor.setProperty()" /> <label for="propertyImage3_watermark2"><?php echo _t('이미지에 워터마크를 찍습니다.');?></label>
-<?php
-		}
-?>
 														</dd>
 													</dl>
 <?php
@@ -1237,13 +1207,7 @@ function printEntryEditorProperty($alt=NULL) {
 													<dl class="resample-property-box line">
 														<dd>
 															<input type="checkbox" id="propertyImage3_resample3" onclick="checkResampling('resample', 3, 3); editor.setProperty()" /> <label for="propertyImage3_resample3"><?php echo _t('이미지에 리샘플링을 적용합니다.');?></label><br />
-<?php
-		if (file_exists(ROOT."/attach/$owner/watermark.gif")) {
-?>	
 															<input type="checkbox" id="propertyImage3_watermark3" onclick="checkResampling('watermark', 3, 3); editor.setProperty()" /> <label for="propertyImage3_watermark3"><?php echo _t('이미지에 워터마크를 찍습니다.');?></label>
-<?php
-		}
-?>
 														</dd>
 													</dl>
 <?php
