8b0a2c3b45c7a431d04bf4f2e9b3869bbd8e7892
inureyes <inureyes@gmail.com>
2007-01-08 13:05:51 +0000

    * 카테고리 추가가 안되는 문제 수정

--- lib/model/category.php
+++ lib/model/category.php
@@ -135,7 +135,7 @@ function addCategory($owner, $parent, $name) {
 		return false;
 
 	$newPriority = DBQuery::queryCell("SELECT MAX(priority) FROM {$database['prefix']}Categories WHERE owner = $owner") + 1;
-	$result = DBQuery::query("INSERT INTO {$database['prefix']}Categories (owner, id, parent, name, priority, entries, entriesInLogin, label) VALUES ($owner, NULL, $parent, '$name', $newPriority, 0, 0, '$label')");
+	$result = DBQuery::query("INSERT INTO {$database['prefix']}Categories (owner, id, parent, name, priority, entries, entriesInLogin, label, visibility) VALUES ($owner, NULL, $parent, '$name', $newPriority, 0, 0, '$label', 2)");
 	updateEntriesOfCategory($owner);
 	return $result ? true : false;
 }
