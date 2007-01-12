ec4d7110860de03688bd3c49858d093f2fcea6df
graphittie <graphittie@gmail.com>
2007-01-12 03:00:24 +0000

    * DateConverter에 오전/오후 치환자 추가.

---
+++ plugins/DateConverter/config.ini.php
@@ -0,0 +1,14 @@
+<?php
+$data['language'] = 'korean';
+$data['archive_date'] = '{%Y년}{ %M월}{ %D일}{ %H시}{ %I분}{ %S초}';
+$data['calendar_date'] = '{%Y년}{ %M월}{ %D일}{ %H시}{ %I분}{ %S초}';
+$data['comment_date'] = '{%Y년}{ %M월}{ %D일}{ %H시}{ %I분}{ %S초}';
+$data['comment_list_date'] = '{%Y년}{ %M월}{ %D일}{ %H시}{ %I분}{ %S초}';
+$data['guestbook_date'] = '{%Y년}{ %M월}{ %D일}{ %H시}{ %I분}{ %S초}';
+$data['list_date'] = '{%Y년}{ %M월}{ %D일}{ %H시}{ %I분}{ %S초}';
+$data['notice_date'] = '{%Y년}{ %M월}{ %D일}{ %H시}{ %I분}{ %S초}';
+$data['post_date'] = '{%Y년}{ %M월}{ %D일}{ %H시}{ %I분}{ %S초}';
+$data['recent_comment_date'] = '{%Y년}{ %M월}{ %D일}{ %H시}{ %I분}{ %S초}';
+$data['recent_trackback_date'] = '{%Y년}{ %M월}{ %D일}{ %H시}{ %I분}{ %S초}';
+$data['trackback_date'] = '{%Y년}{ %M월}{ %D일}{ %H시}{ %I분}{ %S초}';
+?>
