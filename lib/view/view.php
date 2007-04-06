5d07739b30441d1cd7a69aace14b9a50cc9a5192
crizin <crizin@gmail.com>
2007-04-06 02:14:41 +0000

    #357   * [3069/old_svn]를 [3071/old_svn]에 반영하는 과정에서 누락된 부분 추가합니다

--- lib/view/view.php
+++ lib/view/view.php
@@ -1970,7 +1970,7 @@ function createNewProperty($filename, $imageWidth, $property) {
 	}
 
 	// 가로, 세로 어느 쪽이든 0이면 이미지는 표시되지 않음. 따라서 계산할 필요 없음.
-	if ($attributes['width'] === 0 || $attributes['height'] === 0) {
+	if (@$attributes['width'] === 0 || @$attributes['height'] === 0) {
 		return array($property, false);
 	}
 
