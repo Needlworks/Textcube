434fe00f6499ece0955fc0074fdc613c55c90516
inureyes <inureyes@gmail.com>
2007-05-15 08:56:42 +0000

    #397   * Move to trunk / 1.5 tree. (sandbox [3270/old_svn] )

--- lib/piece/owner/header.php
+++ lib/piece/owner/header.php
@@ -67,6 +67,10 @@ switch($blogMenu['topMenu']) {
 		$blogMenu['loadCSSIE7'] = array('reader');
 		break;
 }
+
+// mapping data management to setting
+if(isset($blogMenu['topMenu']) && $blogMenu['topMenu']=='data') $blogMenu['topMenu'] = 'setting';
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo (isset($blog['language']) ? $blog['language'] : "ko");?>">
