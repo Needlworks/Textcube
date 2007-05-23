1bbeacacf70765bd0d19b8681f369f433e7fbfdf
inureyes <inureyes@gmail.com>
2007-05-23 11:54:03 +0000

    #408   * Move to trunk / 1.5 (sandbox [3317/old_svn])

--- blog/login/index.php
+++ blog/login/index.php
@@ -91,6 +91,33 @@ if (doesHaveOwnership()) {
 			}
 		//]]>
 	</script>
+<?php
+if (!file_exists(ROOT . '/cache/CHECKUP')) {
+?>
+	<script type="text/javascript">
+		//<![CDATA[
+			window.addEventListener("load", checkTextcubeVersion, false);
+			function checkTextcubeVersion() {
+				if (confirm("<?php echo _t('버전업 체크를 위한 파일을 생성합니다. 지금 생성하시겠습니까?');?>"))
+					window.location.href = "<?php echo $blogURL;?>/checkup";
+			}
+		//]]>
+	</script>
+<?php
+} else if (file_get_contents(ROOT . '/cache/CHECKUP') != TEXTCUBE_VERSION) {
+?>
+	<script type="text/javascript">
+		//<![CDATA[
+			window.addEventListener("load", checkTextcubeVersion, false);
+			function checkTextcubeVersion() {
+				if (confirm("<?php echo _t('텍스트큐브 시스템 점검이 필요합니다. 지금 점검하시겠습니까?');?>"))
+					window.location.href = "<?php echo $blogURL;?>/checkup";
+				}
+		//]]>
+	</script>
+<?php
+}
+?>
 </head>
 <body id="body-login">
 	<div id="temp-wrap">
