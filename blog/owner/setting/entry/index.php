6ae4139c03994f41d2b4bfa94a6ad8d71c0ca9c0
inureyes <inureyes@gmail.com>
2007-05-29 17:42:53 +0000

    #393   * 버전 번호 및 체인지로그 갱신.   * 오타 수정

--- blog/owner/setting/entry/index.php
+++ blog/owner/setting/entry/index.php
@@ -132,7 +132,7 @@ foreach (getAllEditors() as $key => $value) {
 										<dl id="blogapi-line" class="line">
 											<dt><span class="label"><?php echo _t('블로그 API 용 비밀번호');?></span></dt>
 											<dd>
-												<p><label for="blogApiPassword"><?php echo _t('BlogAPI에 사용할 비밀번호입니다. 관리자 로그인 비밀번호와 동일하게 사용하실 경우 비어두세요.');?></label></p>
+												<p><label for="blogApiPassword"><?php echo _t('BlogAPI에 사용할 비밀번호입니다. 관리자 로그인 비밀번호와 동일하게 사용하실 경우 비워두세요.');?></label></p>
 												<input type="text" style="width:14em" class="input-text" id="blogApiPassword" name="blogApiPassword" value="<?php echo getUserSetting("blogApiPassword", "");?>" />
 												<input type="button" class="input-button" value="<?php echo _t('임의로 생성')?>" onclick="chooseBlogPassword()" >
 												<input type="button" class="input-button" value="<?php echo _t('관리자 비밀번호를 그대로 사용')?>" onclick="clearBlogPassword()" >
