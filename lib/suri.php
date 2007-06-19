2762b87ae9160e8945a2b7775cd3965a9241d36b
inureyes <inureyes@gmail.com>
2007-06-19 15:05:36 +0000

    #408   * 로그인시 세션의 blogid에 넣는 부분에서 함수의 순서가 문제 되는 부분 수정

--- lib/suri.php
+++ lib/suri.php
@@ -44,7 +44,7 @@ if ($service['type'] == 'single') {
 		respondNotFoundPage();
 }
 
-setBlogId($owner);
+$_SESSION['blogid'] = $owner;
 $blog = getBlogSetting($owner);
 $skinSetting = getSkinSetting($owner);
 
