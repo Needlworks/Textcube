5fdffa118d19451231055016aa0c9a29a139e3be
jparker <jparker2.0@gmail.com>
2007-06-30 13:03:18 +0000

    #408  * 팀블로용 관리자 플러그인 추가.

---
+++ plugins/ST_TeamBlogSettings/index.php
@@ -0,0 +1,377 @@
+<?php
+function getTeamBlogInitConfigVal( &$data ){
+	$postHeaderDefault = '<fieldset><legend>Writer profile</legend>';
+	$postFooterDefault = '</fieldset>';
+	$data['p1'] = !isset($data['p1'])?true:$data['p1'];
+	$data['postHeader']	= !isset($data['postHeader'])?$postHeaderDefault:$data['postHeader'];
+	$data['postFooter']	= !isset($data['postFooter'])?$postFooterDefault:$data['postFooter'];
+	$data['imageSize']	= !isset($data['imageSize'])?80:$data['imageSize'];
+	$data['imagePosition']	= !isset($data['imagePosition'])?'left':$data['imagePosition'];
+	$data['lineColor']	= !isset($data['lineColor'])?'#e3e3e3':$data['lineColor'];
+}
+
+function getTeamAuthorStyle($target, $mother){
+	global $database, $entry;
+	$row = DBQuery::queryRow("SELECT style, image, profile FROM {$database['prefix']}TeamUserSettings WHERE owner=" . getBlogId() . " AND userid=" . $entry['userid']);
+	if($row['style']){
+		$style = explode("|", $row['style']);
+		if($style[0]=="true"){
+			$authorStyle = "font-weight:bold;";
+		}
+		if($style[1]=="true"){
+			$authorStyle .= "font-style:italic;";
+		}
+		if($style[2]=="true"){
+			$authorStyle .= "text-decoration:underline;";
+		}
+		if($style[3]){
+			$authorStyle .= "color:{$style[3]};";
+		}
+		if($style[4]){
+			$authorStyle .= "font-family:{$style[4]};";
+		}
+		if($style[5]){
+			$authorStyle .= "font-size:{$style[5]}pt;";
+		}
+	}
+	$target = "<span style=\"{$authorStyle}\">".$target."</span>";
+	return $target;
+}
+
+function getTeamProfileView($target, $mother){
+	global $suri, $entry, $entryView, $configVal;
+	requireComponent('Textcube.Function.misc');
+	$data = misc::fetchConfigVal($configVal);
+	getTeamBlogInitConfigVal($data);
+	if ($suri['directive'] != "/rss" && $suri['directive'] != "/sync" && $data['p1'] && !$data['p2']) {
+		$target .= getTeamProfile($entry['userid']);
+	}
+	if ($suri['directive'] != "/rss" && $suri['directive'] != "/sync" && $data['p1'] && $data['p2']) {
+		misc::dress('TeamBlogProfileTag', getTeamProfile($entry['userid']), $entryView);
+	}
+	return $target;
+}
+
+function getTeamProfile($userid){
+	global $database, $blogURL, $configVal;
+	requireComponent('Textcube.Function.misc');
+	$data = misc::fetchConfigVal($configVal);
+	getTeamBlogInitConfigVal($data);
+	$row = DBQuery::queryRow("SELECT style, image, profile FROM {$database['prefix']}TeamUserSettings WHERE owner=".getBlogId()." and userid=".$userid);
+	$imageSrc = "{$blogURL}/attach/".getBlogId()."/team/".$row['image'];
+	if($row['image']){
+		$imageTag = "<img src=\"".$imageSrc."\" align=\"top\" />";
+		$imageStyle = "style=\"width:".($data['imageSize']+6)."px; margin-right:10px;\"";
+	}
+	$profile = nl2br(addLinkSense(htmlspecialchars($row['profile']), ' onclick="return openLinkInNewWindow(this)"'));
+	$html  = "<div class=\"teamProfile\">";
+	$html .= $data['postHeader'];
+	$html .= "<div class=\"teamMain\">";
+	$html .= "<div class=\"teamImage\" {$imageStyle}>".$imageTag."</div>";
+	$html .= "<div class=\"teamDesc\">".$profile."</div>";
+	$html .= "</div>";
+	$html .= $data['postFooter'];
+	$html .= "</div>";
+	return $html;
+}
+
+function getTeamBlogSettings() {
+	global $database, $blogURL, $pluginURL, $configVal;
+	requireComponent('Textcube.Function.misc');
+	$data = misc::fetchConfigVal($configVal);
+	getTeamBlogInitConfigVal($data);
+?>
+	<script type="text/javascript" src="<?php echo $pluginURL;?>/plugin-main.js"></script>
+<?php
+	$teamblog_user = DBQuery::queryRow("SELECT name, loginid FROM {$database['prefix']}Users WHERE userid=".getUserId());
+	$row = DBQuery::queryRow("SELECT style, image, profile FROM {$database['prefix']}TeamUserSettings WHERE owner=".getBlogId()." and userid=".getUserId());
+	if(!$row){
+		DBQuery::execute("INSERT INTO {$database['prefix']}TeamUserSettings (owner,userid,style,image,profile,updated) VALUES('".getBlogId()."','".getUserId()."','','', '',UNIX_TIMESTAMP())");
+	}
+	if($row['image']){
+		$image = "{$blogURL}/attach/".getBlogId()."/team/".$row['image'];
+		$imageRemoveCheck = "";
+	}else{
+		$image = "{$blogURL}/image/spacer.gif";
+		$imageRemoveCheck = " disabled ";
+	}
+
+	if($row['style']){
+		$style = explode("|", $row['style']);
+		if($style[0]=="true"){
+			$authorStyle = "font-weight:bold;";
+			$boldCheck = " checked ";
+		}
+		if($style[1]=="true"){
+			$authorStyle .= "font-style:italic;";
+			$italicCheck = " checked ";
+		}
+		if($style[2]=="true"){
+			$authorStyle .= "text-decoration:underline;";
+			$underlineCheck = " checked ";
+		}
+		if($style[3]){
+			$authorStyle .= "color:{$style[3]};";
+			$color = $style[3];
+		}
+		if($style[4]){
+			$authorStyle .= "font-family:{$style[4]};";
+			$family = explode(",",$style[4]);
+			
+		}
+		if($style[5]){
+			$authorStyle .= "font-size:{$style[5]}pt;";
+			$size = $style[5];
+			
+		}
+	}
+	$profile = "";
+	if($row['profile']){
+		$profile = $row['profile'];
+	}
+?>
+	<div id="part-setting-teams" class="part">
+		<h2 class="caption"><span class="main-text"><?php echo _t('팀블로그 정보를 설정합니다');?></span></h2>
+		
+		<div class="data-inbox">
+				<div  class="sectionGroup">
+					<fieldset class="container">
+						<legend><?php echo _t('필명 설정');?></legend>
+						<dl id="team-style-line" class="line">
+							<dt><label for="title"><?php echo _t('필명 스타일');?></label></dt>
+							<dd>by <span id="nicknameStyle" style="<?php echo $authorStyle;?>"><?php echo htmlspecialchars($teamblog_user['name']);?></span></dd>
+							<dd>
+								<span><input <?php echo $boldCheck;?> type="checkbox" value="1" id="fontBold" onclick="styleExecCommand('fontBold', 'fontbold', 'bold');" /> <label for="fontBold"><b>굵게</b></label></span>
+								<span><input <?php echo $italicCheck;?> type="checkbox" id="fontItalic" onclick="styleExecCommand('fontItalic', 'fontitalic', 'italic');" /> <label for="fontItalic"><i>기울임</i></label></span>
+								<span><input <?php echo $underlineCheck;?> type="checkbox" id="fontUnderline" onclick="styleExecCommand('fontUnderline', 'fontunderline', 'underline');" /> <label for="fontUnderline"><u>밑줄</u></label></span>
+								<span id="fontStyleElement">
+									<script type="text/javascript">
+										var colorCheck;
+										var familyCheck;
+										var sizeCheck;
+										var html = ////
+											'<input type="text" id="fontColor" class="input-text2" style="color:<?php echo $color;?>;" value="<?php echo $color;?>" />' + 
+											'<select id="fontColorList" style="width:80px;height:19px;" onchange="styleExecCommand(\'fontColor\', \'fontcolor\', this.value);">' +
+												'<option class="head-option" value="">글자색</option>';
+										for (var i = 0; i < colors.length; ++i) {
+											<?php
+												if($style[3]) echo "colorCheck = (colors[i] == '".str_replace("#","",$color)."')?' selected ':'';";
+											?>
+											html += '<option style="color:#' + colors[i] + ';" value="#' + colors[i] + '" ' + colorCheck + '>#' + colors[i] + '</option>';
+										}
+										html += '</select>';
+										
+										html += ////
+											'<select id="fontFamilyList" style="width:120px;height:19px;" onchange="styleExecCommand(\'\', \'fontname\', this.value); ">' +
+												'<option class="head-option" value="">글자체</option>';
+										var fontset = _t('fontDisplayName:fontCode:fontFamily').split('|');
+										for (var i = 1; i < fontset.length; ++i) {
+											var fontinfo = fontset[i].split(':');
+											if (fontinfo.length != 3) continue;
+											<?php
+												if($style[4]) echo "familyCheck = (fontinfo[1] == ".$family[0].")?' selected ':'';";
+											?>
+											html += '<option style="font-family: \'' + fontinfo[1] + '\';" value="\'' + fontinfo[1] + '\', \'' + fontinfo[2] + '\'"' + familyCheck + '>' + fontinfo[0] + '</option>';
+										}
+										for (var i = 0; i < defaultfonts.length; ++i) {
+											var entry = defaultfonts[i];
+											<?php
+												if($style[4]) echo "familyCheck = (entry[0] == ".$family[0].")?' selected ':'';";
+											?>
+											html += '<option style="font-family: \'' + entry[0] + '\';" value="\'' + entry.join("','") + '\'" ' + familyCheck + '>' + entry[0] + '</option>';
+										}
+										html += '</select>';
+
+										html += ////
+											'<select id="fontSizeList" style="width:50px;height:19px;" onchange="styleExecCommand(\'\', \'fontsize\', this.value); ">' +
+												'<option class="head-option" value="">크기</option>';
+										for (var i = 8; i < 16; ++i) {
+											<?php
+												if($style[5]) echo "sizeCheck = (i == ".$size.")?' selected ':'';";
+											?>
+											html += '<option value="' + i + '" ' + sizeCheck + '>' + i + 'pt</option>';
+										}
+										html +=	'</select>';
+										document.getElementById('fontStyleElement').innerHTML = html;
+									</script>
+								</span>
+							</dd>
+						</dl>
+						<dl>
+							<div class="button-box">
+								<input type="submit" class="save-button input-button" value="<?php echo _t('저장하기');?>" onclick="setStyleSave(); return false;" />
+							</div>
+						</dl>
+						<dl id="team-image-line" class="line">
+							<dt><label for="profile"><?php echo _t('프로필 사진');?></label></dt>
+							<dd>
+								<img id="teamImage" src="<?php echo $image;?>" width="80" height="80" border="1" /><br />(Size : <?php echo $data['imageSize']?> x <?php echo $data['imageSize']?>)<br />
+								<form id="file_upload_form" method="post" target="uploadTarget" enctype="multipart/form-data" action="<?php echo $blogURL;?>/plugin/teamFileUpload/">
+									<input type="hidden" name="type" value="" />
+									<input type="file" name="teamImageFile" id="teamImageFile" size="35" onchange="uploadImage(this.form, 'upload');" /> <a href="http://mypictr.com/?size=<?php echo $data['imageSize']?>x<?php echo $data['imageSize']?>" onclick="window.open(this.href); return false;"><font color="#527A98"><u>mypictr.com</u></font></a>에서 사진 편집.<br />
+									<div class="tip">(찾아보기를 이용하여 사진을 선택하시면 바로 <b>변경</b> 저장됩니다)</div>
+									<div class="tip">
+										<input type="checkbox" name="imageRemove" id="imageRemove" onclick="uploadImage(this.form, 'delete');" <?php echo $imageRemoveCheck;?> />
+										<label for="imageRemove"><?php echo _t('프로필 사진 초기화');?></label>
+									</div>
+									<iframe id="uploadTarget" name="uploadTarget" src="" style="width:0;height:0;border:0px solid;"></iframe>
+								</form>
+						</dd>
+						</dl>
+						<dl id="team-profile-line" class="line">
+							<dt><label for="profile"><?php echo _t('프로필 내용');?></label></dt>
+							<dd>
+								<textarea id="profile" name="profile" cols="15" rows="7"><?php echo htmlspecialchars($profile);?></textarea>
+							</dd>
+						</dl>
+						<dl>
+							<div class="button-box">
+								<input type="submit" class="save-button input-button" value="<?php echo _t('저장하기');?>" onclick="setProfileSave(); return false;" />
+							</div>
+						</dl>
+					</fieldset>
+				</div>
+		</div>
+	</div>
+	
+	<div class="clear"></div>
+
+<?php 
+}
+
+function getTeamContentsSave($target){
+	global $database;
+	$flag = $_POST['flag'];
+	$style = $_POST['fontstyle'];
+	$profile = $_POST['profile'];
+	if(doesHaveOwnership() && doesHaveMembership()){
+		if($flag == "style"){
+			if(DBQuery::execute("UPDATE {$database['prefix']}TeamUserSettings SET style=\"{$style}\", updated=UNIX_TIMESTAMP() WHERE owner=".getBlogId()." and userid=".getUserId())){
+				respondResultPage(0);
+			}
+		}else if($flag == "profile"){
+			$profile = mysql_tt_escape_string(mysql_lessen($profile, 65535));
+			if(DBQuery::execute("UPDATE {$database['prefix']}TeamUserSettings SET profile=\"{$profile}\", updated=UNIX_TIMESTAMP() WHERE owner=".getBlogId()." and userid=".getUserId())){
+				respondResultPage(0);
+			}
+		}
+		respondResultPage(-1);
+	}
+}
+
+function getImageFileUpload($target){
+	global $database;
+	if(doesHaveOwnership() && doesHaveMembership()){
+		$type = $_POST['type'];
+		$file = $_FILES['teamImageFile'];
+		$errcode = 0;
+		if($type == "upload"){
+			$fileExt=Path::getExtension($file['name']);
+			if(($fileExt!='.gif')&&($fileExt!='.jpg')&&($fileExt!='.png')){
+				$errmsg = "잘못된 파일 형식입니다. 다시 시도하세요";
+				$errcode = 1;
+			}else{
+				$result = getAddAttachment($file);
+				$errmsg = "새로운 프로필 사진을 저장 했습니다.";
+			}
+		}else if($type == "delete"){
+			$tmpImage = DBQuery::queryCell("SELECT image FROM {$database['prefix']}TeamUserSettings WHERE owner=".getBlogId()." and userid=".getUserId());
+			if($tmpImage){
+				$result = getDeleteAttachment();
+				$errmsg = "등록된 프로필 사진을 삭제 하였습니다.";
+			}else{
+				$errmsg = "삭제할 파일이 없습니다. 다시 시도하세요";
+				$errcode = 1;
+			}
+		}
+	}
+	
+	$script  = '<script type="text/javascript">';
+	if($errcode != 1){
+		$script .= '	window.parent.top.document.getElementById("teamImage").src = "'.$result.'";';
+	}
+	$script .= '	window.parent.top.PM.showMessage("'.$errmsg.'", "center", "bottom");';
+	$script .= '</script>';
+	echo $script;
+}
+
+function getAddAttachment($file){
+	global $database, $blogURL;
+	requireComponent('Textcube.Function.misc');
+	if(empty($file['name'])||($file['error']!=0))
+		return false;
+	$attachment = array();
+	$attachment['ext'] = misc::getFileExtension(Path::getBaseName($file['name']));
+	$path = ROOT."/attach/".getBlogId()."/team";
+	if(!is_dir($path)){
+		mkdir($path);
+		if(!is_dir($path))
+			return false;
+		@chmod($path,0777);
+	}
+	do{
+		$attachment['name']=rand(1000000000,9999999999).".".$attachment['ext'];
+		$attachment['path']="$path/{$attachment['name']}";
+	}while(file_exists($attachment['path']));
+
+	if(!move_uploaded_file($file['tmp_name'],$attachment['path']))
+		return false;
+	@chmod($attachment['path'],0666);
+	$tmpImage = DBQuery::queryCell("SELECT image FROM {$database['prefix']}TeamUserSettings WHERE owner=".getBlogId()." and userid=".getUserId());
+	if(!DBQuery::execute("UPDATE {$database['prefix']}TeamUserSettings SET image='".$attachment['name']."', updated=UNIX_TIMESTAMP() WHERE owner=".getBlogId()." and userid=".getUserId())){
+		@unlink($attachment['path']);
+		$result = "{$blogURL}/image/spacer.gif";
+	}else{
+		$result = "{$blogURL}/attach/".getBlogId()."/team/".$attachment['name'];
+	}
+	if(!empty($tmpImage))
+	@unlink($path."/".$tmpImage);
+	return $result;
+}
+
+function getDeleteAttachment($filename){
+	global $database, $blogURL;
+	$tmpImage = DBQuery::queryCell("SELECT image FROM {$database['prefix']}TeamUserSettings WHERE owner=".getBlogId()." and userid=".getUserId());
+	if($tmpImage){
+		DBQuery::execute("UPDATE {$database['prefix']}TeamUserSettings SET image='', updated=UNIX_TIMESTAMP() WHERE owner=".getBlogId()." and userid=".getUserId());
+		@unlink(ROOT."/attach/".getBlogId()."/team/".$tmpImage);
+	}
+	$result = "{$blogURL}/image/spacer.gif";
+	return $result;
+}
+
+function getTeamBlogStyle($target) {
+	global $blogURL;
+	$target .= "<link rel=\"stylesheet\" type=\"text/css\" href=\"".$blogURL."/plugin/teamBlogStyle/\" />".CRLF;
+	return $target;
+}
+
+function getTeamBlogStyleSet($target){
+	global $blogURL, $pluginURL, $configVal;
+	requireComponent('Textcube.Function.misc');
+	$data = misc::fetchConfigVal($configVal);
+	getTeamBlogInitConfigVal($data);
+	$lineColor = (strpos($data['lineColor'], "#")===0)?$data['lineColor']:"#".$data['lineColor'];
+	header('Content-type: text/css; charset=utf-8');
+	echo '
+		/* Team Blog Profile CSS Setting */
+		.teamProfile		{ margin:10px 0px 0px 0px;font:8.5pt dotum, AppleGothic;}
+		.teamProfile fieldset{ margin:0px; border:1px solid '.$lineColor.'; padding:0px 0px 6px 0px;}
+
+		.teamProfile legend { font-weight:bold; margin:0 0 0 5px;}
+		*html .teamProfile legend { margin:0; padding:0 !important;}
+		*:first-child+html .teamProfile legend { margin:0; padding:0 !important;}
+		
+		.teamProfile .teamMain  {margin:0px 6px;} 
+		.teamProfile .teamImage {margin:5px 0px 0px 0px; float:'.$data['imagePosition'].';}
+		.teamProfile .teamImage img {padding:2px; border:1px solid '.$lineColor.'; width:'.$data['imageSize'].'px; background-color:#fff;}
+		.teamProfile .teamDesc  {margin:5px 0px 0px 0px; float:'.$data['imagePosition'].';}
+	';
+}
+
+function getTeamBlog_DataSet($DATA){
+	requireComponent('Textcube.Function.misc');
+	$cfg = misc::fetchConfigVal($DATA);
+	return true;
+}
+?>
---
+++ plugins/ST_TeamBlogSettings/plugin-main.js
@@ -0,0 +1,137 @@
+/// Copyright (c) 2004-2007, Needlworks / Tatter Network Foundation
+/// All rights reserved. Licensed under the GPL.
+/// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
+
+var defaultfonts = [
+		['Andale Mono', 'times'],
+		['Arial', 'helvetica', 'sans-serif'],
+		['Arial Black', 'avant garde'],
+		['Book Antiqua', 'palatino'],
+		['Comic Sans MS', 'sand'],
+		['Courier New', 'courier', 'monospace'],
+		['Georgia', 'times new roman', 'times', 'serif'],
+		['Helvetica'],
+		['Impact', 'chicago'],
+		['Symbol'],
+		['Tahoma', 'arial', 'helvetica', 'sans-serif'],
+		['Times New Roman', 'times', 'serif'],
+		['Trebuchet MS', 'geneva'],
+		['Verdana', 'arial', 'helvetica', 'sans-serif']
+	];
+var colors = ['666666', '008000', '009966', '99CC66', '999966', 'CC9900', 'D41A01',
+				  'FF0000', 'FF7635', 'FF9900', 'FF3399', '9B18C1', '993366',
+				  '666699', '0000FF', '177FCD', '006699', '003366', '333333',
+				  '000000', '8E8E8E', 'C1C1C1', 'FFFFFF', 'FFDAED', 'C9EDFF',
+				  'D0FF9D', 'FAFFA9', 'E4E4E4'];
+
+function styleExecCommand(objname, flag, style){
+	var obj1 = document.getElementById('nicknameStyle');
+	var obj2 = (objname)?document.getElementById(objname):"";
+	if(flag == 'fontcolor'){
+		obj1.style.color = style;
+		obj2.value = style;
+		obj2.style.color = style;
+	}else if(flag == 'fontname'){
+		obj1.style.fontFamily = style;
+	}else if(flag == 'fontsize'){
+		obj1.style.fontSize = style + 'pt';
+	}else if(flag == 'fontbold'){
+		if(obj1.style.fontWeight){
+			obj1.style.fontWeight = '';
+			obj2.style.checked = false;
+		}else{
+			obj1.style.fontWeight = style;
+			obj2.style.checked = true;
+		}
+	}else if(flag == 'fontitalic'){
+		if(obj1.style.fontStyle){
+			obj1.style.fontStyle = '';
+			obj2.style.checked = false;
+		}else{
+			obj1.style.fontStyle = style;
+			obj2.style.checked = true;
+		}
+	}else if(flag == 'fontunderline'){
+		if(obj1.style.textDecoration){
+			obj1.style.textDecoration = '';
+			obj2.style.checked = false;
+		}else{
+			obj1.style.textDecoration = style;
+			obj2.style.checked = true;
+		}
+	}else if(flag == 'teamimage'){
+		obj2.src = style;
+	}
+}
+function removeFormatting() {
+	var str = document.getElementById('nicknameStyle').innerHTML;
+	var styleTags = new Array("b", "strong", "i", "em", "u", "ins", "strike", "del", "font");
+	for(var i in styleTags) {
+		var regTag = new RegExp("</?" + styleTags[i] + "(?:>| [^>]*>)", "i");
+		while(result = regTag.exec(str))
+			str = str.replaceAll(result[0], "");
+	}
+	str = str.replace(new RegExp('\\s*style="[^"]*"', "gi"), "");
+	var styleContainers = new Array("span", "div");
+	for(var i in styleContainers) {
+		var regTag = new RegExp("<span\\s*?>((?:.|\\s)*?)</span>", "i");
+		while(result = regTag.exec(str))
+			str = str.replace(result[0], result[1]);
+	}
+	document.getElementById('nicknameStyle').innerHTML = str;
+}
+
+function uploadImage(frm, type) {
+	frm.type.value = type;
+	if(type == 'upload'){
+		frm.submit();
+		frm.imageRemove.disabled = false;
+	}else{
+		if(confirm('정말 삭제하시겠습니까?')){
+			frm.submit();
+			frm.imageRemove.disabled = true;
+			frm.imageRemove.checked = false;
+		}else{
+			frm.imageRemove.checked = false;
+			return false;
+		}
+	}
+	frm.reset();
+}
+
+function setStyleSave() {
+	try {
+		var fontBold = document.getElementById('fontBold').checked;
+		var fontItalic = document.getElementById('fontItalic').checked;
+		var fontUnderline = document.getElementById('fontUnderline').checked;
+		var fontColor = document.getElementById('fontColor').value;
+		var fontFamilyList = document.getElementById('fontFamilyList').value;
+		var fontSizeList = document.getElementById('fontSizeList').value;
+		var queryString = "flag=style&fontstyle=" + encodeURIComponent(fontBold + "|" + fontItalic + "|" + fontUnderline + "|" + fontColor + "|" + fontFamilyList + "|" + fontSizeList);
+
+		var request = new HTTPRequest("POST", blogURL + "/plugin/teamContentsSave/");
+		request.onSuccess = function () {
+			PM.showMessage("필명 스타일이 저장 되었습니다.", "center", "bottom");
+		}
+		request.onError = function() {
+			PM.showMessage("저장하지 못했습니다. 다시 시도 해주세요.", "center", "bottom");
+		}
+		request.send(queryString);
+	} catch(e) {}
+}
+
+function setProfileSave() {
+	try {
+		var profile = document.getElementById('profile').value;
+		var queryString = "flag=profile&profile=" + encodeURIComponent(profile);
+
+		var request = new HTTPRequest("POST", blogURL + "/plugin/teamContentsSave/");
+		request.onSuccess = function () {
+			PM.showMessage("프로필 설명이 저장 되었습니다.", "center", "bottom");
+		}
+		request.onError = function() {
+			PM.showMessage("저장하지 못했습니다. 다시 시도 해주세요.", "center", "bottom");
+		}
+		request.send(queryString);
+	} catch(e) {}
+}
