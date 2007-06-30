b3f893dd87daa4148230afa2849705a8a30f58e6
inureyes <inureyes@gmail.com>
2007-06-30 04:24:03 +0000

    #438   * 관리자 화면에서의 처리.   * http://forum.tattersite.com/ko/viewtopic.php?id=1706

--- blog/owner/index.php
+++ blog/owner/index.php
@@ -2,6 +2,6 @@
 /// Copyright (c) 2004-2007, Needlworks / Tatter Network Foundation
 /// All rights reserved. Licensed under the GPL.
 /// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
-$url = rtrim(isset($_SERVER['REDIRECT_URL']) ? $_SERVER['REDIRECT_URL'] : $_SERVER['SCRIPT_NAME'], '/');
+$url = rtrim(isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : $_SERVER['SCRIPT_NAME'], '/');
 header("Location: $url/center/dashboard");
 ?>
