350e2d8ee43c366f2f24926b49a2e536e0f3f3b2
inureyes <inureyes@gmail.com>
2007-08-10 13:58:23 +0000

    #283  * trunk에 추가.

---
+++ blog/pluginForOwner/index.php
@@ -0,0 +1,13 @@
+<?php
+/// Copyright (c) 2004-2007, Needlworks / Tatter Network Foundation
+/// All rights reserved. Licensed under the GPL.
+/// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
+define('ROOT', '../..');
+require ROOT . '/lib/includeForBlogOwner.php';
+if (false) {
+	fetchConfigVal();
+}
+fireEvent($suri['directive'] . '/' . $suri['value']);
+if (!headers_sent())
+	respondNotFoundPage();
+?>
