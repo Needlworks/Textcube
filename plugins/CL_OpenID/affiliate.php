3594ba50e908ee545aa8e6c436c51d3c282b76c7
coolengineer <hojin.choi@gmail.com>
2007-12-11 16:52:02 +0000

    #378  다중 블로그로 설치된 경우, 오픈아이디란? 링크로 들어가 로그인이 안되는 버그 수정

--- plugins/CL_OpenID/affiliate.php
+++ plugins/CL_OpenID/affiliate.php
@@ -11,8 +11,8 @@ $another_config = str_replace( $another_config, ".php", ".local.php" );
 if( file_exists( $another_config ) ) {
 	require( $another_config );
 } else {
-	global $serviceURL;
-	$_try_auth_url = $serviceURL . "/plugin/openid/try_auth?redirect=$requestURI";
+	global $hostURL, $blogURL;
+	$_try_auth_url = $hostURL . $blogURL . "/plugin/openid/try_auth?redirect=$requestURI";
 	$_op_base = "http://www.idtail.com";
 	$_encoded_args      = base64_encode( "login_url:" . $_try_auth_url );
 	$openid_help_link   = $_op_base . "/affiliate/help/textcube/" . $_encoded_args;
