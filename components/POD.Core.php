bc2b870fd9e0b103c4336da5e29b958827cd6815
inureyes <inureyes@gmail.com>
2007-12-20 15:46:44 +0000

    #759  * POD 버전 업데이트 (0.19a)

--- components/POD.Core.php
+++ components/POD.Core.php
@@ -1,9 +1,9 @@
 <?php
-// POD : PHP Ontology-based Data model/framework
-// Version 0.14a
+// POD : PHP Ontology(or Object-Oriented)-based Data model/framework
+// Version 0.19a
 // By Jeongkyu Shin (jkshin@ncsl.postech.ac.kr)
 // Created       : 2007.11.30
-// Last modified : 2007.12.14
+// Last modified : 2007.12.21
 
 // (C) 2007 Jeongkyu Shin. All rights reserved. 
 // Licensed under the GPL.
@@ -13,9 +13,9 @@
 require_once 'Needlworks.Database.php';
 
 // Bypass variables are supported. ($_pod_setting);
-class POD extends DBQuery{
-	/** Pre-definition **/ 
-	var $_domain;
+class POD extends DBQuery {
+	/** Pre-definition **/
+/*	var $_domain;
 	var $_prototype;
 	var $_type;
 	var $_version;
@@ -25,7 +25,7 @@ class POD extends DBQuery{
 	var $_classPointer;
 	var $_foreignKeyId;
 	var $_isCheckout;
-	var $_DBMS;
+	var $_DBMS;*/
 
 	/** Initialization **/
 	function POD($domain = null, $type = null, $prefix = '') {
@@ -47,8 +47,8 @@ class POD extends DBQuery{
 		unset($this);
 	}
 
-	/** Model **/
-	function define() {
+	/** Definition **/
+	function define($rewriteTreeOnly = false) {
 		/*** TO DO : Upgrade current table schemes into newer one.***/
 		if(empty($this->_version) || empty($this->_prototype)) return $this->_false();
 		$clause = 'CREATE TABLE '.$this->_tablePrefix.$this->_prototype.' (';
@@ -71,12 +71,18 @@ class POD extends DBQuery{
 
 		$clause = rtrim($clause, ',').')'.$this->_setCharset();
 		var_dump($clause);
-		if($this->execute($clause)) {
+		if($rewriteTreeOnly != false) {
 			$this->_structure[$this->domain()] = $this->_queue[$this->domain()];
 			$this->_saveStructure();
+			return true;
+		} else if($this->execute($clause)) {
+			$this->_structure[$this->domain()] = $this->_queue[$this->domain()];
+			$this->_saveStructure();
+			return true;
 		}
 		else return $this->_false();
 	}
+	
 	function purge() {
 		if(empty($this->_prototype)) return $this->_false();
 		$this->execute("DROP TABLE ".$this->_tablePrefix.$this->_prototype);
@@ -98,9 +104,11 @@ class POD extends DBQuery{
 		unset($this->_structure[$this->domain()][$this->type()]);
 		$this->_saveStructure();
 	}
+	
 	function alter() {
 		return _alterStructure();
 	}
+	
 	function reset() {
 		$this->_structure = $this->_queue = $this->_loadStructure();
 		$this->_instances =
@@ -113,69 +121,21 @@ class POD extends DBQuery{
 		$this->_foreignKeyId = null;
 		$this->_isCheckout = false;
 	}
+	
 	function structure() {
 		return $this->_structure[$this->domain()][$this->type()];
 	}
-	function _loadStructure() {
-		if(empty($this->_structure)) {
-			$structure = $this->queryCell("SELECT value FROM ".$this->_tablePrefix."opd WHERE name = 'structure'");
-			if(!empty($structure)) $this->_structure = unserialize($structure);
-			else {
-				$this->_structure = array();
-			}
-		}
-		return $this->_structure;
-	}
-
-	function _saveStructure() {
-		if($this->execute("REPLACE INTO ".$this->_tablePrefix."opd SET name = 'structure', value = '".serialize($this->_structure)."'") == false) {
-			$this->execute("CREATE TABLE ".$this->_tablePrefix."opd (name varchar(32) NOT NULL DEFAULT '', value text NOT NULL, PRIMARY KEY (name)) TYPE=MyISAM");
-			return $this->execute("REPLACE INTO ".$this->_tablePrefix."opd SET name = 'structure', value = '".serialize($this->_structure)."'");
-		} else return true;
-	}
-
-	/** Type **/
-
-	function alias($instance, $alias) {
-//		if(!$this->_validateInstance($instance)) return $this->_false();
-		$this->_aliases[$instance] = $alias;
-		return true;
-	}
-
-	/** View **/
-	function valueType($instance) {
-	}
-	function valueCardinality($instance) {
-		if(empty($this->_repository) || empty($this->_repository[0][$instance])) return null;
-		if($this->_numberOfClasses == 0) return $this->_false();
-		$column = array();
-		foreach($this->_repository as $class) {
-			array_push($column, $class[$instance]);
-		}
-		return count(array_count_values($column));
-	}
-	function cardinality() {
-		return $this->_numberOfClasses;
-	}
-	function allValues($instance = null){
-		if($instance == null) return $this->_repository;
-		if(!$this->_validateInstance($instance)) return $this->_false();
-		$column = array();
-		foreach($this->_repository as $repos) {
-			if($repos[$instance] == null && $this->aliases[$instance] != null)
-				array_push($column, $repos[$this->aliases[$instance]]);
-			else array_push($column, $repos[$instance]);
-		}
-		return $column;
-	}
+	
 	function superClass() {
 		if($this->type()==null) return null;
 		else return $this->_structure[$this->domain()][$this->type()]['superclasses'];
 	}
+	
 	function subClass() {
 		if($this->type()==null) return null;
 		else return $this->_structure[$this->domain()][$this->type()]['subclasses'];
 	}
+	
 	function documentation() {
 	}
 	
@@ -188,6 +148,7 @@ class POD extends DBQuery{
 		if(empty($this->_domain)) return 'default';
 		else return $this->_domain;
 	}
+	
 	function type($type = null) {
 		if(!empty($type)) {
 			$this->_type = $type;
@@ -196,11 +157,13 @@ class POD extends DBQuery{
 		if(empty($this->_type)) return null;
 		else return $this->_type;
 	}
+	
 	function version($version = null) {
 		if(!empty($version)) $this->_version = $version;
 		if($this->type()==null) return null;
 		else return $this->_structure[$this->domain()][$this->type()]['version'];
 	}
+	
 	function setInstance($instance, $attribute = null, $additional = null) {
 		$this->_queue[$this->domain()][$this->type()]['instances'][$instance] = array();
 		if($attribute!=null) $this->_queue[$this->domain()][$this->type()]['instances'][$instance]['attribute'] = $attribute;
@@ -209,6 +172,7 @@ class POD extends DBQuery{
 
 	function isProducedBy() {
 	}
+	
 	function subClassOf($type) {
 		if($this->_queue[$this->domain()][$this->type()]['superclasses'] == null)
 			$this->_queue[$this->domain()][$this->type()]['superclasses'] = array();
@@ -226,6 +190,7 @@ class POD extends DBQuery{
 		}
 		return true;
 	}
+	
 	function superClassOf($type) {
 		if($this->_queue[$this->domain()][$this->type()]['subclasses'] == null)
 			$this->_queue[$this->domain()][$this->type()]['subclasses'] = array();
@@ -245,6 +210,7 @@ class POD extends DBQuery{
 		}
 		return true;
 	}
+	
 	function relation($type, $field, $matches) {
 		// Every elements should exist before marking relation.
 		if($this->_queue[$this->domain()][$type] == null) return $this->_false();
@@ -263,64 +229,8 @@ class POD extends DBQuery{
 		$this->_queue[$this->domain()][$type]['relations'][$this->type()][$matches] = $field;
 		return true;
 	}
-	function hasValue($instances) {
-	}
-	function hasAttribute() {
-	}
-	function hasOneOf() {
-	}
-	function onto() {
-	}
-	function totalOn() {
-	}
-	function merge($type) {
-		if(!$this->_validateType($type)) return $this->_false();
-		$this->_mergeWith[$type] = true; // TO DO : Need to validate $relation.
-		return true;
-	}
-	function refer($type, $condition = null) {
-		if(!$this->_validateType($type)) return $this->_false();
-		$this->_references[$type] = true;
-		if($condition != null) $this->restrict($type, $condition);
-		return true;
-	}
-	function duplicate() {
-	}
-	function restrict($type, $condition = null) {
-		if(!$this->_validateType($type)) return $this->_false();
-		if($this->_references[$type] == null) return $this->_false();
-		if($condition == null) {$this->_references[$type] = true; return $this->_false();}
-		if($this->_references[$type] === true || empty($this->_references[$type])) $item = array();
-		else $item = $this->_references[$type];
-		foreach(explode(',',$condition) as $cond) { array_push($item, $cond);}
-		$this->_references[$type] = $item;
-		return true;
-	}
-	function projection() {
-	}
-	function composition() {
-	}
-	function rangeOf($instance, $range) {
-		array_push($this->_conditions, $instance.$range);
-		return true;
-	}
-	function relatedTo($instance1, $instance2, $relation = '=') {
-		switch(strtolower($relation)) {
-			case 'req': $relation = '=<'; break;
-			case 'leq': $relation = '>='; break;
-			case 'eq': $relation = '='; break;
-			default: break;
-		}
-		array_push($this->_conditions, $instance1.$relation.$instance2);
-		return true;
-	}
-	function slotCardinality() {
-	}
-	function classPartition() {
-	}
-	function sameSlotValues() {
-	}
-	/** Properties **/
+
+	/** Attribute / View **/
 	function instance($instances) {
 		if(func_num_args() == 1) {
 			$values = func_get_arg(0);
@@ -334,6 +244,7 @@ class POD extends DBQuery{
 		}
 		return true;
 	}
+	
 	function allInstances($instances = null) {
 		if($instances != null) {
 			$instance = explode(',',$instances);
@@ -345,8 +256,54 @@ class POD extends DBQuery{
 		}
 		return $this->_instances;
 	}
+	
+	function valueType($instance) {
+	}
+	
+	function valueCardinality($instance) {
+		if(empty($this->_repository) || empty($this->_repository[0][$instance])) return null;
+		if($this->_numberOfClasses == 0) return $this->_false();
+		$column = array();
+		foreach($this->_repository as $class) {
+			array_push($column, $class[$instance]);
+		}
+		return count(array_count_values($column));
+	}
+	
+	function cardinality() {
+		return $this->_numberOfClasses;
+	}
+	
+	function allValues($instance = null){
+		if($instance == null) return $this->_repository;
+		if(!$this->_validateInstance($instance)) return $this->_false();
+		$column = array();
+		foreach($this->_repository as $repos) {
+			if($repos[$instance] == null && $this->aliases[$instance] != null)
+				array_push($column, $repos[$this->aliases[$instance]]);
+			else array_push($column, $repos[$instance]);
+		}
+		return $column;
+	}
+	
+	function hasValue($instances) {
+	}
+	
+	function hasAttribute() {
+	}
+	
+	function hasOneOf() {
+	}
+	
+	function onto() {
+	}
+	
+	function totalOn() {
+	}
+
 	function setType() {
 	}
+	
 	function commit() {
 		if(empty($this->_prototype)) return $this->_false();
 		if($this->_isCheckout == true) {	// Update case.
@@ -369,6 +326,7 @@ class POD extends DBQuery{
 		var_dump($clause);
 		return $this->execute($clause);
 	}
+	
 	function checkout($instances = '*') {
 		if(empty($this->_prototype)) return $this->_false();
 		if($instances != '*') {
@@ -429,6 +387,7 @@ class POD extends DBQuery{
 		}
 		return $this->_false();
 	}
+	
 	function remove() {
 		if(empty($this->_prototype)) return $this->_false();
 		// Find field pairs.
@@ -447,6 +406,68 @@ class POD extends DBQuery{
 		}
 		return $this->_false();
 	}
+		
+	
+	/** Relations **/
+	function rangeOf($instance, $range) {
+		array_push($this->_conditions, $instance.$range);
+		return true;
+	}
+	
+	function relatedTo($instance1, $instance2, $relation = '=') {
+		switch(strtolower($relation)) {
+			case 'req': $relation = '=<'; break;
+			case 'leq': $relation = '>='; break;
+			case 'eq': $relation = '='; break;
+			default: break;
+		}
+		array_push($this->_conditions, $instance1.$relation.$instance2);
+		return true;
+	}
+	
+	function slotCardinality() {
+	}
+	
+	function classPartition() {
+	}
+	
+	function sameSlotValues() {
+	}
+
+	/** Extensions **/
+	function merge($type) {
+		if(!$this->_validateType($type)) return $this->_false();
+		$this->_mergeWith[$type] = true; // TO DO : Need to validate $relation.
+		return true;
+	}
+	
+	function refer($type, $condition = null) {
+		if(!$this->_validateType($type)) return $this->_false();
+		$this->_references[$type] = true;
+		if($condition != null) $this->restrict($type, $condition);
+		return true;
+	}
+	
+	function duplicate() {
+	}
+	
+	function restrict($type, $condition = null) {
+		if(!$this->_validateType($type)) return $this->_false();
+		if($this->_references[$type] == null) return $this->_false();
+		if($condition == null) {$this->_references[$type] = true; return $this->_false();}
+		if($this->_references[$type] === true || empty($this->_references[$type])) $item = array();
+		else $item = $this->_references[$type];
+		foreach(explode(',',$condition) as $cond) { array_push($item, $cond);}
+		$this->_references[$type] = $item;
+		return true;
+	}
+	
+	function projection() {
+	}
+	
+	function composition() {
+	}
+	
 	function shift() {
 		if(($this->_classPointer+1) >= $this->_numberOfClasses) {
 			return $this->_false(); // End of pointer.
@@ -457,7 +478,7 @@ class POD extends DBQuery{
 		}
 		return true;
 	}
-
+	
 	function unshift() {
 		if(($this->_classPointer-1) < 0) return $this->_false(); // End of pointer.
 		$this->_classPointer -= 1;
@@ -475,8 +496,34 @@ class POD extends DBQuery{
 
 	function range() {
 	}
+	function alias($instance, $alias) {
+//		if(!$this->_validateInstance($instance)) return $this->_false();
+		$this->_aliases[$instance] = $alias;
+		return true;
+	}
 
-	/** subfunction part **/
+	/** subroutine part **/
+	/** They should be 'private', however PHP4 does not support private.
+	    To support PHP4, I left them as public with prefix '_' **/
+
+	function _loadStructure() {
+		if(empty($this->_structure)) {
+			$structure = $this->queryCell("SELECT value FROM ".$this->_tablePrefix."opd WHERE name = 'structure'");
+			if(!empty($structure)) $this->_structure = unserialize($structure);
+			else {
+				$this->_structure = array();
+			}
+		}
+		return $this->_structure;
+	}
+
+	function _saveStructure() {
+		if($this->execute("REPLACE INTO ".$this->_tablePrefix."opd SET name = 'structure', value = '".serialize($this->_structure)."'") == false) {
+			$this->execute("CREATE TABLE ".$this->_tablePrefix."opd (name varchar(32) NOT NULL DEFAULT '', value text NOT NULL, PRIMARY KEY (name)) TYPE=MyISAM");
+			return $this->execute("REPLACE INTO ".$this->_tablePrefix."opd SET name = 'structure', value = '".serialize($this->_structure)."'");
+		} else return true;
+	}
+
 	function _clause() {
 		$condition = '';
 		$baseMarkup = '';
@@ -510,6 +557,7 @@ class POD extends DBQuery{
 		$condition .= ' ('.implode(',',array_keys($this->_instances)).') VALUES ('.implode(',',$this->_instances).')';
 		return $condition;
 	}
+	
 	function _updateClause() {
 		$condition = ' SET ';
 		if(!isset($this->_foreignKeyId)) {
@@ -522,9 +570,13 @@ class POD extends DBQuery{
 		$condition .= " WHERE pod_fid = ".$this->_foreignKeyId;
 		return $condition;
 	}
+	
 	function _setCharset() {
-		return ' DEFAULT CHARSET=utf8';
+		if($this->charset())
+			return ' DEFAULT CHARSET='.$this->charset();
+		else return '';
 	}
+	
 	function setIndex($indexName, $instances = null, $primary = null) {
 		if(empty($instances)) $instances = $indexName;
 		$indexSet = explode(',',$instances);
@@ -534,15 +586,18 @@ class POD extends DBQuery{
 			$this->_queue[$this->domain()][$this->type()]['indexes'][$indexName] = $indexSet;
 		}
 	}
+	
 	function _validateInstance($instance) {
 		if($this->_structure[$this->domain()][$this->type()]['instances'][$instance] == null) return $this->_false();
 		if($this->_repository[0][$instance] == null) return $this->_false();
 		return true;
 	}
+	
 	function _validateType($type) {
 		if($this->_structure[$this->domain()][$type] == null) return $this->_false();
 		return true;
 	}
+	
 	function _alterStructure() {
 		if($this->_version <= $this->_structure[$this->domain()][$this->type()]['version']) return $this->_false();
 		if(empty($this->_queue)) return $this->_false();
@@ -603,10 +658,13 @@ class POD extends DBQuery{
 		}
 		return $this->_false();	
 	}
+	
 	function _getNewKeyId() {
 		return $this->queryCell("SELECT max(pod_fid) FROM ".$this->_tablePrefix.$this->type()) + 1;
 	}
+	
 	function _false($err = null) {
 		return false;
 	}
 }
+?>
