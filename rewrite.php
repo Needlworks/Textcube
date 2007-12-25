949555d21c0a75e056d537e9637b39825696a182
inureyes <inureyes@gmail.com>
2007-12-25 08:16:24 +0000

    #718  * rewrite 처리 모듈 루틴 수정  * 크리티컬 업데이트였으므로 버전 번호 수정

--- rewrite.php
+++ rewrite.php
@@ -2,12 +2,9 @@
 /// Copyright (c) 2004-2007, Needlworks / Tatter Network Foundation
 /// All rights reserved. Licensed under the GPL.
 /// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
-
 	$accessInfo = array(
 		'host'     => $_SERVER['HTTP_HOST'],
 		'fullpath' => $_SERVER["REQUEST_URI"],
-		//'input'    => (isset($_SERVER['REDIRECT_QUERY_STRING']) ? $_SERVER['REDIRECT_QUERY_STRING'] : $_SERVER['QUERY_STRING']),
-		//'input'    => ltrim($_SERVER['REQUEST_URI'],'/'),
 		'position' => $_SERVER["SCRIPT_NAME"],
 		'root'     => str_replace('rewrite.php','',$_SERVER["SCRIPT_NAME"])
 		);
@@ -16,28 +13,28 @@
 	require ROOT.'/config.php';
 	$part = strtok($accessInfo['input'],'/');
 	if(in_array($part, array('image','plugins','script','skin','style','attach','cache','thumbnail'))) {
-		$content = file_get_contents(ltrim(($part == 'thumbnail' ? str_replace('thumbnail','cache/thumbnail',$accessInfo['root'].$accessInfo['input']) : $accessInfo['root'].$accessInfo['input']),'/'));
-		if(!empty($content)) { echo $content; exit;}
+		$file = @file_get_contents(ltrim(($part == 'thumbnail' ? preg_replace('/thumbnail/','cache/thumbnail',$accessInfo['root'].$accessInfo['input'],1) : $accessInfo['root'].$accessInfo['input']),'/'));
+		if(!empty($file)) { echo $file; exit;}
 		else exit;
 	}
 	$accessInfo['URLfragment'] = explode('/',$accessInfo['input']);
-	$lastElm = array_slice($accessInfo['URLfragment'], -1, 1);
 	switch ($service['type']) {
 		case 'path' : // For path-based multi blog.
 			array_splice($accessInfo['URLfragment'],0,1); 
 			$pathPart = ltrim(rtrim(strtok(strstr($accessInfo['input'],'/'), '?'),'/'),'/');
 			break;
-		case 'domain' : 	case 'single' : 	default : 
+		case 'domain' :	case 'single' :	default : 
 			$pathPart = ltrim(rtrim(strtok($accessInfo['fullpath'], '?'),'/'),'/');
 			break;
 	}
 	if(!empty($accessInfo['URLfragment']) && in_array($accessInfo['URLfragment'][0],array('entry','notice','location','cover','attachment','category','keylog','tag','search','plugin','author'))) {
 		$interfacePath = 'blog/'.$accessInfo['URLfragment'][0].'/index.php';
-	} else if(is_numeric(strtok($lastElm[0], '?'))) {
-		$pathPart = strtok(implode('/',array_slice($accessInfo['URLfragment'],0,count($accessInfo['URLfragment'])-1)), '&');
+	} else if(is_numeric(strtok(end($accessInfo['URLfragment']), '?'))) {
+		$pathPart = strtok(implode('/',array_slice($accessInfo['URLfragment'],0,count($accessInfo['URLfragment'])-1)), '?');
 		$interfacePath = 'blog/'.(empty($pathPart) ? '' : $pathPart.'/').'item.php';
 	} else {
 		$interfacePath = 'blog/'.(empty($pathPart) ? '' : $pathPart.'/').'index.php';
 	}
-	require $interfacePath;
+	unset($pathPart,$part);
+	include_once $interfacePath;
 ?>
