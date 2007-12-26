a0fc838bddb29fc6afbff86ad86b7f75caaaaf99
inureyes <inureyes@gmail.com>
2007-12-26 03:50:50 +0000

    #777  * 주석 추가

--- blog/checkup/index.php
+++ blog/checkup/index.php
@@ -2,6 +2,8 @@
 /// Copyright (c) 2004-2007, Needlworks / Tatter Network Foundation
 /// All rights reserved. Licensed under the GPL.
 /// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
+
+// Symbolic link to support migration from Textcube 1.5 to 1.6
 define('ROOT', '../..');
 require ROOT . '/interface.blog/checkup/index.php';
 ?>
