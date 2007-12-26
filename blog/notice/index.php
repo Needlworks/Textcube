acf306fa61c4e9f2321b3f1bc4c4174c9d47776e
inureyes <inureyes@gmail.com>
2007-12-26 03:34:40 +0000

    #718  * notice의 경우 글번호를 사용하므로 id를 처리하도록 수정.

--- blog/notice/index.php
+++ blog/notice/index.php
@@ -8,10 +8,30 @@ if (false) {
 	fetchConfigVal();
 }
 
-list($entries, $paging) = getEntriesWithPagingByNotice($blogid, $suri['page'], $blog['entriesOnPage']);
-fireEvent('OBStart');
-require ROOT . '/lib/piece/blog/begin.php';
-require ROOT . '/lib/piece/blog/entries.php';
-require ROOT . '/lib/piece/blog/end.php';
-fireEvent('OBEnd');
+if(isset($suri['id'])) {
+	list($entries, $paging) = getEntryWithPaging($blogid, $suri['id'], true);
+	fireEvent('OBStart');
+	require ROOT . '/lib/piece/blog/begin.php';
+	if (empty($entries)) {
+		header('HTTP/1.1 404 Not Found');
+		if (empty($skin->pageError)) {
+			dress('article_rep', '<div style="text-align:center;font-size:14px;font-weight:bold;padding-top:50px;margin:50px 0;color:#333;background:url(' . $service['path'] . '/image/warning.gif) no-repeat top center;">' . _text('존재하지 않는 페이지입니다.') . '</div>', $view);
+		} else {
+			dress('article_rep', NULL, $view);
+			dress('page_error', $skin->pageError, $view);
+		}
+		unset($paging);
+	} else {
+		require ROOT . '/lib/piece/blog/entries.php';
+	}
+	require ROOT . '/lib/piece/blog/end.php';
+	fireEvent('OBEnd');
+} else {
+	list($entries, $paging) = getEntriesWithPagingByNotice($blogid, $suri['page'], $blog['entriesOnPage']);
+	fireEvent('OBStart');
+	require ROOT . '/lib/piece/blog/begin.php';
+	require ROOT . '/lib/piece/blog/entries.php';
+	require ROOT . '/lib/piece/blog/end.php';
+	fireEvent('OBEnd');
+}
 ?>
--- blog/notice/item.php
+++ blog/notice/item.php
@@ -2,27 +2,5 @@
 /// Copyright (c) 2004-2007, Needlworks / Tatter Network Foundation
 /// All rights reserved. Licensed under the GPL.
 /// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
-define('ROOT', '../..');
-require ROOT . '/lib/includeForBlog.php';
-if (false) {
-	fetchConfigVal();
-}
-
-list($entries, $paging) = getEntryWithPaging($blogid, $suri['id'], true);
-fireEvent('OBStart');
-require ROOT . '/lib/piece/blog/begin.php';
-if (empty($entries)) {
-	header('HTTP/1.1 404 Not Found');
-	if (empty($skin->pageError)) {
-		dress('article_rep', '<div style="text-align:center;font-size:14px;font-weight:bold;padding-top:50px;margin:50px 0;color:#333;background:url(' . $service['path'] . '/image/warning.gif) no-repeat top center;">' . _text('존재하지 않는 페이지입니다.') . '</div>', $view);
-	} else {
-		dress('article_rep', NULL, $view);
-		dress('page_error', $skin->pageError, $view);
-	}
-	unset($paging);
-} else {
-	require ROOT . '/lib/piece/blog/entries.php';
-}
-require ROOT . '/lib/piece/blog/end.php';
-fireEvent('OBEnd');
+require 'index.php';
 ?>
