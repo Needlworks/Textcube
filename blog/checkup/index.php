03fe4085f591f6aa3b56f36ea4f099752da61729
inureyes <inureyes@gmail.com>
2007-12-26 03:50:08 +0000

    #777  * 1.5 -> 1.6의 마이그레이션을 위한 심볼릭 링크 추가

---
+++ blog/checkup/index.php
@@ -0,0 +1,7 @@
+<?php
+/// Copyright (c) 2004-2007, Needlworks / Tatter Network Foundation
+/// All rights reserved. Licensed under the GPL.
+/// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
+define('ROOT', '../..');
+require ROOT . '/interface.blog/checkup/index.php';
+?>
