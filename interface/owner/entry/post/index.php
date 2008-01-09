97173caedddb16fca2fc52e235627d0b13bd5876
inureyes <inureyes@gmail.com>
2008-01-09 07:00:43 +0000

    #718  * 글쓰기가 안되는 문제 수정

--- interface/owner/entry/post/index.php
+++ interface/owner/entry/post/index.php
@@ -3,5 +3,5 @@
 /// All rights reserved. Licensed under the GPL.
 /// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
 define('__TEXTCUBE_POST__', true);
-require ROOT. '/interface/owner/entry/edit/item.php';
+require ROOT. '/interface/owner/entry/edit/index.php';
 ?>
