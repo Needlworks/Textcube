ed71d14857355e99d6264f57bef8cb2057c8dd60
coolengineer <hojin.choi@gmail.com>
2008-01-30 17:38:20 +0000

    * #817  * lunamoth 님 의견 반영  * http://dev.textcube.org/ticket/817#comment:7

--- interface/owner/control/system/index.php
+++ interface/owner/control/system/index.php
@@ -2,26 +2,90 @@
 /// Copyright (c) 2004-2008, Needlworks / Tatter Network Foundation
 /// All rights reserved. Licensed under the GPL.
 /// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
-$IV = array(
-	'GET' => array(
-		'page' => array('number','min'=>1,'default'=>1)
-	) 
-);
-
-$page=(isset($_GET['page']) && $_GET['page'] >= 1 ? $_GET['page'] : 1 );
-
-
-
-$service['admin_script']='control.js';
 
 require ROOT . '/lib/includeForBlogOwner.php';
 require ROOT . '/lib/piece/owner/header.php';
 require ROOT . '/lib/piece/owner/contentMenu.php';
 
-global $blogURL;
-$page = $_GET['page'];
+function getSymbolByQuantity($bytes) {
+	$symbols = array('B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB');
+	$exp = floor(log($bytes)/log(1024));
 
+	return sprintf('%.2f '.$symbols[$exp], ($bytes/pow(1024, floor($exp))));
+}
+
+/* Current time */
+$serverTime = strftime( "Server Time: %Y-%m-%d %H:%M:%S %z (%Z)", time() );
+
+/* Database version */
+$dbVersion = '';
+if( $service['dbms'] == 'mysql' ) {
+	$dbVersion = POD::queryColumn("SELECT VERSION()");
+	$dbVersion = "MySQL: " . $dbVersion[0];
+	$dbStat = preg_replace( "/\s{2,}/", "<br />", mysql_stat());
+}
+
+/* Webserver information */
+$webServer = "Unknown";
+if( function_exists('apache_get_version') ) {
+	$webServer = apache_get_version();
+}
+
+/* OS version */
+$osVersion = "Unknown";
+if( function_exists('php_uname') ) {
+	$osVersion = php_uname();
+}
+
+/* Disk space */
+$totalSpace = getSymbolByQuantity( disk_total_space( dirname( __FILE__ ) ) );
+$freeSpace = getSymbolByQuantity( disk_free_space( dirname( __FILE__ ) ) );
+$diskSpace = "$freeSpace / $totalSpace (Free/Total)";
+
+/* Load average */
+$loadAvg = "Unknown";
+if( function_exists( 'sys_getloadavg' ) ) {
+	$loadAvg = sys_getloadavg();
+	$loadAvg = "Last 1,5,15 min(s): {$loadAvg[0]} / {$loadAvg[1]} / {$loadAvg[2]}";
+}
 ?>
+<h2 class="caption"><span class="main-text"><?php echo _t('Server Info'); ?></span></h2>
+<div class="generalinfo">
+<table>
+<tr>
+	<td>Server time</td>
+	<td><?php echo $serverTime; ?></td>
+</tr>
+<tr>
+	<td>Database</td>
+	<td><? echo $dbVersion; ?></td>
+</tr>
+<tr>
+	<td>Database stat</td>
+	<td><? echo $dbStat; ?></td>
+</tr>
+<tr>
+	<td>Web server</td>
+	<td><? echo $webServer; ?></td>
+</tr>
+<tr>
+	<td>Operating System</td>
+	<td><? echo $osVersion; ?></td>
+</tr>
+<tr>
+	<td>Install path</td>
+	<td><? echo dirname(dirname(dirname((dirname(dirname(__FILE__)))))); ?></td>
+</tr>
+<tr>
+	<td>Disk space</td>
+	<td><? echo $diskSpace; ?></td>
+</tr>
+<tr>
+	<td>Load Avg.</td>
+	<td><? echo $loadAvg; ?></td>
+</tr>
+</table>
+</div>
 <h2 class="caption"><span class="main-text"><?php echo _t('PHP Info'); ?></span></h2>
 <div class="phpinfo">
 <?php 
