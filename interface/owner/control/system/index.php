9d783665a9ffded231881b314286bf25095388aa
coolengineer <hojin.choi@gmail.com>
2008-01-30 14:09:53 +0000

    * #817  * embeded css를 제거한 버전

--- interface/owner/control/system/index.php
+++ interface/owner/control/system/index.php
@@ -24,6 +24,37 @@ $page = $_GET['page'];
 ?>
 <h2 class="caption"><span class="main-text"><?php echo _t('PHP Info'); ?></span></h2>
 <div class="phpinfo">
-<?php phpinfo();?>
+<?php 
+ob_start();
+phpinfo();
+$phpinfo = ob_get_contents();
+ob_end_clean();
+echo preg_replace( "@.*<body.*?>(.*)</body>.*@sim",'$1', $phpinfo );
+
+/* ORIGINAL embeded css from phpinfo()
+<style type="text/css"><!--
+body {background-color: #ffffff; color: #000000;}
+body, td, th, h1, h2 {font-family: sans-serif;}
+pre {margin: 0px; font-family: monospace;}
+a:link {color: #000099; text-decoration: none; background-color: #ffffff;}
+a:hover {text-decoration: underline;}
+table {border-collapse: collapse;}
+.center {text-align: center;}
+.center table { margin-left: auto; margin-right: auto; text-align: left;}
+.center th { text-align: center !important; }
+td, th { border: 1px solid #000000; font-size: 75%; vertical-align: baseline;}
+h1 {font-size: 150%;}
+h2 {font-size: 125%;}
+.p {text-align: left;}
+.e {background-color: #ccccff; font-weight: bold; color: #000000;}
+.h {background-color: #9999cc; font-weight: bold; color: #000000;}
+.v {background-color: #cccccc; color: #000000;}
+i {color: #666666; background-color: #cccccc;}
+img {float: right; border: 0px;}
+hr {width: 600px; background-color: #cccccc; border: 0px; height: 1px; color: #000000;}
+//--></style>
+*/
+
+?>
 </div>
 <?php require ROOT . '/lib/piece/owner/footer.php';?>
