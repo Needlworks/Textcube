9da7b5684cc4043d26b9ec9f8322d1e922f6f1a8
inureyes <inureyes@gmail.com>
2008-02-20 02:24:42 +0000

    #819  * [5306/old_svn] 에서 사용되는 apikey 디렉토리 추가

---
+++ interface/owner/setting/account/apikey/index.php
@@ -0,0 +1,16 @@
+<?php
+/// Copyright (c) 2004-2008, Needlworks / Tatter Network Foundation
+/// All rights reserved. Licensed under the GPL.
+/// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
+$IV = array(
+	'POST' => array(
+		'APIKey' => array('string', 'default'=>'')
+	)
+);
+require ROOT . '/lib/includeForBlogOwner.php';
+requireStrictRoute();
+$result = false;
+$result = changeAPIKey(getUserId(), $_POST['APIKey']);
+if($result) respond::ResultPage(0);
+else respond::ResultPage(-1);
+?>
