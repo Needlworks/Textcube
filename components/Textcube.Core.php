d48eacbd48d2acd3efecc50788172b40759d1a74
inureyes <inureyes@gmail.com>
2008-02-29 13:23:50 +0000

    #836  * 헤더의 참여중인 블로그 출력을 언제나 select로 출력되도록 수정  * suggested by creorix ( http://forum.tattersite.com/ko/viewtopic.php?pid=24683#p24683 )

--- components/Textcube.Core.php
+++ components/Textcube.Core.php
@@ -205,7 +205,7 @@ class User {
 				return;
 			case 1:
 				$title = setting::getBlogSettingGlobal("title",null,$blogList[0],true);
-				return "<span id=\"teamblog\">".($title ? $title : _f('%1 님의 블로그',User::getBlogOwnerName($blogList[0])))."</span>";
+				return "<select id=\"teamblog\">".($title ? $title : _f('%1 님의 블로그',User::getBlogOwnerName($blogList[0])))."</select>";
 			default:
 				$changeBlogView = str_repeat(TAB,7)."<select id=\"teamblog\" onchange=\"location.href='{$blogURL}/owner/setting/teamblog/changeBlog/?blogid='+this.value\">".CRLF;
 				foreach($blogList as $info){
