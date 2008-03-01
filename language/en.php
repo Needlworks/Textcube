1b87f6a688de73cc395dfcacb080a68db3963377
inureyes <inureyes@gmail.com>
2008-03-01 04:28:14 +0000

    #710  * 언어 리소스 누락 부분 추가

--- language/en.php
+++ language/en.php
@@ -4,6 +4,33 @@
 /* By inureyes (inureyes@gmail.com) */
 
 // 1.6.0
+//$__text['Asia/Seoul'] = '';
+//$__text['Asia/Tokyo'] = '';
+//$__text['Asia/Shanghai'] = '';
+//$__text['Asia/Taipei'] = '';
+//$__text['Asia/Calcutta'] = '';
+//$__text['Europe/Berlin'] = '';
+//$__text['Europe/Paris'] = '';
+//$__text['Europe/London'] = '';
+//$__text['GMT'] = '';
+//$__text['America/New_York'] = '';
+//$__text['America/Chicago'] = '';
+//$__text['America/Denver'] = '';
+//$__text['America/Los_Angeles'] = '';
+//$__text['Australia/Sydney'] = '';
+//$__text['Australia/Melbourne'] = '';
+//$__text['Australia/Adelaide'] = '';
+//$__text['Australia/Darwin'] = '';
+//$__text['Australia/Perth'] = '';
+
+$__text['일요일'] = 'Sun';
+$__text['월요일'] = 'Mon';
+$__text['화요일'] = 'Tue';
+$__text['수요일'] = 'Wed';
+$__text['목요일'] = 'Thu';
+$__text['금요일'] = 'Fri';
+$__text['토요일'] = 'Sat';
+
 $__text["%1 설정"] = 'Set up %1';
 $__text["%1님께서 블로그의 팀원으로 초대합니다"] = '%1 as a teamblog leader invites you to join';
 $__text["BlogAPI를 사용하는 편집기들은 이 블로그로 글을 보내기 위하여 'API 주소'를 사용합니다."] = 'When using external blog editors, such as Ecto or MarsEdit, you can use this address.';
