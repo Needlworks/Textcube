f68128149930b66445d79ee5ce0e67ea5c911397
graphittie <graphittie@gmail.com>
2008-03-08 19:49:00 +0000

    * refs #871.  * PHP에 의해 출력되는 404 에러 메세지의 inline style 제거. TCwarning이라는 클래스 명을 부여하여 하위 호환성을 유지.

--- interface/blog/notice.php
+++ interface/blog/notice.php
@@ -14,7 +14,7 @@ if(isset($suri['id'])) {
 	if (empty($entries)) {
 		header('HTTP/1.1 404 Not Found');
 		if (empty($skin->pageError)) {
-			dress('article_rep', '<div style="text-align:center;font-size:14px;font-weight:bold;padding-top:50px;margin:50px 0;color:#333;background:url(' . $service['path'] . '/image/warning.gif) no-repeat top center;">' . _text('존재하지 않는 페이지입니다.') . '</div>', $view);
+			dress('article_rep', '<div class="TCwarning" style="background-image:url(\'' . $service['path'] . '/image/warning.gif\');">' . _text('존재하지 않는 페이지입니다.') . '</div>', $view);
 		} else {
 			dress('article_rep', NULL, $view);
 			dress('page_error', $skin->pageError, $view);
