7506f81479e3aaddd5d79dc18f5ef58855580888
inureyes <inureyes@gmail.com>
2008-04-16 11:25:18 +0000

    #934  * 버그 수정  * 통계 관련 컴포넌트 개수  * 레거시 함수 추가 및 기존 모델중 일부 제외

--- lib/includeForReader.php
+++ lib/includeForReader.php
@@ -23,7 +23,7 @@ require ROOT .'/lib/model/blog.service.php';
 require ROOT .'/lib/model/blog.blogSetting.php';
 require ROOT .'/lib/model/blog.user.php';
 require ROOT .'/lib/model/blog.fx.php';
-require ROOT .'/lib/model/common.paging.php';
+require ROOT .'/lib/model/common.legacysupport.php';
 require ROOT .'/lib/model/common.setting.php';
 require ROOT .'/lib/model/common.plugin.php';
 require ROOT .'/lib/model/reader.common.php';
