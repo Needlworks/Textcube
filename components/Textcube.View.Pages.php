0f33d723bebe075eacfbbc4ac71ffce1e88951e9
inureyes <inureyes@gmail.com>
2008-04-22 10:58:31 +0000

    #943  * DB 접속에 실패하면 에러 메세지를 띄우도록 수정

--- components/Textcube.View.Pages.php
+++ components/Textcube.View.Pages.php
@@ -6,7 +6,15 @@ class Pages {
 	}
 	
 	function reset() {
-		
+		$this->message =
+		$this->mode =
+		null;
+	}
+
+	function view($message = null) {
+		if(!empty($message)) $this->message = $message;
+		echo $this->message;	// Will implement later~
+		return true;
 	}
 }
 ?>
