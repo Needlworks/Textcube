8491ef0720facb5334b2d0606b0171b99487f098
inureyes <inureyes@gmail.com>
2008-05-04 23:02:00 +0000

    #897  * 탭관련 중복 코드를 줄이기 위하여 [5874/old_svn]에서 수정한 부분에서 누락된 파일 추가

---
+++ lib/piece/owner/communicationTab.php
@@ -0,0 +1,8 @@
+							<ul id="communication-tabs-box" class="tabs-box">
+								<li<?php echo isset($tabsClass['comment']) ? ' class="selected"' : NULL;?>><a href="<?php echo $blogURL;?>/owner/communication/comment?page=1<?php echo $tabsClass['postfix'];?>&amp;status=comment"><?php echo _t('댓글');?></a></li>
+								<li<?php echo isset($tabsClass['guestbook']) ? ' class="selected"' : NULL;?>><a href="<?php echo $blogURL;?>/owner/communication/comment?page=1<?php echo $tabsClass['postfix'];?>&amp;status=guestbook"><?php echo _t('방명록');?></a></li>
+								<li<?php echo isset($tabsClass['notify']) ? ' class="selected"' : NULL;?>><a href="<?php echo $blogURL;?>/owner/communication/notify"><?php echo _t('댓글 알리미');?></a></li>
+								<li<?php echo isset($tabsClass['received']) ? ' class="selected"' : NULL;?>><a href="<?php echo $blogURL;?>/owner/communication/trackback?page=1<?php echo $tabsClass['postfix'];?>&amp;status=received"><?php echo _t('걸린 글');?></a></li>
+								<li<?php echo isset($tabsClass['sent']) ? ' class="selected"' : NULL;?>><a href="<?php echo $blogURL;?>/owner/communication/trackback?page=1<?php echo $tabsClass['postfix'];?>&amp;status=sent"><?php echo _t('건 글');?></a></li>
+								<li<?php echo isset($tabsClass['trash']) ? ' class="selected"' : NULL;?>><a href="<?php echo $blogURL;?>/owner/communication/trash<?php echo $tabsClass['postfix'];?>"><?php echo _t('휴지통');?></a></li>
+							</ul>
