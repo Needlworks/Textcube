0c63257e0acb7818f0bfa5f642e8157ef95bffa1
creorix <textcube@needlworks.org>
2008-05-29 13:36:26 +0000

    #957  * skin 목록을 가져올 때 정말 스킨이 포함된 폴더인지 검사해서 목록에 추가하도록 변경

--- interface/owner/control/server/index.php
+++ interface/owner/control/server/index.php
@@ -32,7 +32,10 @@ closedir($handler);
 $skinList = array($service['skin'] => null);
 if (is_dir(ROOT.'/skin/') && $handler = opendir(ROOT.'/skin/')) {
 	while (($file = readdir($handler)) !== false) {
-		if (!is_dir(ROOT.'/skin/') || substr($file, 0, 1) == '.' || $file == 'customize') {
+		if (!is_dir(ROOT.'/skin/') || in_array($file, array('.', '..', 'customize'))) {
+			continue;
+		}
+		if (!file_exists(ROOT.'/skin/'.$file.'/index.xml') || !file_exists(ROOT.'/skin/'.$file.'/skin.html')) {
 			continue;
 		}
 		$skinList[$file] = null;
