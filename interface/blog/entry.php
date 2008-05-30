6d22e886c76fe50937b2a8c4db1a00cb7a0d2034
inureyes <inureyes@gmail.com>
2008-05-30 11:18:44 +0000

    #881  * entry에서 제목이 없을 경우에는 일반 블로그 (표지 아닌) 주소로 접속할 수 있도록 함.

--- interface/blog/entry.php
+++ interface/blog/entry.php
@@ -6,22 +6,26 @@ require ROOT . '/lib/includeForBlog.php';
 if (false) {
 	fetchConfigVal();
 }
-// For category view.
 
-if(isset($_GET['category'])) { // category exists
+if(empty($suri['value'])) {
+	list($entries, $paging) = getEntriesWithPaging($blogid, $suri['page'], $blog['entriesOnPage']);
+} else if(isset($_GET['category'])) { // category exists
 	if(Validator::isInteger($_GET['category'], 0)) {
 		list($entries, $paging) = getEntryWithPagingBySlogan($blogid, $suri['value'],false,$_GET['category']);
 	}
 } else { // Just normal entry view
 	list($entries, $paging) = getEntryWithPagingBySlogan($blogid, $suri['value']);
 }
+
 fireEvent('OBStart');
 require ROOT . '/lib/piece/blog/begin.php';
 
 $automaticLink = "<link rel=\"stylesheet\" href=\"{$serviceURL}/style/system.css\" type=\"text/css\" media=\"screen\" />\n";
 dress('SKIN_head_end', $automaticLink."[##_SKIN_head_end_##]", $view);
 
-if (empty($entries)) {
+if (empty($suri['value'])) {
+	require ROOT . '/lib/piece/blog/entries.php';
+} else if (empty($entries)) {
 	header('HTTP/1.1 404 Not Found');
 	if (empty($skin->pageError)) { 
 		dress('article_rep', '<div class="TCwarning">' . _text('존재하지 않는 페이지입니다.') . '</div>', $view);
@@ -33,6 +37,9 @@ if (empty($entries)) {
 } else {
 	require ROOT . '/lib/piece/blog/entries.php';
 }
+
+
+
 require ROOT . '/lib/piece/blog/end.php';
 fireEvent('OBEnd');
 ?>
