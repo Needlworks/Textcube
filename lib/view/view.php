63de6211898141adfc3e9fc07c2d4181dba4edd7
coolengineer <hojin.choi@gmail.com>
2008-06-05 00:30:48 +0000

    * #378  * 댓글 -> 글 로 수정

--- lib/view/view.php
+++ lib/view/view.php
@@ -242,7 +242,7 @@ function getCommentView($entry, $skin) {
 		foreach ($comments as $key => $value) {
 			if ($value['secret'] == 1) {
 				if ($authorized) {
-					$comments[$key]['comment'] = _text('[비밀댓글]') .' '. $comments[$key]['comment'];
+					$comments[$key]['comment'] = _text('[비밀글]') .' '. $comments[$key]['comment'];
 				} else {
 					if( !doesHaveOpenIDPriv($value) ) {
 						$comments[$key]['name'] = _text('비밀방문자');
@@ -283,7 +283,7 @@ function getCommentView($entry, $skin) {
 			else if ($commentSubItem['secret'] == 1) {
 				$rp_class = $prefix1 . '_secret';
 				if ($authorized) {
-					$commentSubItem['comment'] = '<span class="hiddenCommentTag_content">' . _text('[비밀댓글]') . '</span> ' . $commentSubItem['comment'];
+					$commentSubItem['comment'] = '<span class="hiddenCommentTag_content">' . _text('[비밀글]') . '</span> ' . $commentSubItem['comment'];
 				} else {
 					$rp_class .= ' hiddenComment';
 					$commentSubItem['name'] = '<span class="hiddenCommentTag_name">' . _text('비밀방문자') . '</span>'.(doesHaveOpenIDPriv($commentSubItem)?' '.$commentSubItem['name']:'');
