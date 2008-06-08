5de17201bd14326a720146a3c1569796f44f816e
inureyes <inureyes@gmail.com>
2008-06-08 11:19:31 +0000

    #852  * 관련 컴포넌트 살짝 수정

--- components/Textcube.Data.GuestComment.php
+++ components/Textcube.Data.GuestComment.php
@@ -139,6 +139,8 @@ class GuestComment {
 		if (isset($this->commenter)) {
 			if (!Validator::number($this->commenter, 1))
 				return $this->_error('commenter');
+			if (!$this->name = User::getName($this->commenter))
+				return $this->_error('commenter');
 			$query->setAttribute('replier', $this->commenter);
 		}
 		if (isset($this->name)) {
