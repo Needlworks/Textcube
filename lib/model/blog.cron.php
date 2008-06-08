1dd4837f69e1da5144f446603c517db5921e08ec
coolengineer <hojin.choi@gmail.com>
2008-06-08 16:23:21 +0000

    * #951  * 24h, Daily Cron 추가

--- lib/model/blog.cron.php
+++ lib/model/blog.cron.php
@@ -11,7 +11,7 @@ function dumbCronScheduler($checkOnly=true)
 
 	$dumbCronStamps = getServiceSetting('dumbCronStamps',
 			serialize( array( '1m' => 0, '5m' => 0, '30m' => 0, 
-					'1h' => 0, '2h' => 0, '6h' => 0, '12h' => 0 )));
+					'1h' => 0, '2h' => 0, '6h' => 0, '12h' => 0, '24h' => 0, 'Daily' => 0 )));
 
 	$dumbCronStamps = unserialize( $dumbCronStamps );
 
@@ -22,12 +22,17 @@ function dumbCronScheduler($checkOnly=true)
 					'1h'  => 60*60,
 					'2h'  => 60*60*2,
 					'6h'  => 60*60*6,
-					'12h' => 60*60*12 );
+					'12h' => 60*60*12,
+					'24h' => 60*60*24,
+					'Daily' => 60*60*24 );
 	/* Events: Cron1m, Cron5m, Cron30m, Cron1h, Cron2h, Cron6h, Cron12h */
 	$log_file = dirname(__FILE__).DS."..".DS."..".DS."cache".DS."cronlog.txt";
 	$log = fopen( $log_file, "a" );
 	foreach( $schedules as $d => $diff ) {
-		if( $now > $diff + $dumbCronStamps[$d]    ) { 
+		if( !isset( $dumbCronStamps[$d] ) ) {
+			$dumbCronStamps[$d] = 0;
+		}
+		if( $now > $diff + $dumbCronStamps[$d] ) { 
 			if( $checkOnly && eventExists("Cron$d") ) return true;
 			fireEvent( "Cron$d",  null, $now );
 			fwrite( $log, date( 'Y-m-d H:i:s' ).' '.$blog['name']." Cron$d executed ({$_SERVER['REQUEST_URI']})\r\n" );
