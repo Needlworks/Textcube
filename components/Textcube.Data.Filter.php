fa5d39d045fa418915ddffd434502b9df61c5c12
creorix <textcube@needlworks.org>
2008-06-12 16:55:44 +0000

    #608  * Wildcard-based IP matching 지원 (e.g. 123.456.*.*)

--- components/Textcube.Data.Filter.php
+++ components/Textcube.Data.Filter.php
@@ -103,9 +103,31 @@ class Filter {
 	/*@static@*/
 	function isFiltered($type, $value) {
 		global $database;
-		$type = POD::escapeString($type);
-		$value = POD::escapeString($value);
-		return POD::queryExistence("SELECT * FROM {$database['prefix']}Filters WHERE blogid = ".getBlogId()." AND type = '$type' AND '$value' LIKE CONCAT('%', pattern, '%')");
+
+		switch ($type) {
+			case 'ip':
+				$ip = explode('.', $value);
+				$conditions = array();
+				for ($i = 1; $i <= 4; $i++) {
+					$pattern = array_slice($ip, 0, $i);
+					if (($num = count($pattern)) < 4) {
+						for ($j = 0; $j < 4 - $num; $j++) {
+							$pattern[] = '*';
+						}
+					}
+					$conditions[] = 'pattern = "'.POD::escapeString(implode('.', $pattern)).'"';
+				}
+				if (!empty($conditions)) {
+					$conditions = ' AND ('.implode(' OR ', $conditions).')';
+				} else {
+					$conditions = ' AND 1 = 0';
+				}
+				return POD::queryExistence('SELECT * FROM '.$database['prefix'].'Filters WHERE blogid = '.getBlogId().' AND type = "ip"'.$conditions.' LIMIT 1');
+			default:
+				$type = POD::escapeString($type);
+				$value = POD::escapeString($value);
+				return POD::queryExistence("SELECT * FROM {$database['prefix']}Filters WHERE blogid = ".getBlogId()." AND type = '$type' AND '$value' LIKE CONCAT('%', pattern, '%')");
+		}
 	}
 	
 	function _buildQuery() {
