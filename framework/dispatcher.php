baaa68d68a2689779b164635bc96c8d014523142
daybreaker <me@daybreaker.info>
2008-07-07 15:34:12 +0000

    #1052: 좀더 구체적인 실행 과정 설명

--- framework/dispatcher.php
+++ framework/dispatcher.php
@@ -3,14 +3,32 @@
 /// All rights reserved. Licensed under the GPL.
 /// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
 
-// This file is called AT FIRST to parse URL and query strings.
+// This file...
+//   is executed AT FIRST.
+//   specifies the main workflow of Textcube 2.0.
 
-/* TODO: Load config.php */
+/* TODO: Load config.php and vital libraries. */
+// Redirect to setup process if there's no config.php.
 
-/* TODO: Unify the URI strings / workaround (IIS, Apache - mod_php or fastcgi, lighttpd, and etc.) */
+/* TODO: Unify the environment and do work-arounds. (For IIS, Apache - mod_php or fastcgi, lighttpd, and etc.) */
 
-/* TODO: Special pre-handlers (favicon.ico, index.gif) */
+/* TODO: Parse and normalize URI. */
+// Structure of fancy URL:
+//   host + blog prefix + interface path + pagination info + extra arguments not in $_REQUEST
 
-/* TODO: Load interface */
+/* TODO: Session management. */
+
+/* TODO: Special pre-handlers. (favicon.ico, index.gif) */
+
+/* TODO: Load more libraries depending on handler types. (outside, admin, feeder, ...) */
+// integrate includeForBlog.php, includeForBlogOwner.php, includeForFeeder.php, etc. here.
+
+/* TODO: Load final interface handler. */
+// Check existence of interface path.
+// Each interface...
+//   validates passed arguments through IV.
+//   specify required ACL/permissions and check them.
+//   loads its necessary libraries, models and components.
+// before actual execution.
 
 ?>
