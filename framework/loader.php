cbca40ad9fc516c9d083e02c2caa77fe2e4d314d
daybreaker <me@daybreaker.info>
2008-07-29 08:42:20 +0000

    #1052: svn 꼬인 것 수정 -_-?

--- framework/loader.php
+++ framework/loader.php
@@ -1,4 +1,3 @@
-<<<<<<< .mine
 <?php
 /// Copyright (c) 2004-2008, Needlworks / Tatter Network Foundation
 /// All rights reserved. Licensed under the GPL.
@@ -25,32 +24,3 @@ function __autoload($classname) {
 }
 
 ?>
-=======
-<?php
-/// Copyright (c) 2004-2008, Needlworks / Tatter Network Foundation
-/// All rights reserved. Licensed under the GPL.
-/// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
-
-$classPaths = array(
-	'IModel' => './data/IAdapter.php',
-	'IAdapter' => './data/IModel.php',
-	'ICache' => './cache/ICache.php',
-	'Entry' => './model/Entry.php'
-);
-
-$config = Config::getInstance();
-// Set paths for DB classes according to the current backend configuration.
-$classPaths['Adapter'] = './data/' . $config->backend_name . '/Adapter.php';
-$classPaths['Model'] = './data/' . $config->backend_name . '/Model.php';
-
-function __autoload($classname) {
-	global $classPaths;
-	if (isset($classPaths[$classname])) {
-		$include_path = str_replace('{DB}', $config->backend_name, $classPaths[$classname]);
-		require_once($classPaths[$classname]);
-	}
-	// TODO: error handling or find out?
-}
-
-?>
->>>>>>> .r6459
