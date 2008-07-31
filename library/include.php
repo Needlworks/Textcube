597f77153bd58f8196b55dee24c39d1293626588
inureyes <inureyes@gmail.com>
2008-07-31 18:26:54 +0000

    #1065  * 레거시 루틴 추가  * 파일명 네이밍 컨벤션의 문제입니다.

--- library/include.php
+++ library/include.php
@@ -18,6 +18,7 @@ function requireComponent($name) {
 	global $__requireComponent;
 	//if (!preg_match('/^[a-zA-Z0-9\.]+$/', $name))		return;
 	$name = str_replace('Tattertools', 'Textcube',$name); // Legacy routine.
+	$name = str_replace('Textcube.Function.misc', 'Textcube.Function.Misc',$name); // Legacy routine ( < 1.8).
 	if(!in_array($name,$__requireComponent)) {
 		include_once (ROOT . "/library/components/$name.php");
 		array_push($__requireComponent,$name);
