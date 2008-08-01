e3e162c0d0cd095ebeada54caf17a33c47641e5c
daybreaker <me@daybreaker.info>
2008-08-01 18:58:32 +0000

    #1052: spl_autoload_register를 사용하여 library의 autoloader와 충돌하지 않게 함

--- loader.php
+++ loader.php
@@ -3,26 +3,33 @@
 /// All rights reserved. Licensed under the GPL.
 /// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
 
-$classPaths = array(
-	'Context' => './framework/context.php',
-	'Debug' => './debug.php',
-	'IModel' => './data/IAdapter.php',
-	'IAdapter' => './data/IModel.php',
-	'ICache' => './cache/ICache.php',
-	'Entry' => './model/Entry.php'
-);
+final class FrameworkAutoloader
+{
+	private static $classPaths = array(
+			'Context' => './framework/context.php',
+			'Debug' => './debug.php',
+			'IModel' => './data/IAdapter.php',
+			'IAdapter' => './data/IModel.php',
+			'ICache' => './cache/ICache.php',
+			'Entry' => './model/Entry.php'
+		);
 
-$config = Config::getInstance();
-// Set paths for DB classes according to the current backend configuration.
-$classPaths['Adapter'] = './data/' . $config->backend_name . '/Adapter.php';
-$classPaths['Model'] = './data/' . $config->backend_name . '/Model.php';
+	static function init() {
+		$config = Config::getInstance();
 
-function __autoload($classname) {
-	global $classPaths;
-	if (isset($classPaths[$classname])) {
-		require_once($classPaths[$classname]);
+		// Set paths for DB classes according to the current backend configuration.
+		self::$classPaths['Adapter'] = './data/' . $config->backend_name . '/Adapter.php';
+		self::$classPaths['Model'] = './data/' . $config->backend_name . '/Model.php';
 	}
-	// TODO: error handling or find out?
+
+	static function autoload($name) {
+		if (isset(self::$classPaths[$name]))
+			require_once(self::$classPaths[$name]);
+		// Because multiple autoload functions can be defined, we don't throw any exception here.
+		// If PHP finally fails finding the class, it will say FATAL error.
+	}
 }
 
+FrameworkAutoloader::init();
+spl_autoload_register(array('FrameworkAutoloader', 'autoload'));
 ?>
