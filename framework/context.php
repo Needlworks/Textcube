a794670abf79918731a998267850ad98b414c15d
daybreaker <me@daybreaker.info>
2008-08-01 17:19:12 +0000

    #1052: Context 관리 클래스 추가

---
+++ framework/context.php
@@ -0,0 +1,101 @@
+<?php
+/// Copyright (c) 2004-2008, Needlworks / Tatter Network Foundation
+/// All rights reserved. Licensed under the GPL.
+/// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
+
+class URIError extends Exception {};
+
+final class Context
+{
+	private static $instance;
+	public $accessInfo;
+
+	private function __construct() {
+		$config = Config::getInstance();
+
+		// TEMPORARY: copyed from rewrite.php
+		// Workaround for IIS environment
+		if(!isset($_SERVER['REQUEST_URI']) && isset($_SERVER['SCRIPT_NAME'])) {
+			$_SERVER['REQUEST_URI'] = $_SERVER['SCRIPT_NAME'];
+			if(isset($_SERVER['QUERY_STRING']) && !empty($_SERVER['QUERY_STRING']))
+				$_SERVER['REQUEST_URI'] .= '?'.$_SERVER['QUERY_STRING'];
+		}
+		$accessInfo = array(
+			'host'      => $_SERVER['HTTP_HOST'],
+			'fullpath' => str_replace('index.php', '', $_SERVER["REQUEST_URI"]), // SUGGEST: change the name 'fullpath' to 'fullQuery'
+			'position'  => $_SERVER["SCRIPT_NAME"],
+			'root'      => rtrim(str_replace('rewrite.php', '', $_SERVER["SCRIPT_NAME"]), 'index.php')
+			);
+		// Workaround for compartibility with fastCGI / Other environment
+		$accessInfo['input'] = ltrim(substr($accessInfo['fullpath'],
+			strlen($accessInfo['root']) + (defined('__TEXTCUBE_NO_FANCY_URL__') ? 1 : 0)),'/');
+		// DEPRECATE?: Support for Tattertools 0.9x legacy address
+		
+		$accessInfo['URLfragment'] = explode('/',strtok($accessInfo['input'],'?'));
+		if (defined('__TEXTCUBE_NO_FANCY_URL__'))
+			$config->service['type'] = 'single';
+		switch ($config->service['type']) {
+			case 'path': // For path-based multi blog.
+				array_splice($accessInfo['URLfragment'], 0, 1); 
+				$pathPart = ltrim(rtrim(strtok(strstr($accessInfo['input'],'/'), '?'), '/'), '/');
+				break;
+			case 'single':
+				$pathPart = (strpos($accessInfo['input'],'?') !== 0 ?
+					ltrim(rtrim(strtok($accessInfo['input'], '?'), '/'), '/') :
+					'');
+				break;
+			case 'domain': default: 
+				$pathPart = ltrim(rtrim(strtok($accessInfo['fullpath'], '?'), '/'), '/');
+				if (!empty($config->service['path']))
+					$pathPart = ltrim($pathPart, $config->service['path']);
+				break;
+		}
+		$pathPart = strtok($pathPart,'&');
+		$interfacePath = null;
+		if (in_array($pathPart, array('favicon.ico','index.gif'))) {
+			$accessInfo['interfacePath'] = 'interface/'.$pathPart.'.php';
+		} else {
+			if (!empty($accessInfo['URLfragment']) &&
+				in_array($accessInfo['URLfragment'][0],
+						 array('api','archive','attachment','author','category','checkup','cover','cron','entry','feeder','foaf','guestbook','keylog','location','logout','notice','page','plugin','pluginForOwner','search','suggest','sync','tag','ttxml')))
+			{
+				$pathPart = $accessInfo['URLfragment'][0];
+				$interfacePath = 'interface/blog/'.$pathPart.'.php';
+			} else if (is_numeric(strtok(end($accessInfo['URLfragment']), '&'))) {
+				$pathPart = count($accessInfo['URLfragment'])==1 ?
+					null :
+					implode('/', array_slice($accessInfo['URLfragment'], 0, count($accessInfo['URLfragment']) - 1));
+			}
+			if (empty($interfacePath))
+				$interfacePath = 'interface/'.(empty($pathPart) ? '' : $pathPart.'/').'index.php';
+			define('PATH', 'interface/'.(empty($pathPart) ? '' : $pathPart.'/'));
+			unset($pathPart);
+			if (!file_exists($interfacePath)) {
+				throw new URIError("No such interface");
+			}
+			$accessInfo['interfacePath'] = $interfacePath;
+		}
+
+		// TODO: Add these to debug-mode output
+		//echo "<b>\$accessInfo : </b>";
+		//var_dump($accessInfo);
+		//echo "<br /><b>\$pathPart : </b>";
+		//var_dump($pathPart);
+		//echo "<br />\n";
+
+		// TODO: Parse $_GET, $_POST, and etc.
+
+		$this->accessInfo = $accessInfo;
+	}
+
+	function __destruct() {
+		// Nothing to do: destruction of this class means the end of execution
+	}
+
+	public static function getInstance() {
+		if (!isset(self::$instance))
+			self::$instance = new Context();
+		return self::$instance;
+	}
+}
+?>
