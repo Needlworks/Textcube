5ad16e019e8f41d5ae0fe914ac4e74986fd50cf4
inureyes <inureyes@gmail.com>
2008-08-21 19:24:29 +0000

    refs #1075  * 에디터 레이아웃 변경  * 스크린을 최대한 사용하도록 함  * 화면따라 무한정 커지지는 않습니다... 980px로 조정.

--- interface/owner/entry/edit/index.php
+++ interface/owner/entry/edit/index.php
@@ -685,83 +685,6 @@ if (count($templateLists) == 0) {
 									
 									<hr class="hidden" />
 									
-									<div id="save-section">
-<?php
-if (isset($_GET['popupEditor'])) {
-?>
-										<div class="button-box two-button-box">
-											<input type="button" value="<?php echo _t('미리보기');?>" class="preview-button input-button" onclick="entryManager.preview();return false;" />
-											<span class="hidden">|</span>
-											<input type="submit" id="saveButton" value="<?php echo _t('중간 저장');?>" class="save-button input-button" onclick="entryManager.save();return false;" />
-											<span class="hidden">|</span>
-											<input type="submit" value="<?php echo _t('저장 후 돌아가기');?>" class="save-and-return-button input-button" onclick="entryManager.saveAndReturn();return false;" />									
-										</div>
-<?php
-} else {
-?>
-										<div class="button-box three-button-box">
-											<input type="button" value="<?php echo _t('미리보기');?>" class="preview-button input-button" onclick="entryManager.preview();return false;" />
-											<span class="hidden">|</span>
-							    	  	 		<input type="submit" id="saveButton" value="<?php echo _t('중간 저장');?>" class="save-button input-button" onclick="entryManager.save();return false;" />
-											<span class="hidden">|</span>
-							       			<input type="submit" value="<?php echo _t('저장 후 돌아가기');?>" class="save-and-return-button input-button" onclick="entryManager.saveAndReturn();return false;" />
-											<span class="hidden">|</span>
-											<input type="submit" value="<?php echo _t('목록으로');?>" class="list-button input-button" onclick="returnToList();return false;" />
-										</div>
-<?php
-}
-?>
-									</div>
-									
-									<hr class="hidden" />
-									
-									<div id="taglocal-section" class="section">
-										<h3><?php echo _t('태그 &amp; 위치');?></h3>
-												
-										<div id="tag-location-container" class="container">
-											<dl id="tag-line">
-												<dt><span class="label"><?php echo _t('태그');?></span></dt>
-												<dd id="tag"></dd>
-											</dl>
-											
-											<dl id="location-line">
-												<dt><span class="label"><?php echo _t('지역');?></span></dt>
-												<dd id="location"></dd>
-											</dl>
-											
-											<script type="text/javascript">
-												//<![CDATA[
-													try {
-														var oLocationTag = new LocationTag(document.getElementById("location"), "<?php echo $blog['language'];?>", <?php echo isset($service['disableEolinSuggestion']) && $service['disableEolinSuggestion'] ? 'true' : 'false';?>);
-														oLocationTag.setInputClassName("input-text");
-														oLocationTag.setValue("<?php echo addslashes($entry['location']);?>");	
-													} catch (e) {
-														document.getElementById("location").innerHTML = '<input type="text" class="input-text" name="location" value="<?php echo addslashes($entry['location']);?>" /><br /><?php echo _t('지역태그 스크립트를 사용할 수 없습니다. 슬래시(/)로 구분된 지역을 직접 입력해 주십시오.(예: /대한민국/서울/강남역)');?>';
-														// TODO : 이부분(스크립트를 실행할 수 없는 환경일 때)은 직접 입력보다는 0.96 스타일의 팝업이 좋을 듯
-													}
-													
-													try {
-														var oTag = new Tag(document.getElementById("tag"), "<?php echo $blog['language'];?>", <?php echo isset($service['disableEolinSuggestion']) && $service['disableEolinSuggestion'] ? 'true' : 'false';?>);
-														oTag.setInputClassName("input-text");
-<?php
-		$tags = array();
-		if (!defined('__TEXTCUBE_POST__')) {
-			foreach (getTags($entry['blogid'], $entry['id']) as $tag) {
-				array_push($tags, $tag['name']);
-				echo 'oTag.setValue("' . addslashes($tag['name']) . '");';
-			}
-		}
-?>
-													} catch(e) {
-														document.getElementById("tag").innerHTML = '<input type="text" class="input-text" name="tag" value="<?php echo addslashes(str_replace('"', '&quot;', implode(', ', $tags)));?>" /><br /><?php echo _t('태그 입력 스크립트를 사용할 수 없습니다. 콤마(,)로 구분된 태그를 직접 입력해 주십시오.(예: 텍스트큐브, BLOG, 테스트)');?>';
-													}
-												//]]>
-											</script> 
-										</div>
-									</div>
-
-									<hr class="hidden" />
-									
 									<div id="upload-section" class="section">
 										<h3><?php echo _t('업로드');?></h3>
 										
@@ -849,27 +772,85 @@ if (defined('__TEXTCUBE_POST__')) {
 											</dl>
 										</div>
 									</div>
+
+									<hr class="hidden clear" />
+									
+									<div id="save-section">
 <?php
 if (isset($_GET['popupEditor'])) {
 ?>
-									<div class="button-box two-button-box">
-										<input type="button" value="<?php echo _t('미리 보기');?>" class="preview-button input-button" onclick="entryManager.preview();return false;" />
-										<span class="hidden">|</span>
-										<input type="submit" value="<?php echo _t('저장 후 돌아가기');?>" class="save-and-return-button input-button" onclick="entryManager.saveAndReturn();return false;" />									
-								</div>
+										<div class="button-box two-button-box">
+											<input type="button" value="<?php echo _t('미리보기');?>" class="preview-button input-button" onclick="entryManager.preview();return false;" />
+											<span class="hidden">|</span>
+											<input type="submit" id="saveButton" value="<?php echo _t('중간 저장');?>" class="save-button input-button" onclick="entryManager.save();return false;" />
+											<span class="hidden">|</span>
+											<input type="submit" value="<?php echo _t('저장 후 돌아가기');?>" class="save-and-return-button input-button" onclick="entryManager.saveAndReturn();return false;" />									
+										</div>
 <?php
 } else {
 ?>
-									<div class="button-box three-button-box">
-										<input type="button" value="<?php echo _t('미리 보기');?>" class="preview-button input-button" onclick="entryManager.preview();return false;" />
-										<span class="hidden">|</span>
-						       			<input type="submit" value="<?php echo _t('저장 후 돌아가기');?>" class="save-and-return-button input-button" onclick="entryManager.saveAndReturn();return false;" />
-										<span class="hidden">|</span>
-										<input type="submit" value="<?php echo _t('목록으로');?>" class="list-button input-button" onclick="returnToList();return false;" />
-									</div>
+										<div class="button-box three-button-box">
+											<input type="button" value="<?php echo _t('미리보기');?>" class="preview-button input-button" onclick="entryManager.preview();return false;" />
+											<span class="hidden">|</span>
+							    	  	 		<input type="submit" id="saveButton" value="<?php echo _t('중간 저장');?>" class="save-button input-button" onclick="entryManager.save();return false;" />
+											<span class="hidden">|</span>
+							       			<input type="submit" value="<?php echo _t('저장 후 돌아가기');?>" class="save-and-return-button input-button" onclick="entryManager.saveAndReturn();return false;" />
+											<span class="hidden">|</span>
+											<input type="submit" value="<?php echo _t('목록으로');?>" class="list-button input-button" onclick="returnToList();return false;" />
+										</div>
 <?php
 }
 ?>
+									</div>
+									
+									<hr class="hidden" />
+									
+									<div id="taglocal-section" class="section">
+										<h3><?php echo _t('태그 &amp; 위치');?></h3>
+												
+										<div id="tag-location-container" class="container">
+											<dl id="tag-line">
+												<dt><span class="label"><?php echo _t('태그');?></span></dt>
+												<dd id="tag"></dd>
+											</dl>
+											
+											<dl id="location-line">
+												<dt><span class="label"><?php echo _t('지역');?></span></dt>
+												<dd id="location"></dd>
+											</dl>
+											
+											<script type="text/javascript">
+												//<![CDATA[
+													try {
+														var oLocationTag = new LocationTag(document.getElementById("location"), "<?php echo $blog['language'];?>", <?php echo isset($service['disableEolinSuggestion']) && $service['disableEolinSuggestion'] ? 'true' : 'false';?>);
+														oLocationTag.setInputClassName("input-text");
+														oLocationTag.setValue("<?php echo addslashes($entry['location']);?>");	
+													} catch (e) {
+														document.getElementById("location").innerHTML = '<input type="text" class="input-text" name="location" value="<?php echo addslashes($entry['location']);?>" /><br /><?php echo _t('지역태그 스크립트를 사용할 수 없습니다. 슬래시(/)로 구분된 지역을 직접 입력해 주십시오.(예: /대한민국/서울/강남역)');?>';
+														// TODO : 이부분(스크립트를 실행할 수 없는 환경일 때)은 직접 입력보다는 0.96 스타일의 팝업이 좋을 듯
+													}
+													
+													try {
+														var oTag = new Tag(document.getElementById("tag"), "<?php echo $blog['language'];?>", <?php echo isset($service['disableEolinSuggestion']) && $service['disableEolinSuggestion'] ? 'true' : 'false';?>);
+														oTag.setInputClassName("input-text");
+<?php
+		$tags = array();
+		if (!defined('__TEXTCUBE_POST__')) {
+			foreach (getTags($entry['blogid'], $entry['id']) as $tag) {
+				array_push($tags, $tag['name']);
+				echo 'oTag.setValue("' . addslashes($tag['name']) . '");';
+			}
+		}
+?>
+													} catch(e) {
+														document.getElementById("tag").innerHTML = '<input type="text" class="input-text" name="tag" value="<?php echo addslashes(str_replace('"', '&quot;', implode(', ', $tags)));?>" /><br /><?php echo _t('태그 입력 스크립트를 사용할 수 없습니다. 콤마(,)로 구분된 태그를 직접 입력해 주십시오.(예: 텍스트큐브, BLOG, 테스트)');?>';
+													}
+												//]]>
+											</script> 
+										</div>
+									</div>
+
+									<hr class="hidden clear"  />
 								</div>
 								
 								<input type="hidden" name="categoryAtHome" value="<?php echo (isset($_POST['category']) ? $_POST['category'] : '0');?>" />
@@ -878,7 +859,7 @@ if (isset($_GET['popupEditor'])) {
 								<input type="hidden" name="search" value="<?php echo (isset($_POST['search']) ? htmlspecialchars($_POST['search']) : '');?>" />
 							</div>
 						</form>
-						
+						<div id="feather" class="clear"></div>
 						<div id="eolinDialog" class="dialog" style="position: absolute; display: none; z-index: 100;">
 							<div class="temp-box">
 								<h4><?php echo _t('이올린이란?');?></h4>
