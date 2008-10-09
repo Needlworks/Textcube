bc53d94c26c76c41b0225a527d1cea659b07fd25
inureyes <inureyes@gmail.com>
2008-10-09 08:37:46 +0000

    refs #895   * 1.7 트리에 [6797/old_svn] [6798/old_svn] [6799/old_svn] 를 반영

---
+++ interface/owner/communication/trackback/reply/index.php
@@ -0,0 +1,23 @@
+<?php
+/// Copyright (c) 2004-2008, Needlworks / Tatter Network Foundation
+/// All rights reserved. Licensed under the GPL.
+/// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
+$IV = array(
+	'POST' => array(
+		'url' => array('url', 'default'=> null)
+	)
+);
+require ROOT . '/library/includeForBlogOwner.php';
+requireModel("blog.trackback");
+
+requireStrictRoute();
+
+/// First, detect trackback URL from RDF information.
+$info  = getRDFfromURL($_POST['url']);
+if(empty($info)) {
+	/// TODO : parse trackback URL information from site address.
+	respond::ResultPage(false);
+	exit;
+}
+respond::ResultPage(!empty($_POST['url']) && sendTrackback($blogid, $suri['id'], trim($info['trackbackURL'])));
+?>
