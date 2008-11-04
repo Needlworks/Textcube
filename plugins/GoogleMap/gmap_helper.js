7eafab69c49c274d99ace2fa390be1fee8034ab2
daybreaker <me@daybreaker.info>
2008-11-04 16:06:03 +0000

    refs #1131: 누락 파일 추가

---
+++ plugins/GoogleMap/gmap_helper.js
@@ -0,0 +1,13 @@
+// Google Map Helper
+//
+// depends on EAF4.js and Google Map API v2.
+
+function initializeGoogleMap() {
+	// Nothing to do currently.
+}
+
+function GMapTool_Insert() {
+	alert('not implemented yet.');
+}
+
+//window.addEventListener('load', initializeGoogleMap);
