bfd832bc96ac2d030bffbc7a091cda920013cb3d
daybreaker <me@daybreaker.info>
2008-11-18 15:01:57 +0000

    refs #777: IIS에서 null 경고로 페이지 뜨지 않는 문제 수정

--- library/data/mysql/Adapter.php
+++ library/data/mysql/Adapter.php
@@ -275,6 +275,7 @@ class DBQuery {
 	}
 	
 	public static function fetch($handle = null, $type = 'assoc') {
+		if ($handle == null) return null;
 		if($type == 'array') return mysql_fetch_array($handle); // Can I use mysql_fetch_row instead?
 		else if ($type == 'row') return mysql_fetch_row($handle);
 		else return mysql_fetch_assoc($handle);
