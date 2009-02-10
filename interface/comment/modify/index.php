9e06979d32c6048f48e8cd901b957cc023ec5712
inureyes <inureyes@gmail.com>
2009-02-10 14:57:11 +0000

    refs #1188   * [7228/old_svn]  을 trunk 에 반영

---
+++ interface/comment/modify/index.php
@@ -0,0 +1,8 @@
+<?php
+/// Copyright (c) 2004-2008, Needlworks / Tatter Network Foundation
+/// All rights reserved. Licensed under the GPL.
+/// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
+
+$_POST['mode'] = 'edit';
+require ROOT. '/interface/comment/delete/index.php';
+?>
