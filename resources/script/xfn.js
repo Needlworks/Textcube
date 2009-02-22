d94abbeab8404ee99c9ed9bf84cbea2e707cd90f
inureyes <inureyes@gmail.com>
2009-02-22 14:28:05 +0000

    refs #1212   * 자바스크립트와 텍스트 문서도 처리

--- resources/script/xfn.js
+++ resources/script/xfn.js
@@ -1,4 +1,4 @@
-/// Copyright (c) 2004-2008, Tatter & Company / Tatter & Friends.
+/// Copyright (c) 2004-2009, Tatter & Company / Tatter & Friends.
 /// All rights reserved. Licensed under the GPL.
 /// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
 
