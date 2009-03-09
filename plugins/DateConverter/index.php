f33ded8d06e380973dfeaa2c8a23e62a948b7c12
jparker <jparker2.0@gmail.com>
2009-03-09 00:47:53 +0000

    refs #1222  * DateConverter 플러그인에서 List date 잘못 변환 되는 문제 수정.

--- plugins/DateConverter/index.php
+++ plugins/DateConverter/index.php
@@ -2,76 +2,76 @@
 // 언어 템플릿 *******************************************************************************************************
 	// %Y = 4자리수 연도 [0000]
 	// %y = 2자리수 연도 [00]
-	
+
 	// %M = 앞의 0을 표시하는 달. [01~12] or January/Febuary/March/April...
 	// %m = 앞의 0을 표시하지 않는 달. [1~12] or Jan/Feb/Mar/Apr...
-	
+
 	// %D = 앞의 0을 표시하는 일(日). [01~31]
 	// %d = 앞의 0을 표시하지 않는 일(日). [1~31]
 	// %o = 서수 어미가 붙은 일(日). 주로 라틴 문자권에서 사용. [1~31] + [day ordinal postfix]
-	
+
 	// %H = 앞의 0을 표시하는 24시간. [00~24]
 	// %h = 앞의 0을 표시하지 않는 24시간. [0~24]
 	// %F = 앞의 0을 표시하는 12시간. [00~12]
 	// %f = 앞의 0을 표시하지 않는 12시간. [0~12]
-	
+
 	// %I = 앞의 0을 표시하는 분. [00~59]
 	// %i = 앞의 0을 표시하지 않는 분. [0~59]
-	
+
 	// %S = 앞의 0을 표시하는 초. [00~59]
 	// %s = 앞의 0을 표시하지 않는 초. [0~59]
 
 include_once "language.php";
-	
+
 function convertDateFormat($argTarget, $argType) {
 	global $pluginURL, $configVal;
 	global $rgDateInformation;
 
 	if (is_null($configVal)) return $argTarget;
-	
+
 	$temp = explode('/', $pluginURL);
 	array_shift($temp);
 	array_shift($temp);
-	
+
 	if (empty($configVal)) {
 		include 'config.ini.php';
 		$tempArray = $data;
 		unset($data);
-		
+
 	} else {
 		requireComponent('Textcube.Function.misc');
 		$tempArray = Setting::fetchConfigVal($configVal);
 	}
-	
+
 	$rgDateFormat = array();
 	$rgDateFormat['archive date'] = array("language" => $tempArray['language'], "format" => $tempArray['archive_date']);
 	$rgDateFormat['calendar head'] = array("language" => $tempArray['language'], "format" => $tempArray['calendar_date']);
 	$rgDateFormat['comment date'] = array("language" => $tempArray['language'], "format" => $tempArray['comment_date']);
 	$rgDateFormat['comment list date'] = array("language" => $tempArray['language'], "format" => $tempArray['comment_list_date']);
 	$rgDateFormat['guestbook date'] = array("language" => $tempArray['language'], "format" => $tempArray['guestbook_date']);
-	$rgDateFormat['list date'] = array("language" => $tempArray['language'], "format" => $tempArray['archive_date']);
+	$rgDateFormat['list date'] = array("language" => $tempArray['language'], "format" => $tempArray['list_date']);
 	$rgDateFormat['notice date'] = array("language" => $tempArray['language'], "format" => $tempArray['notice_date']);
 	$rgDateFormat['post date'] = array("language" => $tempArray['language'], "format" => $tempArray['post_date']);
 	$rgDateFormat['recent comment date'] = array("language" => $tempArray['language'], "format" => $tempArray['recent_comment_date']);
 	$rgDateFormat['recent trackback date'] = array("language" => $tempArray['language'], "format" => $tempArray['recent_trackback_date']);
 	$rgDateFormat['trackback date'] = array("language" => $tempArray['language'], "format" => $tempArray['trackback_date']);
-	
+
 	if (isset($rgDateFormat[$argType]))
 		$strLanguage = $rgDateFormat[$argType]['language'];
 	else
 		return $argTarget;
-	
+
 	if (empty($strLanguage) || $strLanguage == "default") {
 		$strLanguage = "korean";
 	}
-	
+
 	$strYear = NULL;
 	$strMonth = NULL;
 	$strDay = NULL;
 	$strHour = NULL;
 	$strMinute = NULL;
 	$strSecond = NULL;
-	
+
 	switch ($argType) {
 		case 'archive date':
 		case 'calendar head':
@@ -114,7 +114,7 @@ function convertDateFormat($argTarget, $argType) {
 			}
 			break;
 	}
-	
+
 	$rgCustomIdentifier = array();
 	if ($strYear != false) {
 		$rgCustomIdentifier['%Y'] = $strYear;
@@ -158,7 +158,7 @@ function convertDateFormat($argTarget, $argType) {
 		$rgCustomIdentifier['%S'] = $strSecond;
 		$rgCustomIdentifier['%s'] = removeHeadZero($strSecond);
 	}
-	
+
 	$newTarget = strtr($rgDateFormat[$argType]['format'], $rgCustomIdentifier);
 	$newTarget = preg_replace('/\{[^\{]*%[a-z][^\}]*\}/i', '', $newTarget);
 
