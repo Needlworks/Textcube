428f4a6e4d2985d4d077b9c7e85a365f275e06ca
inureyes <inureyes@gmail.com>
2009-04-28 15:24:32 +0000

    refs #777   * httprequest 앨리어스 추가

---
+++ framework/alias/HTTPRequest.php
@@ -0,0 +1,8 @@
+<?php
+/// Copyright (c) 2004-2009, Needlworks / Tatter Network Foundation
+/// All rights reserved. Licensed under the GPL.
+/// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
+require_once ROOT.'/framework/utils/HTTPRequest.php';
+class HTTPRequest extends Utils_HTTPRequest {
+}
+?>
