b6814e7524f9b04a2f29d5b0ccf9af60379fcabd
inureyes <inureyes@gmail.com>
2009-05-07 06:29:33 +0000

    refs #777   * doc  디렉토리를  documents  로 변경

--- interface/blog/plugin.php
+++ interface/blog/plugin.php
@@ -3,9 +3,7 @@
 /// All rights reserved. Licensed under the GPL.
 /// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
 require ROOT . '/library/preprocessor.php';
-if (false) {
-	fetchConfigVal();
-}
+
 fireEvent($suri['directive'] . '/' . $suri['value']);
 if (!headers_sent())
 	respond::NotFoundPage();
