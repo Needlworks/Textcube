182008e03fac64504fafef29680c4a5b93f69032
inureyes <inureyes@gmail.com>
2009-05-30 00:09:51 +0000

    refs #1278   *[7539] 를 1.7-backport 트리에 반영

--- library/components/Textcube.Function.Setting.php
+++ library/components/Textcube.Function.Setting.php
@@ -282,7 +282,7 @@ class setting {
 		return POD::execute("DELETE FROM {$database['prefix']}UserSettings WHERE userid = ".(is_null($userid) ? getUserId() : $userid)." AND name = '".POD::escapeString($name)."'");
 	}
 
-	function getServiceSetting($name, $value, $global = null) {
+	function getServiceSetting($name, $default, $global = null) {
 		global $database, $__serviceSetting;
 		if(is_null($global)) $name = 'plugin_' . $name;
 		if( empty($__serviceSetting) ) {
