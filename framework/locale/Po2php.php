faa3dd80eea9d261acef050046d3bdc0f610a011
graphittie <graphittie@gmail.com>
2009-06-19 20:09:26 +0000

    refs #777  * [7630/old_svn] 관련.

--- framework/locale/Po2php.php
+++ framework/locale/Po2php.php
@@ -3,7 +3,7 @@
 /// All rights reserved. Licensed under the GPL.
 /// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
 
-class Po2php
+class Locale_Po2php
 {
 	var $msgs;
 	var $nomsgs;
