504b9fa5a4239180dd1e844eee77842c36731afa
inureyes <inureyes@gmail.com>
2009-07-24 04:02:40 +0000

    refs #1300   * line 의 입력시  input validation 을 하도록 함.   * 센터에서 바로 글적기 기능때 사용할 수 있도록  POST  값에 대한 처리도 하도록 수정

--- interface/blog/line.php
+++ interface/blog/line.php
@@ -2,16 +2,35 @@
 /// Copyright (c) 2004-2009, Needlworks / Tatter Network Foundation
 /// All rights reserved. Licensed under the GPL.
 /// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
+$IV = array(
+	'GET' => array(
+		'key' => array('string','default'=>''),
+		'content' => array('string','default'=>''),
+		'page' => array('int',1,'default'=>'')
+	),
+	'POST' => array(
+		'key' => array('string','default'=>''),
+		'content' => array('string','default'=>'')
+	)
+);
 
 require ROOT . '/library/preprocessor.php';
+
+if(!empty($_POST['key']) && !empty($_POST['content'])) {
+	$key = $_POST['key'];
+	$content = $_POST['content'];
+} else {
+	$key = $_GET['key'];
+	$content = $_GET['content'];
+}	
 
 $lineobj = Line::getInstance();
 $lineobj->reset();
 // If line comes.
-if(isset($_GET['key']) && isset($_GET['content'])) {
+if(!empty($key) && !empty($content)) {
 	$password = Setting::getBlogSetting('LinePassword', null, true);
-	if($password == $_GET['key']) {
-		$lineobj->content = $_GET['content'];
+	if($password == $key) {
+		$lineobj->content = $content;
 		$result = $lineobj->add();
 		$cache = new pageCache;
 		$cache->name = 'linesATOM';
