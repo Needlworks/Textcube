6993d656fff568cf4f02747305a66a77db839d40
inureyes <inureyes@gmail.com>
2009-08-14 02:41:03 +0000

    refs #777   * import function  추가.   * requireXXX  들은 다음 버전에서 사라집니다.

--- framework/boot/20-Autoload.php
+++ framework/boot/20-Autoload.php
@@ -3,43 +3,29 @@
 /// All rights reserved. Licensed under the GPL.
 /// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
 
-
+/// Legacy support
 require_once(ROOT.'/framework/legacy/Needlworks.PHP.Loader.php');
-/*
-global $__requireBasics, $__requireLibrary, $__requireComponent, $__requireModel, $__requireView;
-if(!isset($__requireBasics)) $__requireBasics = array();
-if(!isset($__requireLibrary)) $__requireLibrary = array();
-if(!isset($__requireComponent)) $__requireComponent = array();
-if(!isset($__requireModel)) $__requireModel = array();
-if(!isset($__requireView)) $__requireView = array();
-if(!isset($service)) $service = array();
 
-function requireComponent($name) {
+/// This function will override the current requireXXX functions.
+/// python-style import 
+function import () {
+	$args = func_get_args();
+	if(empty($args)) return false;
+	foreach($args as $libPath) {
+		$paths = explode(".",$libPath);
+		if(end($paths) == "*") {
+			array_pop($paths);
+			foreach (new DirectoryIterator(ROOT.'/framework/'.implode("/",$paths) as $fileInfo) {
+				if($fileInfo->isFile()) require_once($fileInfo->getPathname());
+			}
+		} else {
+			require_once ROOT.'/framework/'.$str_replace(".","/",$libPath).".php";
+		}
+	}
 	return true;
 }
 
-function requireModel($name) {
-	global $__requireModel;
-	if(!in_array($name,$__requireModel)) {
-		include_once (ROOT . "/library/model/$name.php");
-		array_push($__requireModel,$name);
-	}
-}
-function requireView($name) {
-	global $__requireView;
-	if(!in_array($name,$__requireView)) {
-		include_once (ROOT . "/library/view/$name.php");
-		array_push($__requireView,$name);
-	}
-}
-function requireLibrary($name) {
-	global $__requireLibrary;
-	if(!in_array($name,$__requireLibrary)) {
-		include_once (ROOT . "/library/$name.php");
-		array_push($__requireLibrary,$name);
-	}
-}
-*/
+/// Autoload function
 class Autoload {
 	static function load($className) {
 		$pos =strrpos($className,'_');
