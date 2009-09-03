559ef559ac114839a17b075814ef01512a73c5f7
inureyes <inureyes@gmail.com>
2009-09-03 15:58:58 +0000

    refs #1322   *  세션 크리에이션 단계에서 도메인을 항상 규격화된 값을 참조하도록 함.

--- library/preprocessor.php
+++ library/preprocessor.php
@@ -126,7 +126,7 @@ if (!defined('NO_SESSION')) {
 	Session::set();
 	session_set_save_handler( array('Session','open'), array('Session','close'), array('Session','read'), array('Session','write'), array('Session','destroy'), array('Session','gc') );
 	session_cache_expire(1);
-	session_set_cookie_params(0, '/', $config->service['domain']);
+	session_set_cookie_params(0, '/', $config->service['session_cookie_domain']);
 	if (session_start() !== true) {
 		header('HTTP/1.1 503 Service Unavailable');
 		exit;
