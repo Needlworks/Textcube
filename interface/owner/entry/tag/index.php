624d2353d7c71cc161a5f3b555ceeee2adca82df
inureyes <inureyes@gmail.com>
2009-09-06 14:44:23 +0000

    refs #1248   *  어떻게 고칠지 대략적인 레이아웃을 잡아서 저장

--- interface/owner/entry/tag/index.php
+++ interface/owner/entry/tag/index.php
@@ -40,13 +40,12 @@ require ROOT . '/interface/common/owner/header.php';
 ?>
 						<script type="text/javascript">
 							//<![CDATA[
-							function updateTagEntries(id) {
-								var request = new HTTPRequest("POST","<?php echo $blogURL;?>/owner/entry/tag/entryList/");
-								var page = 1;
+							function updateTagPanel(id) {
+								var request = new HTTPRequest("POST","<?php echo $blogURL;?>/owner/entry/tag/panel/");
 								request.onSuccess = function () {
 									PM.removeRequest(this);
 									entryView = this.getText("/response/entryView");
-									updateEntryList(entryView);
+									updatePanel(entryView);
 									PM.showMessage("<?php echo _t('태그에 해당되는 글 목록을 불러왔습니다.');?>", "center", "bottom");
 								}
 								request.onError = function () {
@@ -54,9 +53,9 @@ require ROOT . '/interface/common/owner/header.php';
 									PM.showErrorMessage("<?php echo _t('글 목록을 불러올 수 없었습니다.');?>","center","bottom");
 								}
 								PM.addRequest(request, "<?php echo _t('글 목록을 불러오고 있습니다.');?>");
-								request.send("tagId="+id+"&page="+page);
+								request.send("tagId="+id);
 							}
-							function updateEntryList(contentView) {
+							function updatePanel(contentView) {
 								Econtent = document.getElementById("tag-entries");
 								Econtent.innerHTML = contentView;
 								return true;							
@@ -73,22 +72,39 @@ require ROOT . '/interface/common/owner/header.php';
 							<div id="tag-cloud">
 <?php
 	foreach($tags as $t):
-		echo '<a href="#" class="tag" onclick="updateTagEntries('.$t['id'].');return false;">'.$t['name'].'</a> ';
+		echo '<a href="#" class="tag" onclick="updateTagPanel('.$t['id'].');return false;">'.$t['name'].'</a> ';
 	endforeach;
 ?>
 							</div>
-							<div id="tag-entries">
-<?php
-	if(is_null($list)) {
-		echo '								<span class="text notice">'._t("태그를 선택하면 해당되는 글 목록을 볼 수 있습니다").'</span>';
-	} else {
-		foreach($list as $l):
-			
-		endforeach;
-	}
-?>
+							<div id="tag-panel">
+								<dl>
+									<dt><?php echo _t('현재 태그');?></dt>
+									<dd id="tag-name"><?php echo _t('선택되지 않았습니다.');?></dd>
 
-							</div>
+									<dd id="tag-number-of-posts"><?php echo _t('해당 태그가 포함된 글 수');?> :  <span id="tag-number-of-posts-value" class="text">0</span></dd>
+								</dl>
+								<dl>
+									<dt><?php echo _t('글목록');?></dt>
+									<dd>
+										<span class="text"><?php echo _t('이 태그를 갖는 모든 글의 목록을 봅니다');?></span>
+									</dd>
+								</dl>
+								<dl>
+									<dt><?php echo _t('태그 이름 변경');?></dt>
+									<dd>
+										<input type="text" id="tag-newname" class="input-text" name="tag-newname" value="" />
+										<br />
+										<label for="tagnewname"><?php echo _t('현재의 태그를 새로운 이름으로 변경합니다. 같은 태그를 가지고 있는 모든 글의 태그 이름이 함께 바뀌게 됩니다.');?></label>
+									</dd>
+								</dl>
+								<dl>
+									<dt><?php echo _t('태그 삭제');?></dt>
+									<dd>
+										<input type="button" id="tagDelete" class="input-button" name="tagDelete" value="<?php echo _t('삭제');?>" />
+										<br />
+										<label for="tagName"><?php echo _t('이 태그를 삭제합니다. 같은 태그를 가지고 있는 모든 글에서 이 태그가 삭제됩니다.');?></label>
+									</dd>
+								</dl>								
 						</div>
 
 						<hr class="hidden clear" />
