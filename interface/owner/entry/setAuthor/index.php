57aacf715d8b73128bab25a71c82e247c74f6613
yupmin <yupmin@gmail.com>
2009-11-10 16:04:47 +0000

    refs #1367  * 글목록 페이지에서 글작성자를 바꿀수 있음  * 2명이상의 팀블로그에서만, 관리자 이상에서만 기능 활성

---
+++ interface/owner/entry/setAuthor/index.php
@@ -0,0 +1,25 @@
+<?php
+/// Copyright (c) 2004-2009, Needlworks / Tatter Network Foundation
+/// All rights reserved. Licensed under the GPL.
+/// See the GNU General Public License for more details. (/doc/LICENSE, /doc/COPYRIGHT)
+$IV = array(
+	'POST' => array(
+		'targets' => array ('list'),
+		'userid' => array ('id')
+	)
+);
+
+require ROOT . '/library/includeForBlogOwner.php';
+requireStrictRoute();
+requireModel("blog.entry");
+
+$username = User::getName($_POST['userid']);
+if(Acl::check('group.administrators')) {
+	if(!is_null($username) && changeAuthorOfEntries($blogid, $_POST['targets'], $_POST['userid'])) {
+		respond::PrintResult(array('error' => 0, 'name' => $username));
+	} else
+		respond::PrintResult(array('error' => 1, 'message' => _t('존재하지 않은 사용자입니다')));
+} else
+	respond::PrintResult(array('error' => 1, 'message' => _t('권한이 없습니다.')));
+
+?>
