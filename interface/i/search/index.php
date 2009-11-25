904501d7bf3928bd6a090e03d71d566caf12d2ce
inureyes <inureyes@gmail.com>
2009-11-25 07:53:31 +0000

    refs #1249   * 검색 결과 부분의 인터페이스 수정   * TODO : 검색어 입력시 유니코드 문자열이 전송되고 있음.

--- interface/i/search/index.php
+++ interface/i/search/index.php
@@ -5,9 +5,7 @@
 define('__TEXTCUBE_IPHONE__', true);
 require ROOT . '/library/preprocessor.php';
 requireView('iphoneView');
-if (false) {
-	fetchConfigVal();
-}
+
 $search = isset($_GET['search']) ? $_GET['search'] : $suri['value'];
 $search = isset($_GET['q']) ? $_GET['q'] : $search; // Consider the common search query GET name. (for compatibility)
 if(strlen($search) > 0 && !empty($suri['page'])) {
@@ -16,11 +14,11 @@ if(strlen($search) > 0 && !empty($suri['page'])) {
 	$list = array('title' => $search, 'items' => $listWithPaging[0], 'count' => $listWithPaging[1]['total']);
 	$paging = $listWithPaging[1];
 	?>
-	<ul class="search" id="search_<?php echo $suri['page'];?>" title="Search List" selected="false">
+	<ul class="search" id="search_<?php echo $suri['page'];?>" title="<?php echo _text('검색 결과');?>" selected="false">
 	<?php
 		$itemsView .= '<li class="group">'.CRLF;
-		$itemsView .= '	<span class="left">Search List ('.$list['count'].')</span>'.CRLF;
-		$itemsView .= '	<span class="right">Page <span class="now_page">' . $paging['page'] . '</span> / '.$paging['pages'].'</span>'.CRLF;
+		$itemsView .= '	<span class="left">'._text('검색 결과').'('.$list['count'].')</span>'.CRLF;
+		$itemsView .= '	<span class="right">'._text('페이지').'<span class="now_page">' . $paging['page'] . '</span> / '.$paging['pages'].'</span>'.CRLF;
 		$itemsView .= '</li>'.CRLF;
 		foreach ($list['items'] as $item) {	
 			$author = User::getName($item['userid']);
