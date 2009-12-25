a49b70bcf554e25721e9b13a3ed2f04162be86cd
inureyes <inureyes@gmail.com>
2009-12-25 02:32:28 +0000

    refs #1074
     * 언어 리소스 호출시 에러레벨에 따라 문제가 생기는 부분 수정
     * reported by 건더기

--- framework/locale/Locale.php
+++ framework/locale/Locale.php
@@ -4,7 +4,6 @@
 /// See the GNU General Public License for more details. (/documents/LICENSE, /documents/COPYRIGHT)
 
 final class Locale extends Singleton {
-	static $directory, $domain, $locale, $resource, $defaultLanguage;
 	public static function getInstance() {
 		return self::_getInstance(__CLASS__);
 	}
@@ -13,10 +12,18 @@ final class Locale extends Singleton {
 	public function get() {
 		return $this->locale;
 	}
+	
+	public function reset() {
+		$this->defaultLanguage = null;
+		$this->resource = $this->locale = array();
+		$this->directory = null;	
+	}
 
 	public function set($locale, $domain = 'global') {
 		list($common) = explode('-', $locale, 2);
-
+		if(!isset($this->resource)) $this->resource = array();
+		if(!isset($this->locale)) $this->locale = array();
+		if(!isset($this->directory)) $this->directory = null;
 		if (file_exists($this->directory . '/' . $locale .'.php')) { // If locale file exists
 			$this->resource[$domain] = $this->includeLocaleFile($this->directory . '/' . $locale . '.php');
 			$this->locale[$domain] = $locale;
