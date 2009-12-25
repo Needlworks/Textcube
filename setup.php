80923d4193ad1463406dd4d6b6f3815ac8211264
inureyes <inureyes@gmail.com>
2009-12-25 00:37:02 +0000

    refs #1074   * 설치시 쿼리에 문제가 될 수 있는 부분 수정   * mysql 설정을 타는듯

--- setup.php
+++ setup.php
@@ -1199,9 +1199,9 @@ INSERT INTO {$_POST['dbPrefix']}Entries (blogid, userid, id, category, visibilit
             $schema = "
 				UPDATE {$_POST['dbPrefix']}Users SET loginid = '$loginid', name = '$name' WHERE userid = 1;
 				UPDATE {$_POST['dbPrefix']}Users SET password = '$password' WHERE userid = 1 AND password <> '$password2';
-				UPDATE {$_POST['dbPrefix']}BlogSettings SET \"value\" = '{$_POST['blog']}' where blogid = 1 AND name = 'name';
-				UPDATE {$_POST['dbPrefix']}BlogSettings SET \"value\" = '$baseLanguage' where blogid = 1 AND name = 'language';
-				UPDATE {$_POST['dbPrefix']}BlogSettings SET \"value\" = '$baseTimezone' where blogid = 1 AND name = 'timezone';";
+				UPDATE {$_POST['dbPrefix']}BlogSettings SET 'value' = '{$_POST['blog']}' where blogid = 1 AND name = 'name';
+				UPDATE {$_POST['dbPrefix']}BlogSettings SET 'value' = '$baseLanguage' where blogid = 1 AND name = 'language';
+				UPDATE {$_POST['dbPrefix']}BlogSettings SET 'value' = '$baseTimezone' where blogid = 1 AND name = 'timezone';";
             $query = explode(';', trim($schema));
             foreach ($query as $sub) {
                 if (!empty($sub) && !POD::query($sub)) {
