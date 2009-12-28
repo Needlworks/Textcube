1cb56ecbd4707835488d032431fd25882b17eeaf
inureyes <inureyes@gmail.com>
2009-12-28 01:52:47 +0000

    refs #1074   * 사이드바 모듈 출력시 스킨 정보를 제대로 읽지 못하는 문제 수정   * reported by 노랭이군 (http://notice.textcube.org/ko/192#comment4070)

--- interface/owner/skin/sidebar/index.php
+++ interface/owner/skin/sidebar/index.php
@@ -43,7 +43,7 @@ function correctImagePath($match ) {
 		return $match[0] ; // full url의 경우 스킵
 	if( $pathArr[0] != '.'  && $pathArr[0] != '..' ) 
 		return $match[0] ; //첫 디렉토리가 현재 디렉토리가 아닌경우 스킵
-	return str_replace( $match[1],  $serviceURL . "/skin/{$skinSetting['skin']}/" . $match[1], $match[0]);
+	return str_replace( $match[1],  $serviceURL . "/skin/blog/{$skinSetting['skin']}/" . $match[1], $match[0]);
 }
 
 if (false) correctImagePath('');
