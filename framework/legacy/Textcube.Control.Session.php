f0cb55cc1a1f40c0a456ef56d1b55af6838d5f34
inureyes <inureyes@gmail.com>
2010-01-05 16:37:09 +0000

    refs #1322   * 세션 구조를 새 구조에 맞게 재작성

--- framework/legacy/Textcube.Control.Session.php
+++ framework/legacy/Textcube.Control.Session.php
@@ -10,10 +10,16 @@ final class Session {
 	private static $sessionName = null;
 	private static $sessionDBRepair = false;
 	private static $sConfig = null;
+	private static $context = null;
+	
 	function __construct() {
 		$sessionMicrotime = Timer::getMicroTime();
 	}
 	
+	function initialize() {
+		self::$context = Model_Context::getInstance();
+	}
+
 	public static function open($savePath, $sessionName) {
 		return true;
 	}
@@ -23,12 +29,12 @@ final class Session {
 	}
 	
 	public static function getName() {
-		$config = Model_Config::getInstance();
+		if(is_null(self::$context)) self::initialize();	
 		if( self::$sessionName == null ) { 
 			if( !empty($service['session_cookie']) ) {
-				self::$sessionName = $config->service['session_cookie'];
+				self::$sessionName = self::$context->getProperty('service.session_cookie');
 			} else {
-				self::$sessionName = 'TSSESSION'.$config->service['domain'].$config->service['path']; 
+				self::$sessionName = 'TSSESSION'.self::$context->getProperty('service.domain').self::$context->getProperty('service.path'); 
 				self::$sessionName = preg_replace( '/[^a-zA-Z0-9]/', '', self::$sessionName );
 			}
 		}
@@ -36,16 +42,18 @@ final class Session {
 	}
 	
 	public static function read($id) {
-		$config = Model_Config::getInstance();
-		if ($result = self::query('cell',"SELECT privilege FROM {$config->database['prefix']}Sessions 
-			WHERE id = '$id' AND address = '{$_SERVER['REMOTE_ADDR']}' AND updated >= (UNIX_TIMESTAMP() - {$config->service['timeout']})")) {
+		if(is_null(self::$context)) self::initialize();
+
+		if ($result = self::query('cell',"SELECT privilege FROM ".self::$context->getProperty('database.prefix')."Sessions 
+			WHERE id = '$id' AND address = '{$_SERVER['REMOTE_ADDR']}' AND updated >= (UNIX_TIMESTAMP() - ".self::$context->getProperty('service.timeout'))) {
 			return $result;
 		}
 		return '';
 	}
 	
 	public static function write($id, $data) {
-		$config = Model_Config::getInstance();
+		if(is_null(self::$context)) self::initialize();
+
 		if (strlen($id) < 32)
 			return false;
 		$userid = Acl::getIdentity('textcube');
@@ -58,7 +66,7 @@ final class Session {
 		$request = POD::escapeString(substr($_SERVER['REQUEST_URI'], 0, 255));
 		$referer = isset($_SERVER['HTTP_REFERER']) ? POD::escapeString(substr($_SERVER['HTTP_REFERER'],0,255)) : '';
 		$timer = Timer::getMicroTime() - self::$sessionMicrotime;
-		$result = self::query('count',"UPDATE {$config->database['prefix']}Sessions 
+		$result = self::query('count',"UPDATE ".self::$context->getProperty('database.prefix')."Sessions 
 				SET userid = $userid, privilege = '$data', server = '$server', request = '$request', referer = '$referer', timer = $timer, updated = UNIX_TIMESTAMP() 
 				WHERE id = '$id' AND address = '{$_SERVER['REMOTE_ADDR']}'");
 		if ($result && $result == 1) {
@@ -69,42 +77,42 @@ final class Session {
 	}
 	
 	public static function destroy($id, $setCookie = false) {
-		$config = Model_Config::getInstance();
-		@self::query('cell',"DELETE FROM {$config->database['prefix']}Sessions 
+		if(is_null(self::$context)) self::initialize();
+		@self::query('cell',"DELETE FROM ".self::$context->getProperty('database.prefix')."Sessions 
 			WHERE id = '$id' AND address = '{$_SERVER['REMOTE_ADDR']}'");
 		self::gc();
 	}
 	
 	public static function gc($maxLifeTime = false) {
-		$config = Model_Config::getInstance();
-		@self::query('query',"DELETE FROM {$config->database['prefix']}Sessions 
-			WHERE updated < (UNIX_TIMESTAMP() - {$config->service['timeout']})");
+		if(is_null(self::$context)) self::initialize();
+		@self::query('query',"DELETE FROM ".self::$context->getProperty('database.prefix')."Sessions 
+			WHERE updated < (UNIX_TIMESTAMP() - ".self::$context->getProperty('service.timeout').")");
 		$result = @self::query('all',"SELECT DISTINCT v.id, v.address 
-			FROM {$config->database['prefix']}SessionVisits v 
-			LEFT JOIN {$config->database['prefix']}Sessions s ON v.id = s.id AND v.address = s.address 
+			FROM ".self::$context->getProperty('database.prefix')."SessionVisits v 
+			LEFT JOIN ".self::$context->getProperty('database.prefix')."Sessions s ON v.id = s.id AND v.address = s.address 
 			WHERE s.id IS NULL AND s.address IS NULL");
 		if ($result) {
 			$gc = array();
 			foreach ($result as $g)
 				array_push($gc, $g);
 			foreach ($gc as $g)
-				@self::query('query',"DELETE FROM {$config->database['prefix']}SessionVisits WHERE id = '{$g[0]}' AND address = '{$g[1]}'");
+				@self::query('query',"DELETE FROM ".self::$context->getProperty('database.prefix')."SessionVisits WHERE id = '{$g[0]}' AND address = '{$g[1]}'");
 		}
 		return true;
 	}
 	
 	private static function getAnonymousSession() {
-		$config = Model_Config::getInstance();
-		$result = self::query('cell',"SELECT id FROM {$config->database['prefix']}Sessions WHERE address = '{$_SERVER['REMOTE_ADDR']}' AND userid IS NULL AND preexistence IS NULL");
+		if(is_null(self::$context)) self::initialize();
+		$result = self::query('cell',"SELECT id FROM ".self::$context->getProperty('database.prefix')."Sessions WHERE address = '{$_SERVER['REMOTE_ADDR']}' AND userid IS NULL AND preexistence IS NULL");
 		if ($result)
 			return $result;
 		return false;
 	}
 	
 	private static function newAnonymousSession() {
-		$config = Model_Config::getInstance();
+		if(is_null(self::$context)) self::initialize();
 		$meet_again_baby = 3600;
-		if( isset($config->service['timeout']) ) { 
+		if( !empty(self::$context->getProperty('service.timeout'))) { 
 			$meet_again_baby = $config->service['timeout'];
 		}
 
@@ -115,7 +123,7 @@ final class Session {
 			if (($id = self::getAnonymousSession()) !== false)
 				return $id;
 			$id = dechex(rand(0x10000000, 0x7FFFFFFF)) . dechex(rand(0x10000000, 0x7FFFFFFF)) . dechex(rand(0x10000000, 0x7FFFFFFF)) . dechex(rand(0x10000000, 0x7FFFFFFF));
-			$result = self::query('count',"INSERT INTO {$config->database['prefix']}Sessions (id, address, created, updated) VALUES('$id', '{$_SERVER['REMOTE_ADDR']}', UNIX_TIMESTAMP(), UNIX_TIMESTAMP() - $meet_again_baby)");
+			$result = self::query('count',"INSERT INTO ".self::$context->getProperty('database.prefix')."Sessions (id, address, created, updated) VALUES('$id', '{$_SERVER['REMOTE_ADDR']}', UNIX_TIMESTAMP(), UNIX_TIMESTAMP() - $meet_again_baby)");
 			if ($result > 0)
 				return $id;
 		}
@@ -139,9 +147,10 @@ final class Session {
 	
 	public static function isAuthorized($id) {
 		/* OpenID and Admin sessions are treated as authorized ones*/
-		$config = Model_Config::getInstance();
+		if(is_null(self::$context)) self::initialize();
+
 		$result = self::query('cell',"SELECT id 
-			FROM {$config->database['prefix']}Sessions 
+			FROM ".self::$context->getProperty('database.prefix')."Sessions 
 			WHERE id = '$id' 
 				AND address = '{$_SERVER['REMOTE_ADDR']}' 
 				AND (userid IS NOT NULL OR preexistence IS NOT NULL)");
@@ -151,9 +160,9 @@ final class Session {
 	}
 	
 	public static function isGuestOpenIDSession($id) {
-		$config = Model_Config::getInstance();
+		if(is_null(self::$context)) self::initialize();
 		$result = self::query('cell',"SELECT id 
-			FROM {$config->database['prefix']}Sessions 
+			FROM ".self::$context->getProperty('database.prefix')."Sessions 
 			WHERE id = '$id' 
 				AND address = '{$_SERVER['REMOTE_ADDR']}' AND userid < 0");
 		if ($result)
@@ -177,10 +186,11 @@ final class Session {
 	}
 	
 	public static function authorize($blogid, $userid) {
-		$config = Model_Config::getInstance();
+		if(is_null(self::$context)) self::initialize();
+
 		$session_cookie_path = "/";
-		if( !empty($config->service['session_cookie_path']) ) {
-			$session_cookie_path = $config->service['session_cookie_path'];
+		if( !empty(self::$context->getProperty('service.session_cookie_path')) ) {
+			$session_cookie_path = self::$context->getProperty('service.session_cookie_path');
 		}
 		if (!is_numeric($userid))
 			return false;
@@ -188,8 +198,8 @@ final class Session {
 			$_SESSION['userid'] = $userid;
 			$id = session_id();
 			if( self::isGuestOpenIDSession($id) ) {
-				$result = self::query('execute',"UPDATE {$config->database['prefix']}Sessions
-					set userid = $userid WHERE id = '$id' AND address = '{$_SERVER['REMOTE_ADDR']}'");
+				$result = self::query('execute',"UPDATE ".self::$context->getProperty('database.prefix')."Sessions
+					SET userid = $userid WHERE id = '$id' AND address = '{$_SERVER['REMOTE_ADDR']}'");
 				if ($result) {
 					return true;
 				}
@@ -199,7 +209,7 @@ final class Session {
 			return true;
 		for ($i = 0; $i < 3; $i++) {
 			$id = dechex(rand(0x10000000, 0x7FFFFFFF)) . dechex(rand(0x10000000, 0x7FFFFFFF)) . dechex(rand(0x10000000, 0x7FFFFFFF)) . dechex(rand(0x10000000, 0x7FFFFFFF));
-			$result = self::query('execute',"INSERT INTO {$config->database['prefix']}Sessions
+			$result = self::query('execute',"INSERT INTO ".self::$context->getProperty('database.prefix')."Sessions
 				(id, address, userid, created, updated) 
 				VALUES('$id', '{$_SERVER['REMOTE_ADDR']}', $userid, UNIX_TIMESTAMP(), UNIX_TIMESTAMP())");
 			if ($result) {
@@ -214,11 +224,12 @@ final class Session {
 
 	/* Customized queryset (for recovering Session tables) */
 	private static function query($mode='query',$sql) {
-		$config = Model_Config::getInstance();
+		if(is_null(self::$context)) self::initialize();
+
 		$result = self::DBQuery($mode,$sql);
 		if($result === false) {
 			if (self::$sessionDBRepair === false) {		
-				@POD::query("REPAIR TABLE {$config->database['prefix']}Sessions, {$config->database['prefix']}SessionVisits");
+				@POD::query("REPAIR TABLE ".self::$context->getProperty('database.prefix')."Sessions, ".self::$context->getProperty('database.prefix')."SessionVisits");
 				$result = self::DBQuery($mode,$sql);
 				self::$sessionDBRepair = true;
 			}
