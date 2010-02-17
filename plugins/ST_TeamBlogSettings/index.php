3d57665026e53c4151f0d91678f118496108b0ca
yupmin <yupmin@gmail.com>
2010-02-17 18:06:04 +0000

    refs #1441  * service['path']로 재조정

--- plugins/ST_TeamBlogSettings/index.php
+++ plugins/ST_TeamBlogSettings/index.php
@@ -81,7 +81,7 @@ function getTeamProfile($userid){
 }
 
 function getTeamBlogSettings() {
-	global $database, $serviceURL, $blogURL, $pluginURL, $configVal;
+	global $database, $service, $serviceURL, $blogURL, $pluginURL, $configVal;
 	requireComponent('Textcube.Function.misc');
 	$data = setting::fetchConfigVal($configVal);
 	getTeamBlogInitConfigVal($data);
@@ -94,10 +94,10 @@ function getTeamBlogSettings() {
 		POD::execute("INSERT INTO {$database['prefix']}TeamUserSettings (blogid,userid,style,image,profile,updated) VALUES('".getBlogId()."','".getUserId()."','','', '',UNIX_TIMESTAMP())");
 	}
 	if($row['image']){
-		$image = "{$blogURL}/attach/".getBlogId()."/team/".$row['image'];
+		$image = "{$service['path']}/attach/".getBlogId()."/team/".$row['image'];
 		$imageRemoveCheck = "";
 	}else{
-		$image = "{$blogURL}/image/spacer.gif";
+		$image = "{$service['path']}/image/spacer.gif";
 		$imageRemoveCheck = " disabled ";
 	}
 
