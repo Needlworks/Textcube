3e162dbccc1f240b24d7d0efe9887dd717a14a6b
inureyes <inureyes@gmail.com>
2010-07-06 08:43:10 +0000

    refs #1482   * autoload를 위하여 클래스 이름을 Utils_Image 로 변경.

--- framework/utils/Image.php
+++ framework/utils/Image.php
@@ -2,8 +2,8 @@
 /// Copyright (c) 2004-2010, Needlworks  / Tatter Network Foundation
 /// All rights reserved. Licensed under the GPL.
 /// See the GNU General Public License for more details. (/documents/LICENSE, /documents/COPYRIGHT)
-class Image {
-	function Image() {
+class Utils_Image {
+	function __construct() {
 		$this->reset();
 	}
 	
