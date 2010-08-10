a05139eef0cf5e2dd1cf22f3424d0b6ccdc7f187
inureyes <inureyes@gmail.com>
2010-08-10 04:27:42 +0000

    closes #1446   * User can get permission simultaneously after adding new blog.

--- interface/control/action/blog/add/index.php
+++ interface/control/action/blog/add/index.php
@@ -15,6 +15,11 @@ requirePrivilege('group.creators');
 if ($uid = User::getUserIdByEmail($_GET['owner'])) {
 	$result = addBlog('',$uid, $_GET['identify']);
 	if ($result===true) {
+		if($_GET['owner'] == User::getEmail(getUserId())) {	/// Update current user's access list.
+			$priv = array();
+			array_push($priv, "group.administrators");
+			Acl::setAcl( $rec['blogid'], $priv, true );
+		}
 		Respond::PrintResult(array('error' => 0));
 	}
 	else {
