ab58633276982346a24c39c271ee9fc48b15f5be
inureyes <inureyes@gmail.com>
2010-09-03 14:25:08 +0000

    refs #1508   * 스킨 읽는 부분 체크

--- framework/legacy/Textcube.Function.Setting.php
+++ framework/legacy/Textcube.Function.Setting.php
@@ -372,7 +372,7 @@ class Setting {
 				'commentsOnRecent' => 5, 
 				'commentsOnGuestbook' => 5, 
 				'commentsOnEntry' => 15, 
-				'contentWidth' => 450, 
+				'contentWidth' => 550, 
 				'archivesOnPage' => 5,
 				'tagsOnTagbox' => 30, 
 				'tagboxAlign' => 3, 
