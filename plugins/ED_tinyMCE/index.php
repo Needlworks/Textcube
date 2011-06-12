2f6be15ee1d3f530b3ac06b184bc6be1a4c6cd03
inureyes <inureyes@gmail.com>
2011-06-12 00:52:02 -0400

    refs #1586 : Modified : plugins setting script modified.

--- plugins/ED_tinyMCE/index.php
+++ plugins/ED_tinyMCE/index.php
@@ -29,35 +29,30 @@ function tinyMCE_editorinit(&$editor) {
 				// General options
 				mode : 'exact',
 				theme : "advanced",
-				plugins : "autolink,lists,pagebreak,style,layer,table,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,TTMLsupport",
+				//plugins : "autolink,lists,pagebreak,style,layer,table,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,TTMLsupport",
+				plugins : "autolink,lists,pagebreak,style,table,advhr,advimage,advlink,emotions,iespell,inlinepopups,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,wordcount,advlist,TTMLsupport",
 
 				// Theme options
-				theme_advanced_buttons1 : "newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",								theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+				theme_advanced_buttons1 : "newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
+				theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
 				theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-				theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak,restoredraft",
+				theme_advanced_buttons4 : "styleprops,|,cite,abbr,acronym,del,ins,|,visualchars,nonbreaking,template,pagebreak,restoredraft",
 				theme_advanced_toolbar_location : "top",
 				theme_advanced_toolbar_align : "left",
 				theme_advanced_statusbar_location : "bottom",
 				theme_advanced_resizing : true,
 
 				// Example content CSS (should be your site CSS)
-				content_css : "<?php echo (file_exists(ROOT.'/skin/blog/'.$context->getProperty('skin.skin').'/wysiwyg.css') ? $context->getProperty('uri.default').'/skin/blog/'.$context->getProperty('skin.skin').'/wysiwyg.css' : '/css/content.css');?>",
+				content_css : "<?php echo (file_exists(ROOT.'/skin/blog/'.$context->getProperty('skin.skin').'/wysiwyg.css') ? $context->getProperty('uri.service').'/skin/blog/'.$context->getProperty('skin.skin').'/wysiwyg.css' : $context->getProperty('uri.service').'/resources/style/default-wysiwyg.css');?>",
 
-				// Drop lists for link/image/media/template dialogs
-				template_external_list_url : "lists/template_list.js",
+				// Drop lists for link/image/media dialogs
 				external_link_list_url : "lists/link_list.js",
 				external_image_list_url : "lists/image_list.js",
 				media_external_list_url : "lists/media_list.js",
 
 				// Style formats
 				style_formats : [
-					{title : 'Bold text', inline : 'b'},
-					{title : 'Red text', inline : 'span', styles : {color : '#ff0000'}},
-					{title : 'Red header', block : 'h1', styles : {color : '#ff0000'}},
-					{title : 'Example 1', inline : 'span', classes : 'example1'},
-					{title : 'Example 2', inline : 'span', classes : 'example2'},
-					{title : 'Table styles'},
-					{title : 'Table row 1', selector : 'tr', classes : 'tablerow1'}
+					{title : 'Bold text', inline : 'b'}
 				],
 				forced_root_block : false, 
 			});
