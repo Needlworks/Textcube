9df2ae68ec42a2bfd71c08a84fc6070867f3930e
inureyes <inureyes@gmail.com>
2011-06-13 01:21:38 -0400

    refs #1586 : Modified : advanced setting modified.

--- plugins/ED_tinyMCE/index.php
+++ plugins/ED_tinyMCE/index.php
@@ -3,6 +3,7 @@ function tinyMCE_handleconfig($configVal) {
 	$config = Setting::fetchConfigVal($configVal);
 	if (isset($config['defaultmode']) && $config['defaultmode'] != 'WYSIWYG' && $config['defaultmode'] != 'TEXTAREA') return false;
 	if (isset($config['paragraphdelim']) && $config['paragraphdelim'] != 'P' && $config['paragraphdelim'] != 'BR') return false;
+	if (isset($config['editormode']) && $config['editormode'] != 'simple' && $config['editormode'] != 'advanced') return false;
 	return true;
 }
 
@@ -13,24 +14,17 @@ function tinyMCE_editorinit(&$editor) {
 	if (is_null($configVal) || empty($configVal)) {
 		$config = array('paragraphdelim' => 'BR',
 			'defaultmode' => 'WYSIWYG',
-			'editormode' => 'simple');
+			'editormode' => 'advanced');
 	} else {
 		$config = Setting::fetchConfigVal($configVal);
 	}
-	if (in_array(Setting::getBlogSettingGlobal('defaultFormatter','html'),array('markdown','textile')) ||
-		in_array($entry['contentformatter'],array('markdown','textile'))) {
-		$config['defaultmode'] = 'TEXTAREA';
-	} else if (!isset($config['defaultmode'])) {
-		$config['defaultmode'] = 'WYSIWYG';
-	}
-
 	ob_start();
 ?>
 			var editor = new tinymce.Editor('editWindow', {
 				// General options
 				mode : 'exact',
 				theme : 'advanced',
-				language : '<?php echo $context->getProperty('blog.language');?>',
+				language : '<?php echo strtolower($context->getProperty('blog.language'));?>',
 				popup_css_add: "<?php echo $pluginURL;?>/popup.css",
 				forced_root_block : false, 
 <?php
@@ -45,11 +39,11 @@ function tinyMCE_editorinit(&$editor) {
 <?php
 	} else {
 ?>
-				plugins : "autolink,lists,pagebreak,style,table,advhr,advimage,advlink,emotions,iespell,inlinepopups,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,wordcount,advlist,TTMLsupport",
+				plugins : "autolink,lists,pagebreak,style,table,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,wordcount,advlist,TTMLsupport",
 				// Theme options
-				theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect,|,fullscreen",
-				theme_advanced_buttons2 : "search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-				theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+				theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect,|,preview,fullscreen",
+				theme_advanced_buttons2 : "search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,|,insertdate,inserttime,|,forecolor,backcolor,|,code,cleanup",
+				theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl",
 				theme_advanced_buttons4 : "styleprops,|,cite,abbr,acronym,del,ins,|,visualchars,nonbreaking,pagebreak,restoredraft",
 <?php
 	}
