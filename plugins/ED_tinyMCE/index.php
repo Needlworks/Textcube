b685b1f2fd901c3a47e2f049ea74bba817f38244
inureyes <inureyes@gmail.com>
2011-06-15 10:47:26 -0400

    refs #1586 : Added : more/less support.

--- plugins/ED_tinyMCE/index.php
+++ plugins/ED_tinyMCE/index.php
@@ -25,7 +25,7 @@ function tinyMCE_editorinit(&$editor) {
 				plugins : "autolink,autoresize,lists,style,advimage,advlink,emotions,inlinepopups,preview,media,contextmenu,fullscreen,noneditable,visualchars,xhtmlxtras,wordcount,advlist,TTMLsupport",
 				// Theme options
 				theme_advanced_buttons1 : "tcsave,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect,|,preview,fullscreen",
-				theme_advanced_buttons2 : "undo,redo,|,bullist,numlist,|,outdent,indent,blockquote,hr,|,tcattach,link,unlink,anchor,image,media,code,|,forecolor,backcolor,|,charmap,emotions,|,visualchars,restoredraft",
+				theme_advanced_buttons2 : "undo,redo,|,bullist,numlist,|,outdent,indent,blockquote,hr,tcmoreless,|,tcattach,link,unlink,anchor,image,media,code,|,forecolor,backcolor,|,charmap,emotions,|,visualchars,restoredraft",
 				theme_advanced_buttons3 : "",
 				theme_advanced_buttons4 : "",
 <?php
@@ -34,7 +34,7 @@ function tinyMCE_editorinit(&$editor) {
 				plugins : "autolink,autoresize,lists,pagebreak,style,table,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,wordcount,advlist,TTMLsupport",
 				// Theme options
 				theme_advanced_buttons1 : "tcsave,save,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect,|,cite,abbr,acronym,del,ins,|,visualchars,nonbreaking,pagebreak,restoredraft,|,styleprops,|,code,cleanup,|,preview,fullscreen",
-				theme_advanced_buttons2 : "undo,redo,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,sub,sup,|,tcattach,link,unlink,anchor,image,charmap,media,advhr,|,forecolor,backcolor,|,tablecontrols,|,hr,removeformat,visualaid,|,ltr,rtl",
+				theme_advanced_buttons2 : "undo,redo,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,tcmoreless,|,sub,sup,|,tcattach,link,unlink,anchor,image,charmap,media,advhr,|,forecolor,backcolor,|,tablecontrols,|,hr,removeformat,visualaid,|,ltr,rtl",
 				theme_advanced_buttons3 : "",
 				theme_advanced_buttons4 : "",
 <?php
