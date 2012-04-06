0afc295a9c28b873cf968bc6bf02abae8c1f42c6
inureyes <inureyes@gmail.com>
2012-04-06 14:06:05 +0900

    refs #514 : version number modification

--- framework/id/textcube/config.default.php
+++ framework/id/textcube/config.default.php
@@ -5,8 +5,8 @@
 
 // Define basic signatures.
 define('TEXTCUBE_NAME', 'Textcube');
-define('TEXTCUBE_VERSION', '1.9.0 : Beta 5');
-define('TEXTCUBE_REVISION', 'root-main-branch1.9-r29');
+define('TEXTCUBE_VERSION', '2.0.0 : Alpha 2');
+define('TEXTCUBE_REVISION', 'root-main-branch2.0-r30');
 define('TEXTCUBE_COPYRIGHT', 'Copyright &copy; 2004-2012. Needlworks / Tatter Network Foundation. All rights reserved. Licensed under the GPL.');
 define('TEXTCUBE_HOMEPAGE', 'http://www.textcube.org/');
 define('TEXTCUBE_RESOURCE_URL', 'http://resources.textcube.org/1.9.0');
