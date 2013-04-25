430455f11a71774d533fe225c7c335f088f3d1fb
Jeongkyu Shin <inureyes@gmail.com>
2013-04-25 14:41:58 +0900

    refs #1626: bugfix: change code to match webslice function

--- interface/owner/skin/coverpage/index.php
+++ interface/owner/skin/coverpage/index.php
@@ -146,11 +146,11 @@ function pretty_dress($view)
 	list($view, $authorList) = Skin::cutSkinTag($view, 'author_rep');
 	dress('author_rep', getAuthorListView($pd_authorList, $authorList), $view);
 	list($view, $recentEntry) = Skin::cutSkinTag($view, 'rctps_rep');	
-	dress('rctps_rep', getRecentEntriesView($pd_recentEntry, $recentEntry), $view);
+	dress('rctps_rep', getRecentEntriesView($pd_recentEntry, null, $recentEntry), $view);
 	list($view, $recentComments) = Skin::cutSkinTag($view, 'rctrp_rep');	
-	dress('rctrp_rep', getRecentCommentsView($pd_recentComment, $recentComments), $view);
+	dress('rctrp_rep', getRecentCommentsView($pd_recentComment,  null, $recentComments), $view);
 	list($view, $recentTrackback) = Skin::cutSkinTag($view, 'rcttb_rep');	
-	dress('rcttb_rep', getRecentTrackbacksView($pd_recentTrackback, $recentTrackback), $view);
+	dress('rcttb_rep', getRecentTrackbacksView($pd_recentTrackback,  null, $recentTrackback), $view);
 	list($view, $s_link_rep) = Skin::cutSkinTag($view, 'link_rep');	
 	dress('link_rep', getLinksView($pd_link, $s_link_rep), $view);
 	dress('rss_url', "$context->getProperty('uri.blog')/rss", $view);
