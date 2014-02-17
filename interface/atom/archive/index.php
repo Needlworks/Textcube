9164c8e1461eeb5cdacb9a7e891091f26352950b
Jeongkyu Shin <inureyes@gmail.com>
2014-02-17 23:19:25 +0900

    refs #1540 : enhancement - backport to 1.9

---
+++ interface/atom/archive/index.php
@@ -0,0 +1,36 @@
+<?php
+/// Copyright (c) 2004-2014, Needlworks  / Tatter Network Foundation
+/// All rights reserved. Licensed under the GPL.
+/// See the GNU General Public License for more details. (/documents/LICENSE, /documents/COPYRIGHT)
+define('NO_SESSION', true);
+define('__TEXTCUBE_CUSTOM_HEADER__', true);
+define('__TEXTCUBE_LOGIN__',true);
+
+require ROOT . '/library/preprocessor.php';
+requireStrictBlogURL();
+$context = Model_Context::getInstance();
+$period = $suri['id'];
+$blogid = getBlogId();
+
+$cache = pageCache::getInstance();
+$cache->reset('archiveAtom-'.$period);
+if(!$cache->load()) {
+	requireModel("blog.feed");
+	list($entries, $paging) = getEntriesWithPagingByPeriod($blogid, $period, 1, 1, 1);	
+	//var_dump($entries);
+	if(empty($entries)) {
+		header ("Location: ".$context->getProperty('uri.host').$context->getProperty('uri.blog')."/atom");
+		exit;	
+	}
+	$result = getFeedWithEntries($blogid,$entries,_textf('%1 기간의 글 목록',$period),'atom');
+	if($result !== false) {
+		$cache->reset('archiveAtom-'.$period);
+		$cache->contents = $result;
+		$cache->update();
+	}
+}
+header('Content-Type: application/rss+xml; charset=utf-8');
+fireEvent('FeedOBStart');
+echo fireEvent('ViewArchiveRSS', $cache->contents);
+fireEvent('FeedOBEnd');
+?>
