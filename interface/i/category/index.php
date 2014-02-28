37fa8e71734d1e50ae4ed929f3ae88b5ffbbf9bf
Jeongkyu Shin <inureyes@gmail.com>
2014-02-28 20:35:44 +0900

    refs #1649 : category view is modified. - reported by 홀맨 (http://notice.textcube.org/ko/247?commentId=121398#comment121398) - merged the patch from http://pat.im/1043

--- interface/i/category/index.php
+++ interface/i/category/index.php
@@ -5,16 +5,17 @@
 define('__TEXTCUBE_IPHONE__', true);
 require ROOT . '/library/preprocessor.php';
 requireView('iphoneView');
+$context = Model_Context::getInstance();
 printMobileHTMLHeader();
 printMobileHTMLMenu('','list');
 $category = $suri['id'];
 if(isset($category)) {
 	$blog['entriesOnList'] = 8;
-	if(!$listWithPaging = getEntriesWithPagingByCategory($blogid, $category, $suri['page'], $blog['entriesOnList'],1))
+	if(!$listWithPaging = getEntriesWithPagingByCategory($blogid, $category, $suri['page'], $blog['entriesOnList']))
 		$listWithPaging = array(array(), array('total' => 0));
 	$list = array('title' => (empty($suri['value']) ? getCategoryLabelById($blogid, 0) : $suri['value']), 'items' => $listWithPaging[0], 'count' => $listWithPaging[1]['total']);
 	$paging = $listWithPaging[1];
 	print printMobileEntryListView($list['items'],'category_'.$suri['page'],($category == 0 ? _text('모든 카테고리') : ucwords(getCategoryNameById($blogid, $category))),$paging, $list['count']);
-	print printMobileListNavigation($paging,'category/' . $category);
+	if ($context->getProperty('blog.entriesOnList') < $list['count']) print printMobileListNavigation($paging,'category/' . $category);
 }
 ?>
