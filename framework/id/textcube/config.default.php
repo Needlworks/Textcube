befb101bf999a83a4fed42096d8de3a333938272
Jeongkyu Shin <inureyes@gmail.com>
2014-03-02 00:09:29 +0900

    refs #1652 : changelog / version number updated.

--- framework/id/textcube/config.default.php
+++ framework/id/textcube/config.default.php
@@ -5,7 +5,7 @@
 
 // Define basic signatures.
 define('TEXTCUBE_NAME', 'Textcube');
-define('TEXTCUBE_VERSION', '1.9.1 : beta 1');
+define('TEXTCUBE_VERSION', '1.9.1');
 define('TEXTCUBE_REVISION', 'root-main-branch1.9-r30');
 define('TEXTCUBE_CODENAME', 'Largo');
 define('TEXTCUBE_COPYRIGHT', 'Copyright &copy; 2004-2014. Needlworks / Tatter Network Foundation. All rights reserved. Licensed under the GPL.');
