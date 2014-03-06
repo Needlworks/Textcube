8fcdd79719e5b9ced3bc1b99614adc5698fc2ead
Jeongkyu Shin <inureyes@gmail.com>
2014-03-06 16:53:07 +0900

    refs #1650 : bugfix - context call

--- interface/common/blog/begin.php
+++ interface/common/blog/begin.php
@@ -11,14 +11,9 @@ if (!empty($entries) && (count($entries) == 1))
 	$pageTitle = $entries[0]['title'];
 else
 	$pageTitle = '';
-if (!isset($skin))
-<<<<<<< HEAD
-	$skin = new Skin($context->getProperty('skin.skin'));
-
-=======
-	$skin = new Skin($skinSetting['skin']);
 $context = Model_Context::getInstance();
->>>>>>> dcc00ca...  refs #1650 : bugfix - context call
+if (!isset($skin))
+	$skin = new Skin($context->getProperty('skin.skin'));
 $view = $skin->outter;
 $view = str_replace('[##_SKIN_head_end_##]',getScriptsOnHead().'[##_SKIN_head_end_##]', $view); // TO DO : caching this part.
 $view = str_replace('[##_SKIN_body_start_##]',getUpperView(isset($paging) ? $paging : null).'[##_SKIN_body_start_##]', $view);
