8840a7bf3aff521d56952faa002cd42a9678eccf
Jeongkyu Shin <inureyes@gmail.com>
2014-03-09 23:27:49 +0900

    refs #1660 : modified - rearrange order between css and js  - Contributed by outsideris

--- interface/common/blog/begin.php
+++ interface/common/blog/begin.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /// Copyright (c) 2004-2014, Needlworks  / Tatter Network Foundation
 /// All rights reserved. Licensed under the GPL.
 /// See the GNU General Public License for more details. (/documents/LICENSE, /documents/COPYRIGHT)
@@ -15,16 +15,17 @@ $context = Model_Context::getInstance();
 if (!isset($skin))
 	$skin = new Skin($context->getProperty('skin.skin'));
 $view = $skin->outter;
-$view = str_replace('[##_SKIN_head_end_##]',getScriptsOnHead().'[##_SKIN_head_end_##]', $view); // TO DO : caching this part.
-$view = str_replace('[##_SKIN_body_start_##]',getUpperView(isset($paging) ? $paging : null).'[##_SKIN_body_start_##]', $view);
-$view = str_replace('[##_SKIN_body_end_##]',getLowerView().getScriptsOnFoot().'[##_SKIN_body_end_##]', $view); // care the order for js function overloading issue.
 $automaticLink = "	<link rel=\"stylesheet\" href=\"".$context->getProperty('service.resourcepath')."/style/system.css\" type=\"text/css\" media=\"screen\" />\n";
 if (!is_null($context->getProperty('uri.permalink',null))) {
 	$canonicalLink = "  <link rel=\"canonical\" href=\"".$context->getProperty('uri.permalink')."\"/>\n";
 } else {
 	$canonicalLink = '';
 }
-dress('SKIN_head_end', $canonicalLink.$automaticLink."[##_SKIN_head_end_##]", $view);
+dress('SKIN_head_end', $automaticLink.$canonicalLink."[##_SKIN_head_end_##]", $view);
+$view = str_replace('[##_SKIN_head_end_##]',getScriptsOnHead().'[##_SKIN_head_end_##]', $view); // TO DO : caching this part.
+$view = str_replace('[##_SKIN_body_start_##]',getUpperView(isset($paging) ? $paging : null).'[##_SKIN_body_start_##]', $view);
+$view = str_replace('[##_SKIN_body_end_##]',getLowerView().getScriptsOnFoot().'[##_SKIN_body_end_##]', $view); // care the order for js function overloading issue.
+
 $browserUtil = Utils_Browser::getInstance();
 if(Setting::getBlogSettingGlobal('useiPhoneUI',true) && ($browserUtil->isMobile() == true)) {
 	if (!empty($context->getProperty('suri.id'))) {
