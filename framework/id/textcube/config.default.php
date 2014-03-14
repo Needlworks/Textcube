4769248165780d4b77dc551b55954c567f74395f
Jeongkyu Shin <inureyes@gmail.com>
2014-03-15 00:13:41 +0900

    refs #1658 : Version number related bug

--- framework/id/textcube/config.default.php
+++ framework/id/textcube/config.default.php
@@ -5,9 +5,9 @@
 
 // Define basic signatures.
 define('TEXTCUBE_NAME', 'Textcube');
-define('TEXTCUBE_VERSION_ID', '1.9.2 : Release Candidate 1');
+define('TEXTCUBE_VERSION_ID', '1.9.2');
 define('TEXTCUBE_REVISION', 'root-main-branch1.9-r31');
-define('TEXTCUBE_CODENAME', '');
+define('TEXTCUBE_CODENAME', 'Release Candidate 1');
 define('TEXTCUBE_VERSION', TEXTCUBE_VERSION_ID.' : '.TEXTCUBE_CODENAME);
 define('TEXTCUBE_COPYRIGHT', 'Copyright &copy; 2004-2014. Needlworks / Tatter Network Foundation. All rights reserved. Licensed under the GPL.');
 define('TEXTCUBE_HOMEPAGE', 'http://www.textcube.org/');
