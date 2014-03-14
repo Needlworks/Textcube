aef0b911c277413d61587ac6b272b7c57e54f3dd
Joongi Kim <me@daybreaker.info>
2014-03-15 01:19:00 +0900

    refs #1654: revert local-only changes

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 /// Copyright (c) 2004-2014, Needlworks  / Tatter Network Foundation
 /// All rights reserved. Licensed under the GPL.
 /// See the GNU General Public License for more details. (/documents/LICENSE, /documents/COPYRIGHT)
-	#define('__TEXTCUBE_NO_FANCY_URL__',true);
+	define('__TEXTCUBE_NO_FANCY_URL__',true);
 	require 'rewrite.php';
 ?>
