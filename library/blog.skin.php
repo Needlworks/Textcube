775837611c0d1c9d74235bef2e35e2da02d0b312
Jeongkyu Shin <inureyes@gmail.com>
2014-03-20 23:13:24 +0900

    refs #1654 : modified - skincache for one-blog-multi-skin environment.

--- library/blog.skin.php
+++ library/blog.skin.php
@@ -98,7 +98,7 @@ class Skin {
 		$context = Model_Context::getInstance();
 
 		$this->cache = new pageCache;
-		$this->cache->reset('skinCache');
+		$this->cache->reset('skinCache-'.$name);
 		$__gDressTags = array();
 		if($previewMode == true || ($context->getProperty('service.skincache') != true) || !$this->loadCache()) {
 			$this->noneCommentMessage = Setting::getBlogSettingGlobal('noneCommentMessage',null);
