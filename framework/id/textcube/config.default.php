70f54861e2dfaa5596c0e122a5f7db7b959d8616
Jeongkyu Shin <inureyes@gmail.com>
2014-12-25 23:32:35 +0900

    refs #514 : modified - Year footprint modified.
     -

--- framework/id/textcube/config.default.php
+++ framework/id/textcube/config.default.php
@@ -1,5 +1,5 @@
 <?php
-/// Copyright (c) 2004-2014, Needlworks  / Tatter Network Foundation
+/// Copyright (c) 2004-2015, Needlworks  / Tatter Network Foundation
 /// All rights reserved. Licensed under the GPL.
 /// See the GNU General Public License for more details. (/documents/LICENSE, /documents/COPYRIGHT)
 
@@ -7,9 +7,9 @@
 define('TEXTCUBE_NAME', 'Textcube');
 define('TEXTCUBE_VERSION_ID', '1.10.1');
 define('TEXTCUBE_REVISION', 'root-main-branch1.10-r34');
-define('TEXTCUBE_CODENAME', ': Release Candidate 1');
+define('TEXTCUBE_CODENAME', ': Release Candidate 2');
 define('TEXTCUBE_VERSION', TEXTCUBE_VERSION_ID.' : '.TEXTCUBE_CODENAME);
-define('TEXTCUBE_COPYRIGHT', 'Copyright &copy; 2004-2014. Needlworks / Tatter Network Foundation. All rights reserved. Licensed under the GPL.');
+define('TEXTCUBE_COPYRIGHT', 'Copyright &copy; 2004-2015. Needlworks / Tatter Network Foundation. All rights reserved. Licensed under the GPL.');
 define('TEXTCUBE_HOMEPAGE', 'http://www.textcube.org/');
 define('TEXTCUBE_RESOURCE_URL', 'http://resources.textcube.org/1.10.1');
 define('TEXTCUBE_NOTICE_URL','http://feeds.feedburner.com/textcube/');
--- framework/legacy/Textcube.Data.Comment.php
+++ framework/legacy/Textcube.Data.Comment.php
@@ -1,5 +1,5 @@
 <?php
-/// Copyright (c) 2004-2014, Needlworks  / Tatter Network Foundation
+/// Copyright (c) 2004-2015, Needlworks  / Tatter Network Foundation
 /// All rights reserved. Licensed under the GPL.
 /// See the GNU General Public License for more details. (/documents/LICENSE, /documents/COPYRIGHT)
 class Comment {
