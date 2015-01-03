7d36258ed04a17e4d33660a3e70510c88357979b
Jeongkyu Shin <inureyes@gmail.com>
2015-01-03 21:13:34 +0900

    refs #1269 : updated - jquery version (1.11.1 to 1.11.2) to match  jqueryUI version.
     -

--- framework/id/textcube/config.default.php
+++ framework/id/textcube/config.default.php
@@ -28,7 +28,7 @@ define( "XPATH_LIBRARY_ROOT", ROOT . "/library/contrib/phpxpath/" );
 define( "Auth_OpenID_NO_MATH_SUPPORT", 1 );
 define( "OPENID_PASSWORD", "-OPENID-" );
 
-define('JQUERY_VERSION','1.11.1.min');
+define('JQUERY_VERSION','1.11.2.min');
 define('JQUERY_UI_VERSION','1.11.2.min');
 define('LODASH_VERSION','2.4.1.min');
 
