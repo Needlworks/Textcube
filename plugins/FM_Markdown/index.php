e0b68d9337754b2a3d3380ef57bf082d51e10a9e
Jeongkyu Shin <inureyes@gmail.com>
2015-02-15 01:22:23 +0900

    refs #1762 : updated - TTML-style tag preservation routine.

--- plugins/FM_Markdown/index.php
+++ plugins/FM_Markdown/index.php
@@ -1,26 +1,28 @@
 <?php
-// Markdown formatter for Textcube 1.6
+// Markdown formatter for Textcube 1.10
 // By Jeongkyu Shin. (inureyes@gmail.com)
 
 if(!function_exists('Markdown')) require_once 'markdown.php';
 
 function FM_Markdown_format($blogid, $id, $content, $keywords = array(), $useAbsolutePath = true, $bRssMode = false) {
-	global $service;
+	$context = Model_Context::getInstance();
 	$path = __TEXTCUBE_ATTACH_DIR__."/$blogid";
-	$url = "{$service['path']}/attach/$blogid";
+	$url = $context->getProperty("service.path")."/attach/$blogid";
 	if(!function_exists('FM_TTML_bindAttachments')) { // To reduce the amount of loading code!
 		require_once 'ttml.php';
 	}
 	$view = FM_TTML_bindAttachments($id, $path, $url, $content, $useAbsolutePath, $bRssMode);
+	$view = FM_TTML_preserve_TTML_type_tags($view);
 	$view = Markdown($view, $id);
+	$view = FM_TTML_restore_TTML_type_tags($view);
 	$view = FM_TTML_bindTags($id, $view);
 	return $view;
 }
 
 function FM_Markdown_summary($blogid, $id, $content, $keywords = array(), $useAbsolutePath = true) {
-	global $blog;
+	$context = Model_Context::getInstance();
 	$view = FM_Markdown_format($blogid, $id, $content, $keywords, $useAbsolutePath, true);
-    if (!$blog['publishWholeOnRSS']) $view = UTF8::lessen(removeAllTags(stripHTML($view)), 255);
+    if (!$context->getProperty("blog.publishWholeOnRSS")) $view = UTF8::lessen(removeAllTags(stripHTML($view)), 255);
 		return $view;
 }
 ?>
