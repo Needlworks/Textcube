2cb58bcaa7b63fe908c3c6408be2d77b81480872
Jeongkyu Shin <inureyes@gmail.com>
2015-02-25 12:06:14 +0900

    refs #1759 : modified - IP filtering now uses window size.
     -

--- plugins/EAS/index.php
+++ plugins/EAS/index.php
@@ -1,14 +1,14 @@
 <?php
 /* Frypan Anti-spam Service adapter for Textcube
    ---------------------------------------------
-   Version 2.0
+   Version 2.1
    Tatter Network Foundation development team / Needlworks.
 
    Creator          : Gendoh
    Maintainer       : inureyes
 
    Created at       : 2006.6.8
-   Last modified at : 2015.2.1
+   Last modified at : 2015.2.25
 
  General Public License
  http://www.gnu.org/licenses/gpl.html
@@ -26,9 +26,11 @@ function FAS_Call($type, $name, $title, $url, $content)
 
 	$blogstr = $context->getProperty('uri.host').$context->getProperty('uri.blog');
 
+	$DDosTimeWindowSize = 300;
+
 	$rpc = new XMLRPC();
 	$rpc->url = 'http://antispam.textcube.org/RPC/';
-	if ($rpc->call('checkSpam', $blogstr, $type, $name, $title, $url, $content, $_SERVER['REMOTE_ADDR']) == false) 
+	if ($rpc->call('checkSpam', $blogstr, $type, $name, $title, $url, $content, $_SERVER['REMOTE_ADDR']) == false)
 	{
 		// call fail
 		// Do Local spam check with "Thief-cat algorithm"
@@ -37,7 +39,7 @@ function FAS_Call($type, $name, $title, $url, $content)
 		if ($type == 2) // Trackback Case
 		{
 			$storage = "RemoteResponses";
-			$pool->init($storage);
+			$pool->reset($storage);
 
 			$pool->setQualifier("url","eq",$url,true);
 			$pool->setQualifier("isfiltered",">",0);
@@ -45,10 +47,10 @@ function FAS_Call($type, $name, $title, $url, $content)
 			if ($cnt = $pool->getCount("id")) {
 				$count += $cnt;
 			}
-			
+
 		} else { // Comment Case
 			$storage = "Comments";
-			$pool->init($storage);
+			$pool->reset($storage);
 			$pool->setQualifier("comment","eq",$$content,true);
 			$pool->setQualifier("name","eq",$name,true);
 			$pool->setQualifier("homepage","eq",$url,true);
@@ -60,9 +62,9 @@ function FAS_Call($type, $name, $title, $url, $content)
 		}
 
 		// Check IP
-		$pool->init($storage);
+		$pool->reset($storage);
 		$pool->setQualifier("ip","eq",$_SERVER['REMOTE_ADDR'],true);
-		//$pool->setQualifier("isfiltered",">",0);
+		$pool->setQualifier("written",">",Timestamp::getUNIXtime()-$DDosTimeWindowSize);
 
 		if ($cnt = $pool->getCount("id")) {
 			$count += $cnt;
@@ -71,19 +73,19 @@ function FAS_Call($type, $name, $title, $url, $content)
 		if ($count >= 10) {
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
 	if (!is_null($rpc->fault)) {
 		// FAS has some problem
 		return true;
 	}
-	
+
 	if ($rpc->result['result'] == true) {
 		return false; // it's spam
 	}
-	
+
 	return true;
 }
 
@@ -94,16 +96,15 @@ function FAS_AddingTrackback($target, $mother)
 
 function FAS_AddingComment($target, $mother)
 {
-	global $user;
 	if ($mother['secret'] ==  true) // it's secret(only owner can see it)
 	{
 		// Don't touch
 		return $target;
 	}
-	
+
 	$type = 1; // comment
 	if ($mother['entry'] == 0) $type = 3; // guestbook
-	
+
 	return $target && FAS_Call($type, $mother['name'], '', $mother['homepage'], $mother['comment']);
 }
 
