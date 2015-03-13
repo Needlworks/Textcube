179182458d46bdd0b84f77ea24b1f8a2a3e145a8
Jeongkyu Shin <inureyes@gmail.com>
2015-03-14 00:25:58 +0900

    refs #1662 : removed - legacy mobile language resources.
     -

--- library/preprocessor.php
+++ library/preprocessor.php
@@ -282,7 +282,7 @@ if (!defined('NO_INITIALIZAION')) {
     -------------------------------------------
     Load and bind specific plugin codes and initialze them.
 */
-if(in_array($context->getProperty('uri.interfaceType'), array('blog','owner','reader','mobile'))) {
+if(in_array($context->getProperty('uri.interfaceType'), array('blog','owner','reader'))) {
 	require_once(ROOT.'/library/plugins.php');
 }
 
