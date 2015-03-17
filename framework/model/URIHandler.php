0dde138002922611ac60ba9b3cde0a82c19898d1
Jeongkyu Shin <inureyes@gmail.com>
2015-03-17 16:18:36 +0900

    refs #1776 : modified - global cache storage initialize with blog id.
     -

--- framework/model/URIHandler.php
+++ framework/model/URIHandler.php
@@ -117,7 +117,7 @@ final class Model_URIHandler extends Singleton
 	private function __URIvariableParser() {
 		global $suri, $blog, $blogid, $skinSetting, $gCacheStorage;
 		$blogid        = $this->blogid;
-		$gCacheStorage = new globalCacheStorage; // Initialize global cache
+		$gCacheStorage = new globalCacheStorage($blogid); // Initialize global cache
 
 		$suri        = $this->suri;
 		$blog        = Setting::getBlogSettingsGlobal($this->blogid);
