0b804607e86634978d37db234792580e3356ceba
Jeongkyu Shin <inureyes@gmail.com>
2015-04-05 16:21:49 +0900

    refs #1067 : modified - removed traditional UTF8 class calls.

--- plugins/refererkeyword/index.php
+++ plugins/refererkeyword/index.php
@@ -439,7 +439,7 @@ function refererkeyword() {
                         <td class="<?php echo $urlClassName; ?>"></td>
                         <td class="refererurl">
                         <?php
-                        echo "<a href=\"" . Misc::escapeJSInAttribute($splitRefererURL) . "\" onclick=\"window.open(this.href); return false;\">" . UTF8::lessenAsEm(htmlspecialchars($decodeURL), 90) . "</a>";
+                        echo "<a href=\"" . Misc::escapeJSInAttribute($splitRefererURL) . "\" onclick=\"window.open(this.href); return false;\">" . Utils_Unicode::lessenAsEm(htmlspecialchars($decodeURL), 90) . "</a>";
                         $j++;
                     }
 
