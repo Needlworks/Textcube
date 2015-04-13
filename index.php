e30b8ac2bca428e44ebe8a4535ba3a33e7eea055
Jeongkyu Shin <inureyes@gmail.com>
2015-04-13 20:49:36 +0900

    refs #1067 : modified - roll-back index.php
     -

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/// Copyright (c) 2004-2015, Needlworks  / Tatter Network Foundation
+/// All rights reserved. Licensed under the GPL.
+/// See the GNU General Public License for more details. (/documents/LICENSE, /documents/COPYRIGHT)
+	define('__TEXTCUBE_NO_FANCY_URL__',true);
+	require 'rewrite.php';
+?>
