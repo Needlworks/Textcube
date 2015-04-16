8d4c80433524b80f4f0f5833138c06bfebe0e9d1
Jeongkyu Shin <inureyes@gmail.com>
2015-04-17 00:58:27 +0900

    refs #1795 : modify - allow post-config load.
     -

--- framework/id/textcube/Dispatcher.php
+++ framework/id/textcube/Dispatcher.php
@@ -9,7 +9,10 @@ final class Dispatcher {
     public $uri, $service, $interfacePath;
 
     public function __construct() {
-        //$this->pathSelector();
+		//$this->pathSelector();
+		if (!defined('__TEXTCUBE_CONFIG_FILE__')) {
+			define('__TEXTCUBE_CONFIG_FILE__', ROOT . '/config.php');
+		}
         $this->URIinterpreter();
         $this->pathSelector();
     }
@@ -29,7 +32,7 @@ final class Dispatcher {
     }
 
     private function pathSelector() {
-        define('__TEXTCUBE_CONFIG_FILE__', ROOT . '/config.php');
+        //define('__TEXTCUBE_CONFIG_FILE__', ROOT . '/config.php');
         if (!defined('__TEXTCUBE_CACHE_DIR__')) {
             define('__TEXTCUBE_CACHE_DIR__', ROOT . '/cache');
         }
